22b5f52812a2f4588c093d6d3ee727f89c10d1b6
Saravanan Dayalan <dayalsaravanan@gmail.com>
2021-10-20 13:29:22 +0530

    simulation in free space

--- Chapter_1/Examples/fd3d_1_1.py
+++ Chapter_1/Examples/fd3d_1_1.py
@@ -7,11 +7,11 @@
 # FDTD simulation of a pulse in free space after 100 steps.
 # The pulse originated in the center and travels outward.
 
+import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
 matplotlib.rcParams.update({'font.family': 'serif', 'font.size': 8,
-    'axes.labelsize': 10, 'axes.titlesize': 10, 'figure.titlesize': 10})
-import matplotlib.pyplot as plt
+                            'axes.labelsize': 10, 'axes.titlesize': 10, 'figure.titlesize': 10})
 
 ke = 200
 ex = np.zeros(ke)
@@ -32,7 +32,7 @@ for time_step in range(1, nsteps + 1):
 
     # put a Gaussian pulse in the middle
     ex[kc] = np.exp(-0.5 * ((t0 - time_step)/spread)**2)
-     
+
     # calculate the Hy field
     for k in range(ke - 1):
         hy[k] = hy[k] + 0.5 * (ex[k] - ex[k + 1])
@@ -42,9 +42,9 @@ fig.suptitle(r'FDTD simulation of a pulse in free space after 100 time steps')
 ax1.plot(ex, 'k', lw=1)
 ax1.text(100, 0.5, 'T = {}'.format(time_step), horizontalalignment='center')
 ax1.set(xlim=(0, 200), ylim=(-1.2, 1.2), ylabel=r'E$_x$')
-ax1.set(xticks=range(0,201,20), yticks=np.arange(-1,1.2,1))
+ax1.set(xticks=range(0, 201, 20), yticks=np.arange(-1, 1.2, 1))
 ax2.plot(hy, 'k', lw=1)
 ax2.set(xlim=(0, 200), ylim=(-1.2, 1.2), xlabel=r'FDTD cells', ylabel=r'H$_y$')
-ax2.set(xticks=range(0,201,20), yticks=np.arange(-1,1.2,1))
+ax2.set(xticks=range(0, 201, 20), yticks=np.arange(-1, 1.2, 1))
 plt.subplots_adjust(bottom=0.2, hspace=0.45)
 plt.savefig('fd3d_1_1.pdf')
