08bc77b72f5cfaa7e569222b1bdaabc707d3e759
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-11-02 14:25:14 +0530

    simulation in free space

--- FD1D/Programs/fd1d_1_1.py
+++ FD1D/Programs/fd1d_1_1.py
@@ -7,18 +7,28 @@
 # FDTD simulation of a pulse in free space after 100 steps.
 # The pulse originated in the center and travels outward.
 
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib
-matplotlib.rcParams.update({'font.family': 'serif', 'font.size': 8,
-                            'axes.labelsize': 10, 'axes.titlesize': 10, 'figure.titlesize': 10})
 
-ke = 200
+matplotlib.rcParams["text.usetex"] = True
+matplotlib.rcParams["pgf.texsystem"] = "pdflatex"
+matplotlib.rcParams.update(
+    {
+        "font.family": "serif",
+        "font.size": 8,
+        "axes.labelsize": 10,
+        "axes.titlesize": 10,
+        "figure.titlesize": 10,
+    }
+)
+
+ke = 201
 ex = np.zeros(ke)
 hy = np.zeros(ke)
 
 # Pulse parameters
-kc = int(ke/2)
+kc = int(ke / 2)
 t0 = 40
 spread = 12
 nsteps = 100
@@ -31,20 +41,20 @@ for time_step in range(1, nsteps + 1):
         ex[k] = ex[k] + 0.5 * (hy[k - 1] - hy[k])
 
     # put a Gaussian pulse in the middle
-    ex[kc] = np.exp(-0.5 * ((t0 - time_step)/spread)**2)
+    ex[kc] = np.exp(-0.5 * ((t0 - time_step) / spread) ** 2)
 
     # calculate the Hy field
     for k in range(ke - 1):
         hy[k] = hy[k] + 0.5 * (ex[k] - ex[k + 1])
 
 fig, (ax1, ax2) = plt.subplots(2)
-fig.suptitle(r'FDTD simulation of a pulse in free space after 100 time steps')
-ax1.plot(ex, 'k', lw=1)
-ax1.text(100, 0.5, 'T = {}'.format(time_step), horizontalalignment='center')
-ax1.set(xlim=(0, 200), ylim=(-1.2, 1.2), ylabel=r'E$_x$')
+fig.suptitle(r"FDTD simulation of a pulse in free space after 100 time steps")
+ax1.plot(ex, "k", lw=1)
+ax1.text(100, 0.5, "T = {}".format(time_step), horizontalalignment="center")
+ax1.set(xlim=(0, 200), ylim=(-1.2, 1.2), ylabel=r"E$_x$")
 ax1.set(xticks=range(0, 201, 20), yticks=np.arange(-1, 1.2, 1))
-ax2.plot(hy, 'k', lw=1)
-ax2.set(xlim=(0, 200), ylim=(-1.2, 1.2), xlabel=r'FDTD cells', ylabel=r'H$_y$')
+ax2.plot(hy, "k", lw=1)
+ax2.set(xlim=(0, 200), ylim=(-1.2, 1.2), xlabel=r"FDTD cells", ylabel=r"H$_y$")
 ax2.set(xticks=range(0, 201, 20), yticks=np.arange(-1, 1.2, 1))
 plt.subplots_adjust(bottom=0.2, hspace=0.45)
-plt.savefig('fd1d_1_1.png')
+plt.savefig("fd1d_1_1.png")
