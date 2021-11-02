02c83b242e078252278d0fe5679bfb9288a98d38
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-11-02 18:01:15 +0530

    transmitted pulse propagate until it hits the right wall in dielectric medium

--- fd1d/problem/problem_1_4/fd1d_1_3.py
+++ fd1d/problem/problem_1_4/fd1d_1_3.py
@@ -3,16 +3,26 @@
 # Name: D.Saravanan
 # Date: 21/10/2021
 
-""" Simulation of a pulse hitting a dielectric medium and let the transmitted pulse
-propagate until it hits the far right wall """
+""" Simulation of a pulse hitting a dielectric medium and let
+the transmitted pulse propagate until it hits the far right wall """
 
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib
-matplotlib.rcParams.update({'font.family': 'serif', 'font.size': 8,
-                            'axes.labelsize': 10, 'axes.titlesize': 10, 'figure.titlesize': 10})
 
-ke = 200
+matplotlib.rcParams["text.usetex"] = True
+matplotlib.rcParams["pgf.texsystem"] = "pdflatex"
+matplotlib.rcParams.update(
+    {
+        "font.family": "serif",
+        "font.size": 8,
+        "axes.labelsize": 10,
+        "axes.titlesize": 10,
+        "figure.titlesize": 10,
+    }
+)
+
+ke = 201
 ex = np.zeros(ke)
 hy = np.zeros(ke)
 
@@ -27,16 +37,16 @@ boundary_high = [0, 0]
 epsilon = 4
 cb = np.ones(ke)
 cb = 0.5 * cb
-cb[100:] = 0.5/epsilon
+cb[100:] = 0.5 / epsilon
 
 nsteps = 660
 
 # desired points for plotting
 points = [
-    {'num_steps': 100, 'data': None, 'label': ' '},
-    {'num_steps': 220, 'data': None, 'label': ' '},
-    {'num_steps': 440, 'data': None, 'label': ' '},
-    {'num_steps': 660, 'data': None, 'label': 'FDTD cells'}
+    {"num_steps": 100, "data": None, "label": " "},
+    {"num_steps": 220, "data": None, "label": " "},
+    {"num_steps": 440, "data": None, "label": " "},
+    {"num_steps": 660, "data": None, "label": "FDTD cells"},
 ]
 
 # FDTD loop
@@ -47,7 +57,7 @@ for time_step in range(1, nsteps + 1):
         ex[k] = ex[k] + cb[k] * (hy[k - 1] - hy[k])
 
     # put a Gaussian pulse at the low end
-    ex[5] = ex[5] + np.exp(-0.5 * ((t0 - time_step)/spread)**2)
+    ex[5] = ex[5] + np.exp(-0.5 * ((t0 - time_step) / spread) ** 2)
 
     # absorbing boundary conditons
     ex[0] = boundary_low.pop(0)
@@ -61,27 +71,26 @@ for time_step in range(1, nsteps + 1):
 
     # save data at certain points for plotting
     for plot_data in points:
-        if time_step == plot_data['num_steps']:
-            plot_data['data'] = np.copy(ex)
+        if time_step == plot_data["num_steps"]:
+            plot_data["data"] = np.copy(ex)
 
 fig = plt.figure(figsize=(8, 5.25))
-fig.suptitle(r'FDTD simulation of a pulse hitting a dielectric medium')
+fig.suptitle(r"FDTD simulation of a pulse hitting a dielectric medium")
 
 
 def plotting(data, timestep, label):
-    """ plot of E field at a single time step """
-    ax.plot(data, color='k', linewidth=1)
-    ax.plot((0.5/cb - 1)/3, 'k--', linewidth=0.75)
-    ax.set(xlim=(0, 199), ylim=(-0.5, 1.2),
-           xlabel=r'{}'.format(label), ylabel=r'E$_x$')
-    ax.set(xticks=np.arange(0, 199, 20), yticks=np.arange(-0.5, 1.2, 0.5))
-    ax.text(70, 0.5, 'T = {}'.format(timestep), horizontalalignment='center')
-    ax.text(170, 0.5, 'Eps = {}'.format(epsilon), horizontalalignment='center')
+    """plot of E field at a single time step"""
+    ax.plot(data, color="k", linewidth=1)
+    ax.plot((0.5 / cb - 1) / 3, "k--", linewidth=0.75)
+    ax.set(xlim=(0, 200), ylim=(-0.5, 1.2), xlabel=r"{}".format(label), ylabel=r"E$_x$")
+    ax.set(xticks=np.arange(0, 220, 20), yticks=np.arange(-0.5, 1.2, 0.5))
+    ax.text(70, 0.5, "T = {}".format(timestep), horizontalalignment="center")
+    ax.text(170, 0.5, "Eps = {}".format(epsilon), horizontalalignment="center")
 
 
 for subplot_num, plot_data in enumerate(points):
     ax = fig.add_subplot(4, 1, subplot_num + 1)
-    plotting(plot_data['data'], plot_data['num_steps'], plot_data['label'])
+    plotting(plot_data["data"], plot_data["num_steps"], plot_data["label"])
 
 plt.tight_layout()
-plt.savefig('fd1d_1_3.pdf')
+plt.savefig("fd1d_1_3.pdf")
