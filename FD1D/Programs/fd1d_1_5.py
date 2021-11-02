3f9e796d7d6964dbf852e89e4fb97c00fee69f48
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-11-02 14:45:56 +0530

    simulation of a sinusoidal wave hitting a lossy dielectric

--- FD1D/Programs/fd1d_1_5.py
+++ FD1D/Programs/fd1d_1_5.py
@@ -5,34 +5,44 @@
 
 """ Simulation of a sinusoidal wave hitting a lossy dielectric """
 
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib
-matplotlib.rcParams.update({'font.family': 'serif', 'font.size': 8,
-                            'axes.labelsize': 10, 'axes.titlesize': 10, 'figure.titlesize': 10})
 
-ke = 200
+matplotlib.rcParams["text.usetex"] = True
+matplotlib.rcParams["pgf.texsystem"] = "pdflatex"
+matplotlib.rcParams.update(
+    {
+        "font.family": "serif",
+        "font.size": 8,
+        "axes.labelsize": 10,
+        "axes.titlesize": 10,
+        "figure.titlesize": 10,
+    }
+)
+
+ke = 201
 ex = np.zeros(ke)
 hy = np.zeros(ke)
 
-dx = 0.01           # cell size
-dt = dx/6e8         # time step size
-freq = 700e6        # frequency 700 MHz
+dx = 0.01  # cell size
+dt = dx / 6e8  # time step size
+freq = 700e6  # frequency 700 MHz
 
 boundary_low = [0, 0]
 boundary_high = [0, 0]
 
 # Create Dielectric Profile
-epsz = 8.854e-12    # vacuum permittivity (F/m)
-epsilon = 4         # relative permittivity
-sigma = 0.04        # conductivity (S/m)
+epsz = 8.854e-12  # vacuum permittivity (F/m)
+epsilon = 4  # relative permittivity
+sigma = 0.04  # conductivity (S/m)
 
 ca = np.ones(ke)
 cb = 0.5 * np.ones(ke)
 
-eaf = dt * sigma/(2*epsz*epsilon)
-ca[100:] = (1 - eaf)/(1 + eaf)
-cb[100:] = 0.5/(epsilon * (1 + eaf))
+eaf = dt * sigma / (2 * epsz * epsilon)
+ca[100:] = (1 - eaf) / (1 + eaf)
+cb[100:] = 0.5 / (epsilon * (1 + eaf))
 
 nsteps = 500
 
@@ -57,20 +67,20 @@ for time_step in range(1, nsteps + 1):
         hy[k] = hy[k] + 0.5 * (ex[k] - ex[k + 1])
 
 fig, (ax1, ax2) = plt.subplots(2)
-fig.suptitle(r'FDTD simulation of a sinusoidal wave hitting a lossy dielectric')
-ax1.plot(ex, 'k', lw=1)
-ax1.plot((0.5/cb - 1)/3, 'k--', lw=0.75)
-ax1.text(50, 0.5, 'T = {}'.format(time_step), horizontalalignment='center')
-ax1.text(170, 0.5, 'Eps = {}'.format(epsilon), horizontalalignment='center')
-ax1.text(170, -0.5, 'Cond = {}'.format(sigma), horizontalalignment='center')
-ax1.set(xlim=(0, 199), ylim=(-1.2, 1.2), ylabel=r'E$_x$')
-ax1.set(xticks=range(0, 199, 20), yticks=np.arange(-1, 1.2, 1))
-ax2.plot(hy, 'k', lw=1)
-ax2.plot((0.5/cb - 1)/3, 'k--', lw=0.75)
-ax2.text(50, 0.5, 'T = {}'.format(time_step), horizontalalignment='center')
-ax2.text(170, 0.5, 'Eps = {}'.format(epsilon), horizontalalignment='center')
-ax2.text(170, -0.5, 'Cond = {}'.format(sigma), horizontalalignment='center')
-ax2.set(xlim=(0, 200), ylim=(-1.2, 1.2), xlabel=r'FDTD cells', ylabel=r'H$_y$')
-ax2.set(xticks=range(0, 199, 20), yticks=np.arange(-1, 1.2, 1))
+fig.suptitle(r"FDTD simulation of a sinusoidal wave hitting a lossy dielectric")
+ax1.plot(ex, "k", lw=1)
+ax1.plot((0.5 / cb - 1) / 3, "k--", lw=0.75)
+ax1.text(50, 0.5, "T = {}".format(time_step), horizontalalignment="center")
+ax1.text(170, 0.5, "Eps = {}".format(epsilon), horizontalalignment="center")
+ax1.text(170, -0.5, "Cond = {}".format(sigma), horizontalalignment="center")
+ax1.set(xlim=(0, 200), ylim=(-1.2, 1.2), ylabel=r"E$_x$")
+ax1.set(xticks=range(0, 220, 20), yticks=np.arange(-1, 1.2, 1))
+ax2.plot(hy, "k", lw=1)
+ax2.plot((0.5 / cb - 1) / 3, "k--", lw=0.75)
+ax2.text(50, 0.5, "T = {}".format(time_step), horizontalalignment="center")
+ax2.text(170, 0.5, "Eps = {}".format(epsilon), horizontalalignment="center")
+ax2.text(170, -0.5, "Cond = {}".format(sigma), horizontalalignment="center")
+ax2.set(xlim=(0, 200), ylim=(-1.2, 1.2), xlabel=r"FDTD cells", ylabel=r"H$_y$")
+ax2.set(xticks=range(0, 220, 20), yticks=np.arange(-1, 1.2, 1))
 plt.subplots_adjust(bottom=0.2, hspace=0.45)
-plt.savefig('fd1d_1_5.png')
+plt.savefig("fd1d_1_5.png")
