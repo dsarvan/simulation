4f3c4ee21ba9c2c2f17ebb375e1357f64120655d
Dayalan Saravanan <dayalsaravanan@gmail.com>
2022-01-10 16:30:17 +0530

    replaced np.zeros(ke) with ex

--- FD1D/Animation/gaussian.py
+++ FD1D/Animation/gaussian.py
@@ -21,40 +21,38 @@ nsteps = 100
 
 # time step for tha animation (s), maximum time to animate (s)
 dt, tmax = 1, nsteps
-# data to plot
-t, M = [], []
 
 # draw an empty plot, but preset the plot x- and y- limits
 fig, ax = plt.subplots()
-line, = ax.plot([], [])
+line, = ax.plot(ex)
+time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
 ax.set(xlim=(0, ke), ylim=(-1.2, 1.2))
 ax.set(xlabel=r'FDTD cells', ylabel=r'Ex')
 
 def init():
-    return line,
+    line.set_ydata(ex)
+    time_text.set_text('')
+    return line, time_text
 
-def animate(i, t, M):
+def animate(i):
 
     # FDTD loop
     for time_step in range(1, nsteps+1):
 
         for k in range(1, ke):
             ex[k] = ex[k] + 0.5 * (hy[k - 1] - hy[k])
-            t.append(k)
 
         ex[kc] = np.exp(-0.5 * ((t0 - time_step) / spread) ** 2)
-
-        M.append(ex)
-        print(M)
-
+    
         for k in range(ke - 1):
             hy[k] = hy[k] + 0.5 * (ex[k] - ex[k + 1])
-    
-    t.extend(range(1, 101))
-    line.set_data(t, M)
-    return line,
+        
+        line.set_ydata(ex)
+        time_text.set_text('T = {}'.format(time_step))
+
+    return line, time_text
 
 interval, nframes = 1000*dt, int(tmax/dt)
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=nframes, 
-            fargs=(t, M), repeat=False, interval=interval, blit=True)
-anim.save('gaussian_anim.mp4')
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=nframes,
+        interval=interval, blit=True)
+anim.save('gaussian.mp4')
