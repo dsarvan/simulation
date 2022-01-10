cb51c1d96a89aa33ecf05415c3b8044152e1d42f
Dayalan Saravanan <dayalsaravanan@gmail.com>
2022-01-10 19:01:38 +0530

    format with isort and black

--- FD1D/Animation/gaussian.py
+++ FD1D/Animation/gaussian.py
@@ -5,9 +5,9 @@
 
 """ Simulation of Gaussian pulse """
 
-import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
 
 ke = 200
 ex = np.zeros(ke)
@@ -24,35 +24,39 @@ dt, tmax = 1, nsteps
 
 # draw an empty plot, but preset the plot x- and y- limits
 fig, ax = plt.subplots()
-line, = ax.plot(ex)
-time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
+(line,) = ax.plot(ex)
+time_text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
 ax.set(xlim=(0, ke), ylim=(-1.2, 1.2))
-ax.set(xlabel=r'FDTD cells', ylabel=r'Ex')
+ax.set(xlabel=r"FDTD cells", ylabel=r"Ex")
+
 
 def init():
     line.set_ydata(ex)
-    time_text.set_text('')
+    time_text.set_text("")
     return line, time_text
+
 
 def animate(i):
 
     # FDTD loop
-    for time_step in range(1, nsteps+1):
+    for time_step in range(1, nsteps + 1):
 
         for k in range(1, ke):
             ex[k] = ex[k] + 0.5 * (hy[k - 1] - hy[k])
 
         ex[kc] = np.exp(-0.5 * ((t0 - time_step) / spread) ** 2)
-    
+
         for k in range(ke - 1):
             hy[k] = hy[k] + 0.5 * (ex[k] - ex[k + 1])
-        
+
         line.set_ydata(ex)
-        time_text.set_text('T = {}'.format(time_step))
+        time_text.set_text("T = {}".format(time_step))
 
     return line, time_text
 
-interval, nframes = 1000*dt, int(tmax/dt)
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=nframes,
-        interval=interval, blit=True)
-anim.save('gaussian.mp4')
+
+interval, nframes = 1000 * dt, int(tmax / dt)
+anim = animation.FuncAnimation(
+    fig, animate, init_func=init, frames=nframes, interval=interval, blit=True
+)
+anim.save("gaussian.mp4")
