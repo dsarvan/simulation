722a26af043a8adafe2e21f3f92d7bc420a89160
D.Saravanan <dayalsaravanan@gmail.com>
2022-05-19 22:54:43 +0530

    script for 1-dimensional fdtd with gaussian pulse as source

--- Numba/fdtdcupy.py
+++ Numba/fdtdcupy.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-# File: fdtdn.py
+# File: fdtdcupy.py
 # Name: D.Saravanan
 # Date: 16/05/2022
 
 """ Script for 1-dimensional fdtd with gaussian pulse as source """
 
 import numpy as np
+import cupy as cp
 import matplotlib.pyplot as plt
 
 plt.style.use("classic")
@@ -26,27 +27,27 @@ def plotting(time_step, ex, hy):
     """plot function"""
     fig, (ax1, ax2) = plt.subplots(2)
     fig.suptitle(r"FDTD simulation of a pulse in free space after 100 time steps")
-    ax1.plot(ex, "k", lw=1)
+    ax1.plot(ex.get(), "k", lw=1)
     ax1.text(100, 0.5, "T = {}".format(time_step), horizontalalignment="center")
     ax1.set(xlim=(0, 200), ylim=(-1.2, 1.2), ylabel=r"E$_x$")
     ax1.set(xticks=range(0, 220, 20), yticks=np.arange(-1, 1.2, 1))
-    ax2.plot(hy, "k", lw=1)
+    ax2.plot(hy.get(), "k", lw=1)
     ax2.set(xlim=(0, 200), ylim=(-1.2, 1.2), xlabel=r"FDTD cells", ylabel=r"H$_y$")
     ax2.set(xticks=range(0, 220, 20), yticks=np.arange(-1, 1.2, 1))
     plt.subplots_adjust(bottom=0.2, hspace=0.45)
-    plt.savefig("fdtdn.png")
+    plt.savefig("fdtdcupy.png")
 
 @profile
 def emfield(ke, kc, t0, spread, nsteps):
 
-    ex = np.zeros(ke)
-    hy = np.zeros(ke)
+    ex = cp.zeros(ke).astype(np.float32)
+    hy = cp.zeros(ke).astype(np.float32)
 
     for time_step in range(1, nsteps + 1):
 
         ex[1:ke] = ex[1:ke] + 0.5 * (hy[0:ke-1] - hy[1:ke])
 
-        ex[kc] = np.exp(-0.5 * ((t0 - time_step) / spread) ** 2)
+        ex[100] = cp.exp(-0.5 * ((t0 - time_step) / spread) ** 2)
 
         hy[0:ke-1] = hy[0:ke-1] + 0.5 * (ex[0:ke-1] - ex[1:ke])
 
@@ -63,6 +64,7 @@ def main():
     spread = 12
     nsteps = 100
 
+    zz = cp.zeros(1).astype(np.float32)
     time_step, ex, hy = emfield(ke, kc, t0, spread, nsteps)
     plotting(time_step, ex, hy)
 
