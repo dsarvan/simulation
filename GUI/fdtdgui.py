6643c861b2e2a69f02e9ff512b2995cc8f7ae508
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-07-21 20:07:37 +0530

    create root window with title text and resizable false

---
+++ GUI/fdtdgui.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# File: fdtdgui.py
+# Name: D.Saravanan
+# Date: 21/07/2022
+
+""" Script to create a graphical user interface for the FDTD program """
+
+import tkinter as tk
+
+root = tk.Tk()
+root.title("FTDT")
+root.resizable(False, False)
+root.mainloop()
