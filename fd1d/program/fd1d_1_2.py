0f648d057bced020b7e4e1a31909ea0be4ff89a4
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-12-27 19:32:10 +0530

    simulation of a pulse with absorbing boundary conditions

--- fd1d/program/fd1d_1_2.py
+++ fd1d/program/fd1d_1_2.py
@@ -4,6 +4,8 @@
 # Date: 19/10/2021
 
 """ Simulation of a pulse with absorbing boundary conditions """
+# FDTD simulation of a pulse in free space after 260 steps.
+# The pulse originated in the center and travels outward.
 # Notice that the pulse is absorded at the edges without reflecting anything back.
 
 import matplotlib.pyplot as plt
@@ -13,85 +15,56 @@ plt.style.use("classic")
 plt.style.use("../pyplot.mplstyle")
 
 
-def visualize(ex: np.ndarray, hy: np.ndarray) -> None:
-    """Data visualization with Matplotlib"""
-
-    fig, (ax1, ax2) = plt.subplots(2, sharex=False, gridspec_kw={"hspace": 0.2})
-    fig.suptitle(r"FDTD simulation of a pulse in free space after 260 time steps")
-    ax1.plot(ex, "k", lw=1)
-    ax1.text(100, 0.5, "T = 260", horizontalalignment="center")
-    ax1.set(xlim=(0, 200), ylim=(-1.2, 1.2), ylabel=r"E$_x$")
-    ax1.set(xticks=range(0, 220, 20), yticks=np.arange(-1, 1.2, 1))
-    ax2.plot(hy, "k", lw=1)
-    ax2.set(xlim=(0, 200), ylim=(-1.2, 1.2), xlabel=r"FDTD cells", ylabel=r"H$_y$")
-    ax2.set(xticks=range(0, 220, 20), yticks=np.arange(-1, 1.2, 1))
-    plt.subplots_adjust(bottom=0.2, hspace=0.45)
-    plt.savefig("fd1d_1_2.png")
+def visualize(ns: int, nx: int, ex: np.ndarray, hy: np.ndarray) -> None:
+	fig, (ax1, ax2) = plt.subplots(2, sharex=False, gridspec_kw={"hspace": 0.2})
+	fig.suptitle(f"FDTD simulation of a pulse in free space after {ns} time steps")
+	ax1.plot(ex, "k", lw=1)
+	ax1.text(nx//2, 0.5, f"T = {ns}", horizontalalignment="center")
+	ax1.set(xlim=(0, nx-1), ylim=(-1.2, 1.2), ylabel=r"$E_x$")
+	ax1.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 1.2, 1))
+	ax2.plot(hy, "k", lw=1)
+	ax2.set(xlim=(0, nx-1), ylim=(-1.2, 1.2), xlabel=r"FDTD cells", ylabel=r"$H_y$")
+	ax2.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 1.2, 1))
+	plt.subplots_adjust(bottom=0.2, hspace=0.45)
+	plt.savefig("fd1d_1_2.png")
 
 
-def gaussian(t: int, t0: int = 40, sigma: float = 12) -> float:
-    """
-    Gaussian pulse source
-
-    :param int t: an integer counter that serves as the temporal index
-    :param int t0: time step at which gaussian function is maximum, default 40
-    :param float sigma: width of the gaussian pulse, default 12
-
-    :return: gaussian pulse
-    :rtype: float
-
-    """
-
-    return np.exp(-0.5 * ((t - t0) / sigma) ** 2)
+def gaussian(t: int, t0: int, sigma: float) -> float:
+	return np.exp(-0.5 * ((t - t0)/sigma)**2)
 
 
-def simulate(ke: int, ex: np.ndarray, hy: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-    """
-    Finite-difference time-domain method
+def field(t:int, nx:int, ex:np.ndarray, hy:np.ndarray, lb:np.ndarray, hb:np.ndarray):
 
-    :param int ke: number of electric and magnetic field nodes
-    :param np.ndarray ex: electric field oriented in the x direction
-    :param np.ndarray hy: magnetic field oriented in the y direction
+	# calculate the Hy field
+	hy[0:nx-1] = hy[0:nx-1] + 0.5 * (ex[0:nx-1] - ex[1:nx])
 
-    :return: ex, hy: electric and magnetic field
-    :rtype: tuple[np.ndarray, np.ndarray]
+	# calculate the Ex field
+	ex[1:nx] = ex[1:nx] + 0.5 * (hy[0:nx-1] - hy[1:nx])
 
-    """
+	# put a Gaussian pulse in the middle
+	ex[nx//2] = gaussian(t, 40, 12)
 
-    kc: int = ke//2
-    nsteps: int = 260
-
-    lbound, hbound = [0, 0], [0, 0]
-
-    # FDTD simulation loop
-    for t in range(1, nsteps + 1):
-
-        # calculate the Ex field
-        ex[1:ke] = ex[1:ke] + 0.5 * (hy[0:ke-1] - hy[1:ke])
-
-        # put a Gaussian pulse in the middle
-        ex[kc] = gaussian(t, 40, 12)
-
-        # absorbing boundary conditions
-        ex[0], lbound[0], lbound[1] = lbound[0], lbound[1], ex[1]
-        ex[ke-1], hbound[0], hbound[1] = hbound[0], hbound[1], ex[ke-2]
-
-        # calculate the Hy field
-        hy[0:ke-1] = hy[0:ke-1] + 0.5 * (ex[0:ke-1] - ex[1:ke])
-
-    return ex, hy
+	# absorbing boundary conditions
+	ex[0], lb[0], lb[1] = lb[0], lb[1], ex[1]
+	ex[nx-1], hb[0], hb[1] = hb[0], hb[1], ex[nx-2]
 
 
 def main():
-    """Main function"""
 
-    ke = 201
-    ex = np.zeros(ke, dtype=np.float64)
-    hy = np.zeros(ke, dtype=np.float64)
+	nx: int = 201
+	ns: int = 260
 
-    ex, hy = simulate(ke, ex, hy)
-    visualize(ex, hy)
+	ex = np.zeros(nx, dtype=np.float64)
+	hy = np.zeros(nx, dtype=np.float64)
+
+	lb = np.zeros(2, dtype=np.float64)
+	hb = np.zeros(2, dtype=np.float64)
+
+	for t in range(1, ns+1):
+		field(t, nx, ex, hy, lb, hb)
+
+	visualize(ns, nx, ex, hy)
 
 
 if __name__ == "__main__":
-    main()
+	main()
