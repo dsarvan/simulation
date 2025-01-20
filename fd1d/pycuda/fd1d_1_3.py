bf7349c990e213f5a9b571965d62e5530cd4d22f
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-01-20 21:35:02 +0530

    simulation of a pulse hitting a dielectric medium

--- fd1d/pycuda/fd1d_1_3.py
+++ fd1d/pycuda/fd1d_1_3.py
@@ -46,18 +46,22 @@ __global__ void field(int t, int nx, float *cb, float *ex, float *hy, float *bc)
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
 	int stride = blockDim.x * gridDim.x;
 
+	/* calculate the Hy field */
 	for (int i = index; i < nx - 1; i += stride)
 		hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
 
 	__syncthreads();
 
+	/* calculate the Ex field */
 	for (int i = index + 1; i < nx; i += stride)
 		ex[i] = ex[i] + cb[i] * (hy[i-1] - hy[i]);
 
 	__syncthreads();
 
+	/* put a Gaussian pulse in the middle */
 	ex[1] = ex[1] + gaussian(t, 40, 12);
 
+	/* absorbing boundary conditions */
 	ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
 	ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
 }
