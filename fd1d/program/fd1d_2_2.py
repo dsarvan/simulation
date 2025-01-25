55bf1b3aad8391b553fa5c6045c193386e337c68
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-01-25 22:58:07 +0530

    implements the discrete fourier transform with a gaussian pulse as its source

--- fd1d/program/fd1d_2_2.py
+++ fd1d/program/fd1d_2_2.py
@@ -4,157 +4,96 @@
 # Date: 07/12/2021
 
 """ Simulation of a pulse striking a dielectric medium and implements
-    the discrete Fourier transform with a Gaussian pulse as its source """
+the discrete Fourier transform with a Gaussian pulse as its source """
 
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import pi, exp, sqrt, sin, cos, atan2
 
 plt.style.use("classic")
-plt.rcParams["text.usetex"] = True
-plt.rcParams["pgf.texsystem"] = "pdflatex"
-plt.rcParams.update(
-    {
-        "font.family": "serif",
-        "font.size": 10,
-        "axes.labelsize": 12,
-        "axes.titlesize": 12,
-        "figure.titlesize": 12,
-    }
-)
-
-ke = 201
-ex = np.zeros(ke)
-ix = np.zeros(ke)
-Dx = np.zeros(ke)
-hy = np.zeros(ke)
-
-dx = 0.01   # cell size
-dt = dx / 6e8   # time step size
-freq = np.array((100e6, 200e6, 500e6))  # frequency 100 MHz, 200 MHz, 500 MHz
-nfreq = len(freq)   # number of frequencies
-
-boundary_low = [0, 0]
-boundary_high = [0, 0]
-
-# dielectric profile
-epsz = 8.854e-12    # vacuum permittivity (F/m)
-epsr = 4    # relative permittivity
-sigma = 0   # conductivity (S/m)
-
-gax = np.ones(ke)
-gbx = np.zeros(ke)
-gax[100:] = 1/(epsr + (sigma * dt/epsz))
-gbx[100:] = sigma * dt/epsz
-
-# used in the fourier transform
-real_in = np.zeros(nfreq)
-imag_in = np.zeros(nfreq)
-
-amp_in = np.zeros(nfreq)
-phase_in = np.zeros(nfreq)
-
-real_pt = np.zeros((nfreq, ke))
-imag_pt = np.zeros((nfreq, ke))
-
-amp = np.zeros((nfreq, ke))
-phase = np.zeros((nfreq, ke))
-
-# desired points for plotting
-points = [
-    {"num_steps": 200, "ex": None, "amp": None, "phase": None, "label": " ", "label_ab": "(a)"},
-    {"num_steps": 400, "ex": None, "amp": None, "phase": None, "label": "FDTD cells", "label_ab": "(b)"}
-]
-
-t0 = 50
-spread = 10
-nsteps = 400
-
-# FDTD loop
-for time_step in range(1, nsteps + 1):
-
-    # calculate the Dx flux
-    for k in range(1, ke):
-        Dx[k] = Dx[k] + 0.5 * (hy[k - 1] - hy[k])
-
-    # put a sinusoidal at the low end
-    Dx[5] = Dx[5] + np.exp(-0.5 * ((t0 - time_step)/spread)**2)
-
-    # calculate the Ex field from Dx
-    for k in range(1, ke):
-        ex[k] = gax[k] * (Dx[k] - ix[k])
-        ix[k] = ix[k] + gbx[k] * ex[k]
-
-    # calculate the Fourier transform of Ex
-    for k in range(ke):
-        for m in range(nfreq):
-            real_pt[m,k] = real_pt[m,k] + np.cos(2*np.pi*m*dt*time_step) * ex[k]
-            imag_pt[m,k] = imag_pt[m,k] - np.sin(2*np.pi*m*dt*time_step) * ex[k]
-
-    # fourier transform of the input pulse
-    if time_step < 100:
-        for m in range(nfreq):
-            real_in[m] = real_in[m] + np.cos(2*np.pi*m*dt*time_step) * ex[10]
-            imag_in[m] = imag_in[m] - np.sin(2*np.pi*m*dt*time_step) * ex[10]
-
-    # absorbing boundary conditions
-    ex[0] = boundary_low.pop(0)
-    boundary_low.append(ex[1])
-    ex[ke - 1] = boundary_high.pop(0)
-    boundary_high.append(ex[ke - 2])
-
-    # calculate the Hy field
-    for k in range(ke - 1):
-        hy[k] = hy[k] + 0.5 * (ex[k] - ex[k + 1])
-
-    # save data at certain points for plotting
-    for plot_data in points:
-        if time_step == plot_data["num_steps"]:
-            plot_data["ex"] = np.copy(ex)
-
-            # calculate the amplitude and phase at each frequency
-            for m in range(nfreq):
-                amp_in[m] = np.sqrt(real_in[m]**2 + imag_in[m]**2)
-                phase_in[m] = np.arctan2(real_in[m], imag_in[m])
-
-                for k in range(ke):
-                    amp[m,k] = (1/amp_in[m])*np.sqrt(real_pt[m,k]**2 + imag_pt[m, k]**2)
-                    phase[m,k] = np.arctan2(real_pt[m,k], imag_pt[m,k]) - phase_in[m]
-
-            plot_data["amp"] = np.copy(amp)
-            plot_data["phase"] = np.copy(phase)
-
-fig = plt.figure(figsize=(8, 7))
-fig.suptitle(r"Discrete Fourier transform with a Gaussian pulse as its source")
+plt.style.use("../pyplot.mplstyle")
 
 
-def plotting_ex(data, ga, time_step, label_ab):
-    """plot of E field at a single time step"""
-    ax.plot(data, color="k", linewidth=1)
-    ax.plot(-(ga - 1)/0.75, 'k--', linewidth=0.75)
-    ax.set(xlim=(0, 200), ylim=(-1.2, 1.2), ylabel=r"$E_x$")
-    ax.set(xticks=np.arange(0, 220, 20), yticks=np.arange(-1.2, 1.4, 0.5))
-    ax.text(35, 0.3, "Time domain, T = {}".format(time_step), horizontalalignment="center")
-    ax.text(-25, -2.1, label_ab, horizontalalignment="center")
-    return
+def gaussian(t: int, t0: int, sigma: float) -> float:
+	return np.exp(-0.5 * ((t - t0)/sigma)**2)
 
 
-def plotting_amp(data, ga, freq, label):
-    """plot of the Fourier transform amplitude at a single time step"""
-    ax.plot(data[2], color='k', linewidth=1)
-    ax.plot(-(ga - 1)/0.75, 'k--', linewidth=0.75)
-    ax.set(xlim=(0, 200), ylim=(0, 2), xlabel=r"{}".format(label), ylabel=r"$Amp$")
-    ax.set(xticks=np.arange(0, 220, 20), yticks=np.arange(0, 3, 1))
-    ax.text(150, 1.2, "Freq. domain at {} MHz".format(int(round(freq[2]/1e6))), horizontalalignment="center")
-    return
+def exfourier(t, dt, nx, nf, f, ex, r_pt, i_pt):
+	r_pt[0:nf, 0:nx] = r_pt[0:nf, 0:nx] + cos(2*pi*f[0:nf]*dt*t) * ex[0:nx]
+	i_pt[0:nf, 0:nx] = i_pt[0:nf, 0:nx] - sin(2*pi*f[0:nf]*dt*t) * ex[0:nx]
 
 
-for subplot_num, plot_data in enumerate(points, start=0):
-    ax = fig.add_subplot(4, 1, subplot_num * 2 + 1)
-    plotting_ex(plot_data["ex"], gax, plot_data["num_steps"], plot_data["label_ab"])
-
-    ax = fig.add_subplot(4, 1, subplot_num * 2 + 2)
-    plotting_amp(plot_data["amp"], gax, freq, plot_data["label"])
+def pulsefourier(t, dt, nf, f, ex, r_in, i_in):
+	r_in[0:nf] = r_in[0:nf] + cos(2*pi*f[0:nf]*dt*t) * ex[10]
+	i_in[0:nf] = i_in[0:nf] - sin(2*pi*f[0:nf]*dt*t) * ex[10]
 
 
-plt.subplots_adjust(bottom=0.1, hspace=0.45)
-plt.tight_layout(); plt.savefig("fd1d_2_2.png")
+def field(t, dt, nx, nfreq, fr, freq, gax, gbx, r_pt, i_pt, r_in, i_in, dx, ex, ix, hy, bc):
+	# calculate the Hy field
+	hy[0:nx-1] = hy[0:nx-1] + 0.5 * (ex[0:nx-1] - ex[1:nx])
+	# calculate the electric flux density Dx
+	dx[1:nx] = dx[1:nx] + 0.5 * (hy[0:nx-1] - hy[1:nx])
+	# put a sinusoidal wave at the low end
+	dx[1] = dx[1] + gaussian(t, 50, 10)
+	# calculate the Ex field from Dx
+	ex[1:nx] = gax[1:nx] * (dx[1:nx] - ix[1:nx])
+	ix[1:nx] = ix[1:nx] + gbx[1:nx] * ex[1:nx]
+
+	exfourier(t, dt, nx, nfreq, fr, ex, r_pt, i_pt)
+	if t < nx//2: pulsefourier(t, dt, nfreq, freq, ex, r_in, i_in)
+
+	# absorbing boundary conditions
+	ex[0], bc[0], bc[1] = bc[0], bc[1], ex[1]
+	ex[nx-1], bc[3], bc[2] = bc[3], bc[2], ex[nx-2]
+
+
+def dielectric(nx: int, epsr: float = 1, sigma: float = 0.04, ddx: float = 0.01):
+	gax = np.ones(nx, dtype=np.float64)
+	gbx = np.zeros(nx, dtype=np.float64)
+	dt: float = ddx/6e8  # time step
+	eps0: float = 8.854e-12  # vacuum permittivity (F/m)
+	gax[nx//2:] = 1/(epsr + (sigma * dt/eps0))
+	gbx[nx//2:] = sigma * dt/eps0
+	return gax, gbx
+
+
+def main():
+
+	nx: int = 200
+	ns: int = 400
+
+	dx = np.zeros(nx, dtype=np.float64)
+	ex = np.zeros(nx, dtype=np.float64)
+	ix = np.zeros(nx, dtype=np.float64)
+	hy = np.zeros(nx, dtype=np.float64)
+
+	bc = np.zeros(4, dtype=np.float64)
+
+	ddx: float = 0.01  # cell size (m)
+	epsr: float = 4  # relative permittivity
+	sigma: float = 0  # conductivity (S/m)
+	gax, gbx = dielectric(nx, epsr, sigma, ddx)
+
+	dt = ddx/6e8
+
+	freq = np.array((100e6, 200e6, 500e6))  # frequency 100 MHz, 200 MHz, 500 MHz
+	nfreq = len(freq)  # number of frequencies
+	fr = freq.reshape(nfreq, 1)
+
+	r_pt = np.zeros((nfreq, nx), dtype=np.float64)
+	i_pt = np.zeros((nfreq, nx), dtype=np.float64)
+	a_pt = np.zeros((nfreq, nx), dtype=np.float64)
+	p_pt = np.zeros((nfreq, nx), dtype=np.float64)
+
+	r_in = np.zeros(nfreq, dtype=np.float64)
+	i_in = np.zeros(nfreq, dtype=np.float64)
+	a_in = np.zeros(nfreq, dtype=np.float64)
+	p_in = np.zeros(nfreq, dtype=np.float64)
+
+	for t in range(1, ns+1):
+		field(t, dt, nx, nfreq, fr, freq, gax, gbx, r_pt, i_pt, r_in, i_in, dx, ex, ix, hy, bc)
+
+	print(ex)
+
+if __name__ == "__main__":
+	main()
