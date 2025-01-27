8a5f6d94c36170b8560f8bdfc360b190c8861503
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-01-27 19:32:10 +0530

    implements the discrete fourier transform with a gaussian pulse as its source

--- fd1d/animation/fd1d_2_2.py
+++ fd1d/animation/fd1d_2_2.py
@@ -4,130 +4,131 @@
 # Date: 26/01/2022
 
 """ Simulation of a pulse striking a dielectric medium and implements
-    the discrete Fourier transform with a Gaussian pulse as its source """
+the discrete Fourier transform with a Gaussian pulse as its source """
 
-import matplotlib
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import pi, exp, sqrt, sin, cos, atan2
 
-matplotlib.rcParams["text.usetex"] = True
-matplotlib.rcParams["pgf.texsystem"] = "pdflatex"
-matplotlib.rcParams.update(
-    {
-        "font.family": "serif",
-        "font.size": 8,
-        "axes.labelsize": 10,
-        "axes.titlesize": 10,
-        "figure.titlesize": 10,
-    }
-)
-
-ke = 201
-ex = np.zeros(ke)
-ix = np.zeros(ke)
-Dx = np.zeros(ke)
-hy = np.zeros(ke)
-
-dx = 0.01   # cell size
-dt = dx / 6e8   # time step size
-freq = np.array((500e6, 200e6, 100e6))  # frequency 500 MHs, 200 MHz, 100 MHz
-nfreq = len(freq)   # number of frequencies
-
-boundary_low = [0, 0]
-boundary_high = [0, 0]
-
-# dielectric profile
-epsz = 8.854e-12    # vaccum permittivity (F/m)
-epsr = 4    # relative permittivity
-sigma = 0   # conductivity (S/m)
-
-gax = np.ones(ke)
-gbx = np.zeros(ke)
-gax[100:] = 1/(epsr + (sigma * dt/epsz))
-gbx[100:] = sigma * dt/epsz
-
-# used in the fourier transform
-real_in = np.zeros(nfreq)
-imag_in = np.zeros(nfreq)
-
-amp_in = np.zeros(nfreq)
-phase_in = np.zeros(nfreq)
-
-real_pt = np.zeros((nfreq, ke))
-imag_pt = np.zeros((nfreq, ke))
-
-amp = np.zeros((nfreq, ke))
-phase = np.zeros((nfreq, ke))
-
-# gaussian pulse
-t0 = 50
-spread = 10
-nsteps = 400
-
-# FDTD loop
-for time_step in range(1, nsteps + 1):
-
-    # calculate the Dx flux
-    for k in range(1, ke):
-        Dx[k] = Dx[k] + 0.5 * (hy[k - 1] - hy[k])
-
-    # put a gaussian pulse at the low end
-    Dx[5] = Dx[5] + np.exp(-0.5 * ((t0 - time_step)/spread)**2)
-
-    # calculate the Ex field from Dx
-    for k in range(1, ke):
-        ex[k] = gax[k] * (Dx[k] - ix[k])
-        ix[k] = ix[k] + gbx[k] * ex[k]
-
-    # calculate the fourier transform of Ex
-    for k in range(ke):
-        for m in range(nfreq):
-            real_pt[m, k] = real_pt[m, k] + np.cos(2*np.pi * m * dt * time_step) * ex[k]
-            imag_pt[m, k] = imag_pt[m, k] - np.sin(2*np.pi * m * dt * time_step) * ex[k]
-
-    # fourier trasform of the input pulse
-    if time_step < 100:
-        for m in range(nfreq):
-            real_in[m] = real_in[m] + np.cos(2*np.pi * m * dt * time_step) * ex[10]
-            imag_in[m] = imag_in[m] - np.sin(2*np.pi * m * dt * time_step) * ex[10]
-
-    # absorbing boundary conditions
-    ex[0] = boundary_low.pop(0)
-    boundary_low.append(ex[1])
-    ex[ke - 1] = boundary_high.pop(0)
-    boundary_high.append(ex[ke - 2])
-
-    # calculate the Hy field
-    for k in range(ke - 1):
-        hy[k] = hy[k] + 0.5 * (ex[k] - ex[k + 1])
-
-    # calculate the amplitude and phase at each frequency
-    for m in range(nfreq):
-        amp_in[m] = np.sqrt(real_in[m]**2 + imag_in[m]**2)
-        phase_in[m] = np.arctan2(imag_pt[m], real_pt[m])
-
-        for k in range(ke):
-            amp[m,k] = (1/amp_in[m]) * np.sqrt(real_pt[m,k]**2 + imag_pt[m,k]**2)
-            phase[m,k] = np.arctan2(imag_pt[m,k], real_pt[m,k]) - phase_in[m]
+plt.style.use("classic")
+plt.style.use("../pyplot.mplstyle")
 
 
+def gaussian(t: int, t0: int, sigma: float) -> float:
+	return np.exp(-0.5 * ((t - t0)/sigma)**2)
 
 
+def fieldfourier(t, dt, nx, nf, freq, ex, r_pt, i_pt):
+	r_pt[0:nf, 0:nx] = r_pt[0:nf, 0:nx] + cos(2*pi*freq[0:nf]*dt*t) * ex[0:nx]
+	i_pt[0:nf, 0:nx] = i_pt[0:nf, 0:nx] - sin(2*pi*freq[0:nf]*dt*t) * ex[0:nx]
 
 
+def pulsefourier(t, dt, nx, nf, freq, ex, r_in, i_in):
+	r_in[0:nf] = r_in[0:nf] + cos(2*pi*freq[0:nf]*dt*t) * ex[10]
+	i_in[0:nf] = i_in[0:nf] - sin(2*pi*freq[0:nf]*dt*t) * ex[10]
 
 
+def field(t, dt, nx, nf, freq, gax, gbx, r_pt, i_pt, r_in, i_in, dx, ex, ix, hy, bc):
+	# calculate the Hy field
+	hy[0:nx-1] = hy[0:nx-1] + 0.5 * (ex[0:nx-1] - ex[1:nx])
+	# calculate the electric flux density Dx
+	dx[1:nx] = dx[1:nx] + 0.5 * (hy[0:nx-1] - hy[1:nx])
+	# put a Gaussian pulse at the low end
+	dx[1] = dx[1] + gaussian(t, 50, 10)
+	# calculate the Ex field from Dx
+	ex[1:nx] = gax[1:nx] * (dx[1:nx] - ix[1:nx])
+	ix[1:nx] = ix[1:nx] + gbx[1:nx] * ex[1:nx]
+	fieldfourier(t, dt, nx, nf, freq, ex, r_pt, i_pt)
+	if t < nx//2: pulsefourier(t, dt, nx, nf, freq, ex, r_in, i_in)
+	# absorbing boundary conditions
+	ex[0], bc[0], bc[1] = bc[0], bc[1], ex[1]
+	ex[nx-1], bc[3], bc[2] = bc[3], bc[2], ex[nx-2]
 
 
+def dielectric(nx: int, epsr: float = 1, sigma: float = 0.04, ddx: float = 0.01):
+	gax = np.ones(nx, dtype=np.float64)
+	gbx = np.zeros(nx, dtype=np.float64)
+	dt: float = ddx/6e8  # time step
+	eps0: float = 8.854e-12  # vacuum permittivity (F/m)
+	gax[nx//2:] = 1/(epsr + (sigma * dt/eps0))
+	gbx[nx//2:] = sigma * dt/eps0
+	return gax, gbx
 
 
+def main():
+
+	nx: int = 1024
+	ns: int = 1500
+
+	dx = np.zeros(nx, dtype=np.float64)
+	ex = np.zeros(nx, dtype=np.float64)
+	ix = np.zeros(nx, dtype=np.float64)
+	hy = np.zeros(nx, dtype=np.float64)
+
+	bc = np.zeros(4, dtype=np.float64)
+
+	ddx: float = 0.01  # cell size (m)
+	dt: float = ddx/6e8  # time step
+	epsr: float = 4  # relative permittivity
+	sigma: float = 0  # conductivity (S/m)
+	gax, gbx = dielectric(nx, epsr, sigma, ddx)
+
+	# frequency 100 MHz, 200 MHz, 500 MHz
+	freq = np.array((100e6, 200e6, 500e6)).reshape(-1,1)
+	nf = len(freq)	# number of frequencies
+
+	r_pt = np.zeros((nf, nx), dtype=np.float64)
+	i_pt = np.zeros((nf, nx), dtype=np.float64)
+
+	r_in = np.zeros(nf, dtype=np.float64).reshape(-1,1)
+	i_in = np.zeros(nf, dtype=np.float64).reshape(-1,1)
+
+	amplt = np.zeros((nf, nx), dtype=np.float64)
+	phase = np.zeros((nf, nx), dtype=np.float64)
+
+	# define the meta data for the movie
+	fwriter = animation.writers["ffmpeg"]
+	data = {"title": "Simulation of a sinusoidal hitting lossy dielectric"}
+	writer = fwriter(fps=15, metadata=data)
+
+	# draw an empty plot, but preset the plot x- and y- limits
+	fig, (ax1, ax2) = plt.subplots(2, sharex=False, gridspec_kw={"hspace": 0.2})
+	fig.suptitle(r"Implements the discrete Fourier transform with pulse as its source")
+	medium = (1 - gax)/(1 - gax)[-1]
+	medium[medium==0] = -1.5
+	line1, = ax1.plot(ex, "k", lw=1)
+	ax1.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
+	time_text = ax1.text(0.02, 0.90, "", transform=ax1.transAxes)
+	epsr_txt1 = ax1.text(0.80, 0.50, "", transform=ax1.transAxes)
+	cond_txt1 = ax1.text(0.80, 0.40, "", transform=ax1.transAxes)
+	ax1.set(xlim=(0, nx-1), ylim=(-1.2, 2.2), ylabel=r"$E_x$")
+	ax1.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 2.2, 1))
+	line2, = ax2.plot(amplt[2], "k", lw=1)
+	ax2.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
+	epsr_txt2 = ax2.text(0.80, 0.50, "", transform=ax2.transAxes)
+	cond_txt2 = ax2.text(0.80, 0.40, "", transform=ax2.transAxes)
+	ax2.set(xlim=(0, nx-1), ylim=(-1.2, 2.2), xlabel=r"FDTD cells", ylabel=r"$Amp$")
+	ax2.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 2.2, 1))
+
+	with writer.saving(fig, "fd1d_2_2.mp4", 300):
+		for t in range(1, ns+1):
+			field(t, dt, nx, nf, freq, gax, gbx, r_pt, i_pt, r_in, i_in, dx, ex, ix, hy, bc)
+
+			amplt = (1/sqrt(r_in**2 + i_in**2)) * sqrt(r_pt**2 + i_pt**2)
+			phase = atan2(i_pt, r_pt) - atan2(i_in, r_in)
+
+			line1.set_ydata(ex)
+			time_text.set_text(rf"T = {t}")
+			epsr_txt1.set_text(rf"$\epsilon_r$ = {epsr}")
+			cond_txt1.set_text(rf"$\sigma$ = {sigma}")
+
+			line2.set_ydata(amplt[2])
+			epsr_txt2.set_text(rf"$\epsilon_r$ = {epsr}")
+			cond_txt2.set_text(rf"$\sigma$ = {sigma}")
+
+			writer.grab_frame()
 
 
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+	main()
