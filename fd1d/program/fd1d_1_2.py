a62c1a553f595ccbf73f1c8bbee607c56b62466a
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-02-01 19:32:10 +0530

    simulation of a pulse with absorbing boundary conditions

--- fd1d/program/fd1d_1_2.py
+++ fd1d/program/fd1d_1_2.py
@@ -13,50 +13,46 @@ plt.style.use("../pyplot.mplstyle")
 
 
 def visualize(ns: int, nx: int, ex: np.ndarray, hy: np.ndarray) -> None:
-	fig, (ax1, ax2) = plt.subplots(2, sharex=False, gridspec_kw={"hspace": 0.2})
-	fig.suptitle(r"FDTD simulation of a pulse with absorbing boundary conditions")
-	ax1.plot(ex, "k", lw=1)
-	ax1.text(nx/4, 0.5, f"T = {ns}", horizontalalignment="center")
-	ax1.set(xlim=(0, nx-1), ylim=(-1.2, 1.2), ylabel=r"$E_x$")
-	ax1.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 1.2, 1))
-	ax2.plot(hy, "k", lw=1)
-	ax2.set(xlim=(0, nx-1), ylim=(-1.2, 1.2), xlabel=r"FDTD cells", ylabel=r"$H_y$")
-	ax2.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 1.2, 1))
-	plt.subplots_adjust(bottom=0.2, hspace=0.45)
-	plt.savefig("fd1d_1_2.png")
+    fig, (ax1, ax2) = plt.subplots(2, sharex=False, gridspec_kw={"hspace": 0.2})
+    fig.suptitle(r"FDTD simulation of a pulse with absorbing boundary conditions")
+    ax1.plot(ex, "k", lw=1)
+    ax1.text(nx/4, 0.5, f"T = {ns}", horizontalalignment="center")
+    ax1.set(xlim=(0, nx-1), ylim=(-1.2, 1.2), ylabel=r"$E_x$")
+    ax1.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 1.2, 1))
+    ax2.plot(hy, "k", lw=1)
+    ax2.set(xlim=(0, nx-1), ylim=(-1.2, 1.2), xlabel=r"FDTD cells", ylabel=r"$H_y$")
+    ax2.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 1.2, 1))
+    plt.subplots_adjust(bottom=0.2, hspace=0.45)
+    plt.savefig("fd1d_1_2.png")
 
 
 def gaussian(t: int, t0: int, sigma: float) -> float:
-	return np.exp(-0.5 * ((t - t0)/sigma)**2)
-
-
-def field(t: int, nx: int, ex: np.ndarray, hy: np.ndarray, bc: np.ndarray):
-	# calculate the Hy field
-	hy[0:nx-1] = hy[0:nx-1] + 0.5 * (ex[0:nx-1] - ex[1:nx])
-	# calculate the Ex field
-	ex[1:nx] = ex[1:nx] + 0.5 * (hy[0:nx-1] - hy[1:nx])
-	# put a Gaussian pulse in the middle
-	ex[nx//2] = gaussian(t, 40, 12)
-	# absorbing boundary conditions
-	ex[0], bc[0], bc[1] = bc[0], bc[1], ex[1]
-	ex[nx-1], bc[3], bc[2] = bc[3], bc[2], ex[nx-2]
+    return np.exp(-0.5 * ((t - t0)/sigma)**2)
 
 
 def main():
 
-	nx: int = 201
-	ns: int = 260
+    nx: int = 201
+    ns: int = 260
 
-	ex = np.zeros(nx, dtype=np.float64)
-	hy = np.zeros(nx, dtype=np.float64)
+    ex = np.zeros(nx, dtype=np.float64)
+    hy = np.zeros(nx, dtype=np.float64)
 
-	bc = np.zeros(4, dtype=np.float64)
+    bc = np.zeros(4, dtype=np.float64)
 
-	for t in range(1, ns+1):
-		field(t, nx, ex, hy, bc)
+    for t in np.arange(1, ns+1).astype(np.int32):
+        # calculate the Ex field
+        ex[1:nx] = ex[1:nx] + 0.5 * (hy[0:nx-1] - hy[1:nx])
+        # put a Gaussian pulse in the middle
+        ex[nx//2] = gaussian(t, 40, 12)
+        # absorbing boundary conditions
+        ex[0], bc[0], bc[1] = bc[0], bc[1], ex[1]
+        ex[nx-1], bc[3], bc[2] = bc[3], bc[2], ex[nx-2]
+        # calculate the Hy field
+        hy[0:nx-1] = hy[0:nx-1] + 0.5 * (ex[0:nx-1] - ex[1:nx])
 
-	visualize(ns, nx, ex, hy)
+    visualize(ns, nx, ex, hy)
 
 
 if __name__ == "__main__":
-	main()
+    main()
