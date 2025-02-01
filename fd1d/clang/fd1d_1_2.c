8244600c6152de6d9fd3e4de4558ea2360d23f28
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-02-01 19:55:10 +0530

    simulation of a pulse with absorbing boundary conditions

--- fd1d/clang/fd1d_1_2.c
+++ fd1d/clang/fd1d_1_2.c
@@ -10,49 +10,46 @@
 
 
 double gaussian(int t, int t0, double sigma) {
-	return exp(-0.5 * ((t - t0)/sigma) * ((t - t0)/sigma));
+    return exp(-0.5 * ((t - t0)/sigma) * ((t - t0)/sigma));
 }
 
 
-void field(int t, int nx, double *ex, double *hy, double *bc) {
-	/* calculate the Hy field */
-	for (int i = 0; i < nx - 1; i++)
-		hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
+void exfield(int t, int nx, double *ex, double *hy, double *bc) {
+    /* calculate the Ex field */
+    for (int i = 1; i < nx; i++)
+        ex[i] = ex[i] + 0.5 * (hy[i-1] - hy[i]);
+    /* put a Gaussian pulse in the middle */
+    ex[nx/2] = gaussian(t, 40, 12);
+    /* absorbing boundary conditions */
+    ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
+    ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
+}
 
-	/* calculate the Ex field */
-	for (int i = 1; i < nx; i++)
-		ex[i] = ex[i] + 0.5 * (hy[i-1] - hy[i]);
 
-	/* put a Gaussian pulse in the middle */
-	ex[nx/2] = gaussian(t, 40, 12);
-
-	/* absorbing boundary conditions */
-	ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
-	ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
+void hyfield(int nx, double *ex, double *hy) {
+    /* calculate the Hy field */
+    for (int i = 0; i < nx - 1; i++)
+        hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
 }
 
 
 int main() {
 
-	int nx = 201;
-	int ns = 260;
+    int nx = 201;
+    int ns = 260;
 
-	double *ex = (double *) calloc(nx, sizeof(*ex));
-	double *hy = (double *) calloc(nx, sizeof(*hy));
+    double *ex = (double *) calloc(nx, sizeof(*ex));
+    double *hy = (double *) calloc(nx, sizeof(*hy));
 
-	/* initialize ex and hy arrays */
-	for (size_t i = 0; i < nx; i++) {
-		ex[i] = 0.0f;
-		hy[i] = 0.0f;
-	}
+    double bc[4] = {0.0f};
 
-	double bc[4] = {0.0f};
+    for (int t = 1; t <= ns; t++) {
+        exfield(t, nx, ex, hy, bc);
+        hyfield(nx, ex, hy);
+    }
 
-	for (int t = 1; t <= ns; t++)
-		field(t, nx, ex, hy, bc);
+    free(ex);
+    free(hy);
 
-	free(ex);
-	free(hy);
-
-	return 0;
+    return 0;
 }
