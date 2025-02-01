3283537615db9900f28fdf67c99189a43d06f1da
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-02-01 19:58:10 +0530

    simulation of a pulse with absorbing boundary conditions

--- fd1d/cuda/fd1d_1_2.cu
+++ fd1d/cuda/fd1d_1_2.cu
@@ -9,34 +9,33 @@
 #include <math.h>
 #include <cuda_runtime.h>
 
+#define idx (blockIdx.x * blockDim.x + threadIdx.x)
+#define stx (blockDim.x * gridDim.x)
+
 
 __device__ float gaussian(int t, int t0, float sigma) {
     return exp(-0.5 * ((t - t0)/sigma) * ((t - t0)/sigma));
 }
 
 
-__global__ void field(int t, int nx, float *ex, float *hy, float *bc) {
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-    int stride = blockDim.x * gridDim.x;
-
-    /* calculate the Hy field */
-    for (int i = index; i < nx - 1; i += stride)
-        hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
-
-    __syncthreads();
-
+__global__ void exfield(int t, int nx, float *ex, float *hy, float *bc) {
     /* calculate the Ex field */
-    for (int i = index + 1; i < nx; i += stride)
+    for (int i = idx + 1; i < nx; i += stx)
         ex[i] = ex[i] + 0.5 * (hy[i-1] - hy[i]);
-
     __syncthreads();
-
     /* put a Gaussian pulse in the middle */
-    ex[nx/2] = gaussian(t, 40, 12);
-
+    if (idx == nx/2) ex[nx/2] = gaussian(t, 40, 12);
     /* absorbing boundary conditions */
-    ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
-    ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
+    if (idx == 0) ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
+    if (idx == nx-1) ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
+}
+
+
+__global__ void hyfield(int nx, float *ex, float *hy) {
+    /* calculate the Hy field */
+    for (int i = idx; i < nx - 1; i += stx)
+        hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
+    __syncthreads();
 }
 
 
@@ -46,13 +45,12 @@ int main() {
     int ns = 260;
 
     float *ex, *hy;
-
     /* allocate unified memory accessible from host or device */
     cudaMallocManaged(&ex, nx*sizeof(float));
     cudaMallocManaged(&hy, nx*sizeof(float));
 
     /* initialize ex and hy arrays on the host */
-    for (size_t i = 0; i < nx; i++) {
+    for (int i = 0; i < nx; i++) {
         ex[i] = 0.0f;
         hy[i] = 0.0f;
     }
@@ -61,13 +59,17 @@ int main() {
     cudaMallocManaged(&bc, 4*sizeof(float));
     bc[4] = {0.0f};
 
+    int numSM;
+    cudaDeviceGetAttribute(&numSM, cudaDevAttrMultiProcessorCount, 0);
+
     dim3 gridDim, blockDim;
-
     blockDim.x = 256;
-    gridDim.x = (nx + blockDim.x - 1)/blockDim.x;
+    gridDim.x = 32*numSM;
 
-    for (int t = 1; t <= ns; t++)
-        field<<<gridDim, blockDim>>>(t, nx, ex, hy, bc);
+    for (int t = 1; t <= ns; t++) {
+        exfield<<<gridDim, blockDim>>>(t, nx, ex, hy, bc);
+        hyfield<<<gridDim, blockDim>>>(nx, ex, hy);
+    }
 
     cudaDeviceSynchronize();
 
