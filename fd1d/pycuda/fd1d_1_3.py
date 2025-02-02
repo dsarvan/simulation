82b00b7805e9a3336f927fbda32d9dc6a4534b30
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-02-02 19:42:10 +0530

    simulation of a pulse hitting a dielectric medium

--- fd1d/pycuda/fd1d_1_3.py
+++ fd1d/pycuda/fd1d_1_3.py
@@ -17,92 +17,96 @@ plt.style.use("classic")
 plt.style.use("../pyplot.mplstyle")
 
 
-def visualize(ns: int, nx: int, epsr: int, cb: np.ndarray, ex: np.ndarray, hy: np.ndarray) -> None:
-	fig, (ax1, ax2) = plt.subplots(2, sharex=False, gridspec_kw={"hspace": 0.2})
-	fig.suptitle(r"FDTD simulation of a pulse striking dielectric material")
-	medium = (0.5/cb - 1)/(epsr - 1) if epsr > 1 else (0.5/cb - 1)
-	medium[medium==0] = -1.5
-	ax1.plot(ex, "k", lw=1)
-	ax1.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
-	ax1.text(nx/4, 0.5, f"T = {ns}", horizontalalignment="center")
-	ax1.text(3*nx/4, 0.5, f"epsr = {epsr}", horizontalalignment="center")
-	ax1.set(xlim=(0, nx-1), ylim=(-1.2, 1.2), ylabel=r"$E_x$")
-	ax1.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 1.2, 1))
-	ax2.plot(hy, "k", lw=1)
-	ax2.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
-	ax2.set(xlim=(0, nx-1), ylim=(-1.2, 1.2), xlabel=r"FDTD cells", ylabel=r"$H_y$")
-	ax2.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 1.2, 1))
-	plt.subplots_adjust(bottom=0.2, hspace=0.45)
-	plt.show()
+def visualize(ns: int, nx: int, epsr: float, cb: np.ndarray, ex: np.ndarray, hy: np.ndarray) -> None:
+    fig, (ax1, ax2) = plt.subplots(2, sharex=False, gridspec_kw={"hspace": 0.2})
+    fig.suptitle(r"FDTD simulation of a pulse striking dielectric material")
+    medium = (0.5/cb - 1)/(epsr - 1) if epsr > 1 else (0.5/cb - 1)
+    medium[medium==0] = -1e3
+    ax1.plot(ex, "k", lw=1)
+    ax1.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
+    ax1.text(nx/4, 0.5, f"T = {ns}", horizontalalignment="center")
+    ax1.text(3*nx/4, 0.5, f"epsr = {epsr}", horizontalalignment="center")
+    ax1.set(xlim=(0, nx-1), ylim=(-1.2, 1.2), ylabel=r"$E_x$")
+    ax1.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 1.2, 1))
+    ax2.plot(hy, "k", lw=1)
+    ax2.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
+    ax2.set(xlim=(0, nx-1), ylim=(-1.2, 1.2), xlabel=r"FDTD cells", ylabel=r"$H_y$")
+    ax2.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 1.2, 1))
+    plt.subplots_adjust(bottom=0.2, hspace=0.45)
+    plt.show()
 
 
 kernel = """
+#define idx (blockIdx.x * blockDim.x + threadIdx.x)
+#define stx (blockDim.x * gridDim.x)
+
+
 __device__ float gaussian(int t, int t0, float sigma) {
-	return exp(-0.5 * ((t - t0)/sigma) * ((t - t0)/sigma));
+    return exp(-0.5 * ((t - t0)/sigma) * ((t - t0)/sigma));
 }
 
 
-__global__ void field(int t, int nx, float *cb, float *ex, float *hy, float *bc) {
-	int index = blockIdx.x * blockDim.x + threadIdx.x;
-	int stride = blockDim.x * gridDim.x;
+__global__ void exfield(int t, int nx, float *cb, float *ex, float *hy, float *bc) {
+    /* calculate the Ex field */
+    for (int i = idx + 1; i < nx; i += stx)
+        ex[i] = ex[i] + cb[i] * (hy[i-1] - hy[i]);
+    __syncthreads();
+    /* put a Gaussian pulse at the low end */
+    if (idx == 1) ex[1] = ex[1] + gaussian(t, 40, 12);
+    /* absorbing boundary conditions */
+    if (idx == 0) ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
+    if (idx == nx-1) ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
+}
 
-	/* calculate the Hy field */
-	for (int i = index; i < nx - 1; i += stride)
-		hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
 
-	__syncthreads();
-
-	/* calculate the Ex field */
-	for (int i = index + 1; i < nx; i += stride)
-		ex[i] = ex[i] + cb[i] * (hy[i-1] - hy[i]);
-
-	__syncthreads();
-
-	/* put a Gaussian pulse at the low end */
-	ex[1] = ex[1] + gaussian(t, 40, 12);
-
-	/* absorbing boundary conditions */
-	ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
-	ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
+__global__ void hyfield(int nx, float *ex, float *hy) {
+    /* calculate the Hy field */
+    for (int i = idx; i < nx - 1; i += stx)
+        hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
+    __syncthreads();
 }
 """
 
 
-def dielectric(nx: int, epsr: float = 1) -> np.ndarray:
-	cb = 0.5 * np.ones(nx, dtype=np.float32)
-	cb[nx//2:] = 0.5/epsr
-	return cb
+def dielectric(nx: int, epsr: float) -> np.ndarray:
+    cb = 0.5 * gpuarray.ones(nx, dtype=np.float32)
+    cb[nx//2:] = 0.5/epsr
+    return cb
 
 
 def main():
 
-	nx = np.int32(201)
-	ns = np.int32(320)
+    nx = np.int32(201)
+    ns = np.int32(320)
 
-	ex = gpuarray.to_gpu(np.zeros(nx, dtype=np.float32))
-	hy = gpuarray.to_gpu(np.zeros(nx, dtype=np.float32))
+    ex = gpuarray.zeros(nx, dtype=np.float32)
+    hy = gpuarray.zeros(nx, dtype=np.float32)
 
-	bc = gpuarray.to_gpu(np.zeros(4, dtype=np.float32))
+    bc = gpuarray.zeros(4, dtype=np.float32)
 
-	epsr: float = 4 # relative permittivity
-	cb: np.ndarray = gpuarray.to_gpu(dielectric(nx, epsr))
+    epsr: float = 4 # relative permittivity
+    cb: np.ndarray = dielectric(nx, epsr)
 
-	blockDimx = 256
-	gridDimx = int((nx + blockDimx - 1)/blockDimx)
+    numSM: int = drv.Device(0).multiprocessor_count
 
-	mod = SourceModule(kernel)
-	field = mod.get_function("field")
+    blockDimx: int = 256
+    gridDimx: int = 32*numSM
 
-	gridDim = (gridDimx,1)
-	blockDim = (blockDimx,1,1)
+    gridDim = (gridDimx,1)
+    blockDim = (blockDimx,1,1)
 
-	for t in range(1, ns+1):
-		field(np.int32(t), nx, cb, ex, hy, bc, grid=gridDim, block=blockDim)
+    mod = SourceModule(kernel)
+    exfield = mod.get_function("exfield")
+    hyfield = mod.get_function("hyfield")
 
-	drv.Context.synchronize()
+    for t in np.arange(1, ns+1).astype(np.int32):
+        exfield(t, nx, cb, ex, hy, bc, grid=gridDim, block=blockDim)
+        hyfield(nx, ex, hy, grid=gridDim, block=blockDim)
 
-	visualize(ns, nx, epsr, cb.get(), ex.get(), hy.get())
+    drv.Context.synchronize()
+
+    visualize(ns, nx, epsr, cb.get(), ex.get(), hy.get())
 
 
 if __name__ == "__main__":
-	main()
+    main()
