a1fea8e9016e5ad054ea3bd287d4effa02683336
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-02-06 19:42:16 +0530

    simulation of a pulse with absorbing boundary conditions

--- fd1d/clang/fd1d_1_2.c
+++ fd1d/clang/fd1d_1_2.c
@@ -14,19 +14,19 @@ double gaussian(int t, int t0, double sigma) {
 }
 
 
-void exfield(int t, int nx, double *ex, double *hy, double *bc) {
+void exfield(int t, int nx, double *ex, double *hy) {
     /* calculate the Ex field */
     for (int i = 1; i < nx; i++)
         ex[i] = ex[i] + 0.5 * (hy[i-1] - hy[i]);
     /* put a Gaussian pulse in the middle */
     ex[nx/2] = gaussian(t, 40, 12);
-    /* absorbing boundary conditions */
-    ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
-    ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
 }
 
 
-void hyfield(int nx, double *ex, double *hy) {
+void hyfield(int nx, double *ex, double *hy, double *bc) {
+    /* absorbing boundary conditions */
+    ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
+    ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
     /* calculate the Hy field */
     for (int i = 0; i < nx - 1; i++)
         hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
@@ -35,8 +35,8 @@ void hyfield(int nx, double *ex, double *hy) {
 
 int main() {
 
-    int nx = 201;
-    int ns = 260;
+    int nx = 512;  /* number of grid points */
+    int ns = 570;  /* number of time steps */
 
     double *ex = (double *) calloc(nx, sizeof(*ex));
     double *hy = (double *) calloc(nx, sizeof(*hy));
@@ -44,8 +44,8 @@ int main() {
     double bc[4] = {0.0f};
 
     for (int t = 1; t <= ns; t++) {
-        exfield(t, nx, ex, hy, bc);
-        hyfield(nx, ex, hy);
+        exfield(t, nx, ex, hy);
+        hyfield(nx, ex, hy, bc);
     }
 
     free(ex);
