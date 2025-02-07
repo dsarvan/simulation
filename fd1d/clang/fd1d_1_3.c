fc7cfd0b1d447cad7df050ec89632021a0c46df3
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-02-07 19:41:16 +0530

    simulation of a pulse hitting a dielectric medium

--- fd1d/clang/fd1d_1_3.c
+++ fd1d/clang/fd1d_1_3.c
@@ -14,19 +14,19 @@ double gaussian(int t, int t0, double sigma) {
 }
 
 
-void exfield(int t, int nx, double *cb, double *ex, double *hy, double *bc) {
+void exfield(int t, int nx, double *cb, double *ex, double *hy) {
     /* calculate the Ex field */
     for (int i = 1; i < nx; i++)
         ex[i] = ex[i] + cb[i] * (hy[i-1] - hy[i]);
     /* put a Gaussian pulse at the low end */
     ex[1] = ex[1] + gaussian(t, 40, 12);
-    /* absorbing boundary conditions */
-    ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
-    ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
 }
 
 
-void hyfield(int nx, double *ex, double *hy) {
+void hyfield(int nx, double *ex, double *hy, double *bc) {
+    /* absorbing boundary conditions */
+    ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
+    ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
     /* calculate the Hy field */
     for (int i = 0; i < nx - 1; i++)
         hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
@@ -43,20 +43,20 @@ double *dielectric(int nx, double epsr) {
 
 int main() {
 
-    int nx = 201;
-    int ns = 320;
+    int nx = 512;  /* number of grid points */
+    int ns = 740;  /* number of time steps */
 
     double *ex = (double *) calloc(nx, sizeof(*ex));
     double *hy = (double *) calloc(nx, sizeof(*hy));
 
     double bc[4] = {0.0f};
 
-    double epsr = 4; /* relative permittivity */
+    double epsr = 4;  /* relative permittivity */
     double *cb = dielectric(nx, epsr);
 
     for (int t = 1; t <= ns; t++) {
-        exfield(t, nx, cb, ex, hy, bc);
-        hyfield(nx, ex, hy);
+        exfield(t, nx, cb, ex, hy);
+        hyfield(nx, ex, hy, bc);
     }
 
     free(cb);
