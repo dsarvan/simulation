6a49166424746d25de72c0dc803ad9579cd67a2a
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-02-09 19:32:10 +0530

    simulation of a propagating sinusoidal striking a lossy dielectric medium

--- fd1d/program/fd1d_1_5.py
+++ fd1d/program/fd1d_1_5.py
@@ -13,28 +13,25 @@ plt.style.use("classic")
 plt.style.use("../pyplot.mplstyle")
 
 
-def visualize(ns: int, nx: int, epsr: float, sigma: float, cb: np.ndarray, ex: np.ndarray, hy: np.ndarray) -> None:
-    fig, (ax1, ax2) = plt.subplots(2, sharex=False, gridspec_kw={"hspace": 0.2})
+def visualize(ns: int, nx: int, epsr: float, sigma: float, cb: np.ndarray, ex: np.ndarray) -> None:
+    fig, ax = plt.subplots(figsize=(8,3), gridspec_kw={"hspace": 0.2})
     fig.suptitle(r"FDTD simulation of a sinusoidal striking lossy dielectric material")
-    medium = (0.5/cb - 1)/(epsr - 1) if epsr > 1 else (0.5/cb - 1)
+    medium = (0.5/cb - 1)/(epsr - 1)*1e3 if epsr > 1 else (0.5/cb - 1)
     medium[medium==0] = -1e3
-    ax1.plot(ex, "k", lw=1)
-    ax1.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
-    ax1.text(nx/4, 0.5, f"T = {ns}", horizontalalignment="center")
-    ax1.text(3*nx/4, 0.5, f"epsr = {epsr}", horizontalalignment="center")
-    ax1.text(3*nx/4, -0.5, rf"$\sigma$ = {sigma}", horizontalalignment="center")
-    ax1.set(xlim=(0, nx-1), ylim=(-1.2, 1.2), ylabel=r"$E_x$")
-    ax1.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 1.2, 1))
-    ax2.plot(hy, "k", lw=1)
-    ax2.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
-    ax2.set(xlim=(0, nx-1), ylim=(-1.2, 1.2), xlabel=r"FDTD cells", ylabel=r"$H_y$")
-    ax2.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 1.2, 1))
+    ax.plot(ex, color="black", linewidth=1)
+    ax.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
+    ax.set(xlim=(0, nx-1), ylim=(-1.2, 1.2))
+    ax.set(xticks=range(0, nx+1, round(nx//10,-1)))
+    ax.set(xlabel=r"$z\;(cm)$", ylabel=r"$E_x\;(V/m)$")
+    ax.text(0.02, 0.90, rf"$T$ = {ns}", transform=ax.transAxes)
+    ax.text(0.90, 0.90, rf"$\epsilon_r$ = {epsr}", transform=ax.transAxes)
+    ax.text(0.85, 0.80, rf"$\sigma$ = {sigma} $S/m$", transform=ax.transAxes)
     plt.subplots_adjust(bottom=0.2, hspace=0.45)
-    plt.savefig("fd1d_1_5.png")
+    plt.savefig("fd1d_1_5.png", dpi=100)
 
 
-def sinusoidal(t: int, ddx: float, freq: float) -> float:
-    dt: float = ddx/6e8  # time step
+def sinusoidal(t: int, ds: float, freq: float) -> float:
+    dt: float = ds/6e8  # time step (s)
     return np.sin(2 * np.pi * freq * dt * t)
 
 
@@ -50,16 +47,16 @@ def dielectric(nx: int, dt: float, epsr: float, sigma: float) -> tuple:
 
 def main():
 
-    nx: int = 1024
-    ns: int = 1500
+    nx: int = 512  # number of grid points
+    ns: int = 740  # number of time steps
 
     ex = np.zeros(nx, dtype=np.float64)
     hy = np.zeros(nx, dtype=np.float64)
 
     bc = np.zeros(4, dtype=np.float64)
 
-    ddx: float = 0.01  # cell size (m)
-    dt: float = ddx/6e8  # time step
+    ds: float = 0.01  # spatial step (m)
+    dt: float = ds/6e8  # time step (s)
     epsr: float = 4  # relative permittivity
     sigma: float = 0.04  # conductivity (S/m)
     ca, cb = dielectric(nx, dt, epsr, sigma)
@@ -75,7 +72,7 @@ def main():
         # calculate the Hy field
         hy[0:nx-1] = hy[0:nx-1] + 0.5 * (ex[0:nx-1] - ex[1:nx])
 
-    visualize(ns, nx, epsr, sigma, cb, ex, hy)
+    visualize(ns, nx, epsr, sigma, cb, ex)
 
 
 if __name__ == "__main__":
