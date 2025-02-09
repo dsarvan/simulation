dc292775de51baa75a5dc011cbde8740d1393da9
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-02-09 19:44:16 +0530

    simulation of a propagating sinusoidal striking a lossy dielectric medium

--- fd1d/clang/fd1d_1_5.c
+++ fd1d/clang/fd1d_1_5.c
@@ -16,25 +16,25 @@ typedef struct {
 } tuple;
 
 
-double sinusoidal(int t, double ddx, double freq) {
-    double dt = ddx/6e8;  /* time step */
+double sinusoidal(int t, double ds, double freq) {
+    double dt = ds/6e8;  /* time step (s) */
     return sin(2 * M_PI * freq * dt * t);
 }
 
 
-void exfield(int t, int nx, double *ca, double *cb, double *ex, double *hy, double *bc) {
+void exfield(int t, int nx, double *ca, double *cb, double *ex, double *hy) {
     /* calculate the Ex field */
     for (int i = 1; i < nx; i++)
         ex[i] = ca[i] * ex[i] + cb[i] * (hy[i-1] - hy[i]);
     /* put a sinusoidal wave at the low end */
     ex[1] = ex[1] + sinusoidal(t, 0.01, 700e6);
-    /* absorbing boundary conditions */
-    ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
-    ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
 }
 
 
-void hyfield(int nx, double *ex, double *hy) {
+void hyfield(int nx, double *ex, double *hy, double *bc) {
+    /* absorbing boundary conditions */
+    ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
+    ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
     /* calculate the Hy field */
     for (int i = 0; i < nx - 1; i++)
         hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
@@ -57,23 +57,23 @@ tuple dielectric(int nx, double dt, double epsr, double sigma) {
 
 int main() {
 
-    int nx = 1024;
-    int ns = 1500;
+    int nx = 512;  /* number of grid points */
+    int ns = 740;  /* number of time steps */
 
     double *ex = (double *) calloc(nx, sizeof(*ex));
     double *hy = (double *) calloc(nx, sizeof(*hy));
 
     double bc[4] = {0.0f};
 
-    double ddx = 0.01;  /* cell size (m) */
-    double dt = ddx/6e8;  /* time step */
+    double ds = 0.01;  /* spatial step (m) */
+    double dt = ds/6e8;  /* time step (s) */
     double epsr = 4;  /* relative permittivity */
     double sigma = 0.04;  /* conductivity (S/m) */
     tuple n = dielectric(nx, dt, epsr, sigma);
 
     for (int t = 1; t <= ns; t++) {
-        exfield(t, nx, n.ca, n.cb, ex, hy, bc);
-        hyfield(nx, ex, hy);
+        exfield(t, nx, n.ca, n.cb, ex, hy);
+        hyfield(nx, ex, hy, bc);
     }
 
     free(n.ca);
