94277b89052b98461529e7e228ddae019c91fc79
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-02-13 19:34:12 +0530

    simulation of a propagating sinusoidal striking a lossy dielectric medium

--- fd1d/animation/fd1d_2_1.py
+++ fd1d/animation/fd1d_2_1.py
@@ -9,9 +9,15 @@ dielectric with a dielectric constant of 4 and conductivity of 0.04 (S/m) """
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import namedtuple
 
 plt.style.use("classic")
 plt.style.use("../pyplot.mplstyle")
+
+
+medium = namedtuple('medium', (
+    'nax', 'nbx',
+))
 
 
 def sinusoidal(t: int, ds: float, freq: float) -> float:
@@ -26,10 +32,10 @@ def dxfield(t: int, nx: int, dx: np.ndarray, hy: np.ndarray) -> None:
     dx[1] = dx[1] + sinusoidal(t, 0.01, 700e6)
 
 
-def exfield(nx: int, gax: np.ndarray, gbx: np.ndarray, dx: np.ndarray, ix: np.ndarray, ex: np.ndarray) -> None:
+def exfield(nx: int, md: medium, dx: np.ndarray, ix: np.ndarray, ex: np.ndarray) -> None:
     # calculate the Ex field from Dx
-    ex[1:nx] = gax[1:nx] * (dx[1:nx] - ix[1:nx])
-    ix[1:nx] = ix[1:nx] + gbx[1:nx] * ex[1:nx]
+    ex[1:nx] = md.nax[1:nx] * (dx[1:nx] - ix[1:nx])
+    ix[1:nx] = ix[1:nx] + md.nbx[1:nx] * ex[1:nx]
 
 
 def hyfield(nx: int, ex: np.ndarray, hy: np.ndarray, bc: np.ndarray) -> None:
@@ -40,13 +46,15 @@ def hyfield(nx: int, ex: np.ndarray, hy: np.ndarray, bc: np.ndarray) -> None:
     hy[0:nx-1] = hy[0:nx-1] + 0.5 * (ex[0:nx-1] - ex[1:nx])
 
 
-def dielectric(nx: int, dt: float, epsr: float, sigma: float) -> tuple:
-    gax = np.ones(nx, dtype=np.float64)
-    gbx = np.zeros(nx, dtype=np.float64)
+def dielectric(nx: int, dt: float, epsr: float, sigma: float) -> medium:
+    md = medium(
+        nax = np.ones(nx, dtype=np.float64),
+        nbx = np.zeros(nx, dtype=np.float64),
+    )
     eps0: float = 8.854e-12  # vacuum permittivity (F/m)
-    gax[nx//2:] = 1/(epsr + (sigma * dt/eps0))
-    gbx[nx//2:] = sigma * dt/eps0
-    return gax, gbx
+    md.nax[nx//2:] = 1/(epsr + (sigma * dt/eps0))
+    md.nbx[nx//2:] = sigma * dt/eps0
+    return md
 
 
 def main():
@@ -65,7 +73,7 @@ def main():
     dt: float = ds/6e8  # time step (s)
     epsr: float = 4  # relative permittivity
     sigma: float = 0.04  # conductivity (S/m)
-    gax, gbx = dielectric(nx, dt, epsr, sigma)
+    md: medium = dielectric(nx, dt, epsr, sigma)
 
     # define the meta data for the movie
     fwriter = animation.writers["ffmpeg"]
@@ -75,7 +83,7 @@ def main():
     # draw an empty plot, but preset the plot x- and y- limits
     fig, ax = plt.subplots(figsize=(8,3), gridspec_kw={"hspace": 0.2})
     fig.suptitle(r"FDTD simulation of a sinusoidal striking lossy dielectric material")
-    medium = (1 - gax)/(1 - gax[-1])*1e3 if epsr > 1 else (1 - gax)
+    medium = (1 - md.nax)/(1 - md.nax[-1])*1e3 if epsr > 1 else (1 - md.nax)
     medium[medium==0] = -1e3
     axline, = ax.plot(ex, color="black", linewidth=1)
     ax.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
@@ -90,7 +98,7 @@ def main():
     with writer.saving(fig, "fd1d_2_1.mp4", 300):
         for t in np.arange(1, ns+1).astype(np.int32):
             dxfield(t, nx, dx, hy)
-            exfield(nx, gax, gbx, dx, ix, ex)
+            exfield(nx, md, dx, ix, ex)
             hyfield(nx, ex, hy, bc)
 
             axline.set_ydata(ex)
