238a92a027cf9f75e1dc8b97884eee0e51b6c7d5
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-02-13 19:38:16 +0530

    simulation of a propagating sinusoidal striking a lossy dielectric medium

--- fd1d/clang/fd1d_2_1.c
+++ fd1d/clang/fd1d_2_1.c
@@ -11,9 +11,8 @@
 
 
 typedef struct {
-    double *gax;
-    double *gbx;
-} tuple;
+    double *nax, *nbx;
+} medium;
 
 
 double sinusoidal(int t, double ds, double freq) {
@@ -31,11 +30,11 @@ void dxfield(int t, int nx, double *dx, double *hy) {
 }
 
 
-void exfield(int nx, double *gax, double *gbx, double *dx, double *ix, double *ex) {
+void exfield(int nx, medium *md, double *dx, double *ix, double *ex) {
     /* calculate the Ex field from Dx */
     for (int i = 1; i < nx; i++) {
-        ex[i] = gax[i] * (dx[i] - ix[i]);
-        ix[i] = ix[i] + gbx[i] * ex[i];
+        ex[i] = md->nax[i] * (dx[i] - ix[i]);
+        ix[i] = ix[i] + md->nbx[i] * ex[i];
     }
 }
 
@@ -50,17 +49,17 @@ void hyfield(int nx, double *ex, double *hy, double *bc) {
 }
 
 
-tuple dielectric(int nx, double dt, double epsr, double sigma) {
-    tuple n;
-    n.gax = (double *) calloc(nx, sizeof(n.gax));
-    n.gbx = (double *) calloc(nx, sizeof(n.gbx));
-    for (int i = 0; i < nx; n.gax[i] = 1.0f, i++);
+medium dielectric(int nx, double dt, double epsr, double sigma) {
+    medium md;
+    md.nax = (double *) calloc(nx, sizeof(*md.nax));
+    md.nbx = (double *) calloc(nx, sizeof(*md.nbx));
+    for (int i = 0; i < nx; md.nax[i] = 1.0f, i++);
     double eps0 = 8.854e-12;  /* vacuum permittivity (F/m) */
     for (int i = nx/2; i < nx; i++) {
-        n.gax[i] = 1/(epsr + (sigma * dt/eps0));
-        n.gbx[i] = sigma * dt/eps0;
+        md.nax[i] = 1/(epsr + (sigma * dt/eps0));
+        md.nbx[i] = sigma * dt/eps0;
     }
-    return n;
+    return md;
 }
 
 
@@ -80,16 +79,16 @@ int main() {
     double dt = ds/6e8;  /* time step (s) */
     double epsr = 4;  /* relative permittivity */
     double sigma = 0.04;  /* conductivity (S/m) */
-    tuple n = dielectric(nx, dt, epsr, sigma);
+    medium md = dielectric(nx, dt, epsr, sigma);
 
     for (int t = 1; t <= ns; t++) {
         dxfield(t, nx, dx, hy);
-        exfield(nx, n.gax, n.gbx, dx, ix, ex);
+        exfield(nx, &md, dx, ix, ex);
         hyfield(nx, ex, hy, bc);
     }
 
-    free(n.gax);
-    free(n.gbx);
+    free(md.nax);
+    free(md.nbx);
     free(dx);
     free(ex);
     free(ix);
