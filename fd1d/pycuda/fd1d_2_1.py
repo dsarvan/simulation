59f1b922eecb74488f2b9f041957a38a1727edc7
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-02-13 19:36:14 +0530

    simulation of a propagating sinusoidal striking a lossy dielectric medium

--- fd1d/pycuda/fd1d_2_1.py
+++ fd1d/pycuda/fd1d_2_1.py
@@ -8,6 +8,7 @@ dielectric with a dielectric constant of 4 and conductivity of 0.04 (S/m) """
 
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import namedtuple
 
 import pycuda.autoinit
 import pycuda.driver as drv
@@ -18,10 +19,10 @@ plt.style.use("classic")
 plt.style.use("../pyplot.mplstyle")
 
 
-def visualize(ns: int, nx: int, epsr: float, sigma: float, gax: np.ndarray, ex: np.ndarray) -> None:
+def visualize(ns: int, nx: int, epsr: float, sigma: float, nax: np.ndarray, ex: np.ndarray) -> None:
     fig, ax = plt.subplots(figsize=(8,3), gridspec_kw={"hspace": 0.2})
     fig.suptitle(r"FDTD simulation of a sinusoidal striking lossy dielectric material")
-    medium = (1 - gax)/(1 - gax[-1])*1e3 if epsr > 1 else (1 - gax)
+    medium = (1 - nax)/(1 - nax[-1])*1e3 if epsr > 1 else (1 - nax)
     medium[medium==0] = -1e3
     ax.plot(ex, color="black", linewidth=1)
     ax.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
@@ -35,9 +36,19 @@ def visualize(ns: int, nx: int, epsr: float, sigma: float, gax: np.ndarray, ex: 
     plt.show()
 
 
+medium = namedtuple('medium', (
+    'nax', 'nbx',
+))
+
+
 kernel = """
 #define idx (blockIdx.x * blockDim.x + threadIdx.x)
 #define stx (blockDim.x * gridDim.x)
+
+
+typedef struct {
+    float *nax, *nbx;
+} medium;
 
 
 __device__
@@ -52,20 +63,18 @@ void dxfield(int t, int nx, float *dx, float *hy) {
     /* calculate the electric flux density Dx */
     for (int i = idx + 1; i < nx; i += stx)
         dx[i] = dx[i] + 0.5 * (hy[i-1] - hy[i]);
-    __syncthreads();
     /* put a sinusoidal wave at the low end */
     if (idx == 1) dx[1] = dx[1] + sinusoidal(t, 0.01, 700e6);
 }
 
 
 __global__
-void exfield(int nx, float *gax, float *gbx, float *dx, float *ix, float *ex) {
+void exfield(int nx, medium *md, float *dx, float *ix, float *ex) {
     /* calculate the Ex field from Dx */
     for (int i = idx + 1; i < nx; i += stx) {
-        ex[i] = gax[i] * (dx[i] - ix[i]);
-        ix[i] = ix[i] + gbx[i] * ex[i];
+        ex[i] = md->nax[i] * (dx[i] - ix[i]);
+        ix[i] = ix[i] + md->nbx[i] * ex[i];
     }
-    __syncthreads();
 }
 
 
@@ -77,18 +86,19 @@ void hyfield(int nx, float *ex, float *hy, float *bc) {
     /* calculate the Hy field */
     for (int i = idx; i < nx - 1; i += stx)
         hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
-    __syncthreads();
 }
 """
 
 
-def dielectric(nx: int, dt: float, epsr: float, sigma: float) -> tuple:
-    gax = gpuarray.ones(nx, dtype=np.float32)
-    gbx = gpuarray.zeros(nx, dtype=np.float32)
+def dielectric(nx: int, dt: float, epsr: float, sigma: float) -> medium:
+    md = medium(
+        nax = gpuarray.ones(nx, dtype=np.float32),
+        nbx = gpuarray.zeros(nx, dtype=np.float32),
+    )
     eps0: float = 8.854e-12  # vacuum permittivity (F/m)
-    gax[nx//2:] = 1/(epsr + (sigma * dt/eps0))
-    gbx[nx//2:] = sigma * dt/eps0
-    return gax, gbx
+    md.nax[nx//2:] = 1/(epsr + (sigma * dt/eps0))
+    md.nbx[nx//2:] = sigma * dt/eps0
+    return md
 
 
 def main():
@@ -103,11 +113,15 @@ def main():
 
     bc = gpuarray.zeros(4, dtype=np.float32)
 
-    ds: float = 0.01  # spatial step (m)
-    dt: float = ds/6e8  # time step (s)
+    ds = np.float32(0.01)  # spatial step (m)
+    dt = np.float32(ds/6e8)  # time step (s)
     epsr: float = 4  # relative permittivity
     sigma: float = 0.04  # conductivity (S/m)
-    gax, gbx = dielectric(nx, dt, epsr, sigma)
+    md: medium = dielectric(nx, dt, epsr, sigma)
+
+    mdptr = gpuarray.to_gpu(np.array([
+        md.nax.ptr, md.nbx.ptr,
+    ], dtype=np.uint64)).gpudata
 
     numSM: int = drv.Device(0).multiprocessor_count
 
@@ -124,12 +138,12 @@ def main():
 
     for t in np.arange(1, ns+1).astype(np.int32):
         dxfield(t, nx, dx, hy, grid=gridDim, block=blockDim)
-        exfield(nx, gax, gbx, dx, ix, ex, grid=gridDim, block=blockDim)
+        exfield(nx, mdptr, dx, ix, ex, grid=gridDim, block=blockDim)
         hyfield(nx, ex, hy, bc, grid=gridDim, block=blockDim)
 
     drv.Context.synchronize()
 
-    visualize(ns, nx, epsr, sigma, gax.get(), ex.get())
+    visualize(ns, nx, epsr, sigma, md.nax.get(), ex.get())
 
 
 if __name__ == "__main__":
