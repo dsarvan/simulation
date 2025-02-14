346658a4978e775bcc015c7fbc8bcc9cfac3cdef
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-02-14 19:36:14 +0530

    implements the discrete fourier transform with a gaussian pulse as its source

--- fd1d/pycuda/fd1d_2_2.py
+++ fd1d/pycuda/fd1d_2_2.py
@@ -8,7 +8,8 @@ the discrete Fourier transform with a Gaussian pulse as its source """
 
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy import sqrt, arctan2
+from numpy import hypot, arctan2
+from collections import namedtuple
 
 import pycuda.autoinit
 import pycuda.driver as drv
@@ -19,164 +20,205 @@ plt.style.use("classic")
 plt.style.use("../pyplot.mplstyle")
 
 
-def visualize(ns: int, nx: int, epsr: float, sigma: float, gax: np.ndarray, ex: np.ndarray, amp: np.ndarray) -> None:
-	fig, (ax1, ax2) = plt.subplots(2, sharex=False, gridspec_kw={"hspace": 0.2})
-	fig.suptitle(r"Implements the discrete Fourier transform with pulse as its source")
-	medium = (1 - gax)/(1 - gax)[-1]
-	medium[medium==0] = -1.5
-	ax1.plot(ex, "k", lw=1)
-	ax1.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
-	ax1.text(nx/4, 0.5, f"T = {ns}", horizontalalignment="center")
-	ax1.text(3*nx/4, 0.5, rf"$\epsilon_r$ = {epsr}", horizontalalignment="center")
-	ax1.text(3*nx/4, -0.5, rf"$\sigma$ = {sigma}", horizontalalignment="center")
-	ax1.set(xlim=(0, nx-1), ylim=(-1.2, 2.2), ylabel=r"$E_x$")
-	ax1.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 2.2, 1))
-	ax2.plot(amp, "k", lw=1)
-	ax2.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
-	ax2.set(xlim=(0, nx-1), ylim=(-1.2, 2.2), xlabel=r"FDTD cells", ylabel=r"$Amp$")
-	ax2.set(xticks=range(0, nx+1, round(nx//10,-1)), yticks=np.arange(-1, 2.2, 1))
-	plt.subplots_adjust(bottom=0.2, hspace=0.45)
-	plt.show()
+def visualize(ns: int, nx: int, epsr: float, sigma: float, nax: np.ndarray, ex: np.ndarray) -> None:
+    fig, ax = plt.subplots(figsize=(8,3), gridspec_kw={"hspace": 0.2})
+    fig.suptitle(r"FDTD simulation of a pulse striking dielectric material")
+    medium = (1 - nax)/(1 - nax[-1])*1e3 if epsr > 1 else (1 - nax)
+    medium[medium==0] = -1e3
+    ax.plot(ex, color="black", linewidth=1)
+    ax.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
+    ax.set(xlim=(0, nx-1), ylim=(-1.2, 1.2))
+    ax.set(xticks=range(0, nx+1, round(nx//10,-1)))
+    ax.set(xlabel=r"$z\;(cm)$", ylabel=r"$E_x\;(V/m)$")
+    ax.text(0.02, 0.90, rf"$T$ = {ns}", transform=ax.transAxes)
+    ax.text(0.90, 0.90, rf"$\epsilon_r$ = {epsr}", transform=ax.transAxes)
+    ax.text(0.85, 0.80, rf"$\sigma$ = {sigma} $S/m$", transform=ax.transAxes)
+    plt.subplots_adjust(bottom=0.2, hspace=0.45)
+    plt.show()
+
+
+def amplitude(ns: int, nx: int, epsr: float, sigma: float, nax: np.ndarray, amp: np.ndarray) -> None:
+    fig, ax = plt.subplots(figsize=(8,3), gridspec_kw={"hspace": 0.2})
+    fig.suptitle(r"The discrete Fourier transform with pulse as its source")
+    medium = (1 - nax)/(1 - nax[-1])*1e3 if epsr > 1 else (1 - nax)
+    medium[medium==0] = -1e3
+    ax.plot(amp, color="black", linewidth=1)
+    ax.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
+    ax.set(xlim=(0, nx-1), ylim=(-0.2, 2.2))
+    ax.set(xticks=range(0, nx+1, round(nx//10,-1)))
+    ax.set(xlabel=r"$z\;(cm)$", ylabel=r"$Amp\;(V)$")
+    ax.text(0.02, 0.90, rf"$T$ = {ns}", transform=ax.transAxes)
+    ax.text(0.90, 0.90, rf"$\epsilon_r$ = {epsr}", transform=ax.transAxes)
+    ax.text(0.85, 0.80, rf"$\sigma$ = {sigma} $S/m$", transform=ax.transAxes)
+    plt.subplots_adjust(bottom=0.2, hspace=0.45)
+    plt.show()
+
+
+medium = namedtuple('medium', (
+    'nax', 'nbx',
+))
+
+
+ftrans = namedtuple('ftrans', (
+    'r_pt', 'i_pt',
+    'r_in', 'i_in',
+))
 
 
 kernel = """
-__device__ float
-gaussian(int t, int t0, float sigma) {
-	return exp(-0.5 * ((t - t0)/sigma) * ((t - t0)/sigma));
+#define idx (blockIdx.x * blockDim.x + threadIdx.x)
+#define stx (blockDim.x * gridDim.x)
+
+
+typedef struct {
+    float *nax, *nbx;
+} medium;
+
+
+typedef struct {
+    float *r_pt, *i_pt;
+    float *r_in, *i_in;
+} ftrans;
+
+
+__device__
+float gaussian(int t, int t0, float sigma) {
+    return exp(-0.5 * ((t - t0)/sigma) * ((t - t0)/sigma));
 }
 
 
-__global__ void
-field(int t, int nx, float *gax, float *gbx, float *dx, float *ex, float *ix, float *hy) {
-	int index = blockIdx.x * blockDim.x + threadIdx.x;
-	int stride = blockDim.x * gridDim.x;
-
-	/* calculate the Hy field */
-	for (int i = index; i < nx - 1; i += stride)
-		hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
-
-	__syncthreads();
-
-	/* calculate the electric flux density Dx */
-	for (int i = index + 1; i < nx; i += stride)
-		dx[i] = dx[i] + 0.5 * (hy[i-1] - hy[i]);
-
-	__syncthreads();
-
-	/* put a Gaussian pulse at the low end */
-	dx[1] = dx[1] + gaussian(t, 50, 10);
-
-	/* calculate the Ex field from Dx */
-	for (int i = index + 1; i < nx; i += stride)
-		ex[i] = gax[i] * (dx[i] - ix[i]);
-
-	__syncthreads();
-
-	for (int i = index + 1; i < nx; i += stride)
-		ix[i] = ix[i] + gbx[i] * ex[i];
-
-	__syncthreads();
+__global__
+void fourier(int t, int nf, int nx, float dt, float *freq, float *ex, ftrans *ft) {
+    for (int n = threadIdx.y; n < nf; n += blockDim.y) {
+        for (int i = idx; i < nx; i += stx) {
+            /* calculate the Fourier transform of Ex field */
+            int m = n*nx+i;
+            ft->r_pt[m] += cos(2*M_PI*freq[n]*dt*t) * ex[i];
+            ft->i_pt[m] -= sin(2*M_PI*freq[n]*dt*t) * ex[i];
+        }
+        if (idx == 0 && t < nx/2) {
+            /* calculate the Fourier transform of input source */
+            ft->r_in[n] += cos(2*M_PI*freq[n]*dt*t) * ex[10];
+            ft->i_in[n] -= sin(2*M_PI*freq[n]*dt*t) * ex[10];
+        }
+    }
 }
 
 
-__global__ void
-fieldfourier(int t, int nf, int nx, float dt, float *freq, float *ex, float *r_pt, float *i_pt) {
-	int index = blockIdx.x * blockDim.x + threadIdx.x;
-	int stride = blockDim.x * gridDim.x;
-
-	for (int i = index; i < nx; i += stride) {
-		for (int n = threadIdx.y; n < nf; n += blockDim.y) {
-			r_pt[n*nx + i] = r_pt[n*nx + i] + cos(2*M_PI*freq[n]*dt*t) * ex[i];
-			i_pt[n*nx + i] = i_pt[n*nx + i] - sin(2*M_PI*freq[n]*dt*t) * ex[i];
-		}
-	}
-
-	__syncthreads();
+__global__
+void dxfield(int t, int nx, float *dx, float *hy) {
+    /* calculate the electric flux density Dx */
+    for (int i = idx + 1; i < nx; i += stx)
+        dx[i] = dx[i] + 0.5 * (hy[i-1] - hy[i]);
+    /* put a Gaussian pulse at the low end */
+    if (idx == 1) dx[1] = dx[1] + gaussian(t, 50, 10);
 }
 
 
-__global__ void
-pulsefourier(int t, int nf, float dt, float *freq, float *ex, float *r_in, float *i_in) {
-	for (int n = threadIdx.y; n < nf; n += blockDim.y) {
-		r_in[n] = r_in[n] + cos(2*M_PI*freq[n]*dt*t) * ex[10];
-		i_in[n] = i_in[n] - sin(2*M_PI*freq[n]*dt*t) * ex[10];
-	}
+__global__
+void exfield(int nx, medium *md, float *dx, float *ix, float *ex) {
+    /* calculate the Ex field from Dx */
+    for (int i = idx + 1; i < nx; i += stx) {
+        ex[i] = md->nax[i] * (dx[i] - ix[i]);
+        ix[i] = ix[i] + md->nbx[i] * ex[i];
+    }
+}
 
-	__syncthreads();
+
+__global__
+void hyfield(int nx, float *ex, float *hy, float *bc) {
+    /* absorbing boundary conditions */
+    if (idx == 0) ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
+    if (idx == nx-1) ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
+    /* calculate the Hy field */
+    for (int i = idx; i < nx - 1; i += stx)
+        hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
 }
 """
 
 
-def dielectric(nx: int, epsr: float = 1, sigma: float = 0.04, ddx: float = 0.01):
-	gax = np.ones(nx, dtype=np.float32)
-	gbx = np.zeros(nx, dtype=np.float32)
-	dt: float = ddx/6e8  # time step
-	eps0: float = 8.854e-12  # vacuum permittivity (F/m)
-	gax[nx//2:] = 1/(epsr + (sigma * dt/eps0))
-	gbx[nx//2:] = sigma * dt/eps0
-	return gax, gbx
+def dielectric(nx: int, dt: float, epsr: float, sigma: float) -> medium:
+    md = medium(
+        nax = gpuarray.ones(nx, dtype=np.float32),
+        nbx = gpuarray.zeros(nx, dtype=np.float32),
+    )
+    eps0: float = 8.854e-12  # vacuum permittivity (F/m)
+    md.nax[nx//2:] = 1/(epsr + (sigma * dt/eps0))
+    md.nbx[nx//2:] = sigma * dt/eps0
+    return md
 
 
 def main():
 
-	nx = np.int32(10240)
-	ns = np.int32(15000)
+    nx = np.int32(512)  # number of grid points
+    ns = np.int32(740)  # number of time steps
 
-	dx = gpuarray.to_gpu(np.zeros(nx, dtype=np.float32))
-	ex = gpuarray.to_gpu(np.zeros(nx, dtype=np.float32))
-	ix = gpuarray.to_gpu(np.zeros(nx, dtype=np.float32))
-	hy = gpuarray.to_gpu(np.zeros(nx, dtype=np.float32))
+    dx = gpuarray.zeros(nx, dtype=np.float32)
+    ex = gpuarray.zeros(nx, dtype=np.float32)
+    ix = gpuarray.zeros(nx, dtype=np.float32)
+    hy = gpuarray.zeros(nx, dtype=np.float32)
 
-	bc = gpuarray.to_gpu(np.zeros(4, dtype=np.float32))
+    bc = gpuarray.zeros(4, dtype=np.float32)
 
-	ddx = np.float32(0.01)	# cell size (m)
-	dt = np.float32(ddx/6e8)  # time step
-	epsr: float = 4  # relative permittivity
-	sigma: float = 0  # conductivity (S/m)
-	gax, gbx = dielectric(nx, epsr, sigma, ddx)
+    ds = np.float32(0.01)  # spatial step (m)
+    dt = np.float32(ds/6e8)  # time step (s)
+    epsr: float = 4  # relative permittivity
+    sigma: float = 0  # conductivity (S/m)
+    md: medium = dielectric(nx, dt, epsr, sigma)
 
-	gax = gpuarray.to_gpu(gax)
-	gbx = gpuarray.to_gpu(gbx)
+    mdptr = gpuarray.to_gpu(np.array([
+        md.nax.ptr, md.nbx.ptr,
+    ], dtype=np.uint64)).gpudata
 
-	# frequency 100 MHz, 200 MHz, 500 MHz
-	freq = gpuarray.to_gpu(np.array((100e6, 200e6, 500e6), dtype=np.float32).reshape(-1,1))
-	nf = len(freq)	# number of frequencies
+    # frequency 100 MHz, 200 MHz, 500 MHz
+    freq = gpuarray.to_gpu(np.array((100e6, 200e6, 500e6), dtype=np.float32))
+    nf = np.int32(len(freq))  # number of frequencies
 
-	r_pt = gpuarray.to_gpu(np.zeros((nf, nx), dtype=np.float32))
-	i_pt = gpuarray.to_gpu(np.zeros((nf, nx), dtype=np.float32))
+    ft = ftrans(
+        r_pt = gpuarray.zeros((nf, nx), dtype=np.float32),
+        i_pt = gpuarray.zeros((nf, nx), dtype=np.float32),
+        r_in = gpuarray.zeros((nf, 1), dtype=np.float32),
+        i_in = gpuarray.zeros((nf, 1), dtype=np.float32),
+    )
 
-	r_in = gpuarray.to_gpu(np.zeros(nf, dtype=np.float32).reshape(-1,1))
-	i_in = gpuarray.to_gpu(np.zeros(nf, dtype=np.float32).reshape(-1,1))
+    ftptr = gpuarray.to_gpu(np.array([
+        ft.r_pt.ptr, ft.i_pt.ptr,
+        ft.r_in.ptr, ft.i_in.ptr,
+    ], dtype=np.uint64)).gpudata
 
-	amplt = np.zeros((nf, nx), dtype=np.float32)
-	phase = np.zeros((nf, nx), dtype=np.float32)
+    amplt = np.zeros((nf, nx), dtype=np.float32)
+    phase = np.zeros((nf, nx), dtype=np.float32)
 
-	blockDimx = 256
-	blockDimy = nf
-	gridDimx = int((nx + blockDimx - 1)/blockDimx)
+    numSM: int = drv.Device(0).multiprocessor_count
 
-	mod = SourceModule(kernel)
-	field = mod.get_function("field")
-	fieldfourier = mod.get_function("fieldfourier")
-	pulsefourier = mod.get_function("pulsefourier")
+    blockDimx: int = 256
+    blockDimy: int = int(nf)
+    gridDimx: int = 32*numSM
 
-	gridDim = (gridDimx,1)
-	blockDim = (blockDimx,blockDimy,1)
+    gridDim = (gridDimx,1,1)
+    blockDim = (blockDimx,blockDimy,1)
 
-	for t in range(1, ns+1):
-		field(np.int32(t), nx, gax, gbx, dx, ex, ix, hy, grid=gridDim, block=blockDim)
-		fieldfourier(np.int32(t), np.int32(nf), nx, dt, freq, ex, r_pt, i_pt, grid=gridDim, block=blockDim)
-		if t < nx//2: pulsefourier(np.int32(t), np.int32(nf), dt, freq, ex, r_in, i_in, grid=gridDim, block=blockDim)
-		# absorbing boundary conditions
-		ex[0], bc[0], bc[1] = bc[0], bc[1], ex[1]
-		ex[-1], bc[3], bc[2] = bc[3], bc[2], ex[-2]
+    mod = SourceModule(kernel)
+    fourier = mod.get_function("fourier")
+    dxfield = mod.get_function("dxfield")
+    exfield = mod.get_function("exfield")
+    hyfield = mod.get_function("hyfield")
 
-	drv.Context.synchronize()
+    for t in np.arange(1, ns+1).astype(np.int32):
+        dxfield(t, nx, dx, hy, grid=gridDim, block=blockDim)
+        exfield(nx, mdptr, dx, ix, ex, grid=gridDim, block=blockDim)
+        fourier(t, nf, nx, dt, freq, ex, ftptr, grid=gridDim, block=blockDim)
+        hyfield(nx, ex, hy, bc, grid=gridDim, block=blockDim)
 
-	amplt = (1/sqrt(r_in.get()**2 + i_in.get()**2)) * sqrt(r_pt.get()**2 + i_pt.get()**2)
-	phase = arctan2(i_pt.get(), r_pt.get()) - arctan2(i_in.get(), r_in.get())
+    drv.Context.synchronize()
 
-	visualize(ns, nx, epsr, sigma, gax.get(), ex.get(), amplt[2])
+    # calculate the amplitude and phase at each frequency
+    amplt = 1/hypot(ft.r_in.get(),ft.i_in.get()) * hypot(ft.r_pt.get(),ft.i_pt.get())
+    phase = arctan2(ft.i_pt.get(),ft.r_pt.get()) - arctan2(ft.i_in.get(),ft.r_in.get())
+
+    visualize(ns, nx, epsr, sigma, md.nax.get(), ex.get())
+    amplitude(ns, nx, epsr, sigma, md.nax.get(), amplt[2])
+
 
 if __name__ == "__main__":
-	main()
+    main()
