ad0019ce61788ede78053453347165559f3fe605
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-04-19 19:32:10 +0530

    simulation of a sinusoidal in free space in the transverse magnetic mode with pml

---
+++ fd2d/animation/fd2d_3_2.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# File: fd2d_3_2.py
+# Name: D.Saravanan
+# Date: 18/01/2022
+
+""" Simulation of a propagating sinusoidal in free space in the transverse
+magnetic (TM) mode with the two-dimensional perfectly matched layer (PML) """
+
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d
+from collections import namedtuple
+import numpy as np
+
+plt.style.use("classic")
+plt.style.use("../pyplot.mplstyle")
+
+
+pmlayer = namedtuple('pmlayer', (
+    'fx1',
+    'fx2',
+    'fx3',
+    'fy1',
+    'fy2',
+    'fy3',
+    'gx2',
+    'gx3',
+    'gy2',
+    'gy3',
+))
+
+
+def sinusoidal(t: int, ds: float, freq: float) -> float:
+    dt: float = ds/6e8  # time step (s)
+    return np.sin(2 * np.pi * freq * dt * t)
+
+
+def pmlparam(npml: int, nx: int, ny: int, pml: pmlayer) -> None:
+    """ calculate the two-dimensional perfectly matched layer (PML) parameters """
+    for n in range(npml):
+        xm = 0.33 * ((npml-n)/npml)**3
+        xn = 0.33 * ((npml-n-0.5)/npml)**3
+        pml.fx1[n] = pml.fx1[nx-2-n] = pml.fy1[n] = pml.fy1[ny-2-n] = xn
+        pml.fx2[n] = pml.fx2[nx-2-n] = pml.fy2[n] = pml.fy2[ny-2-n] = 1/(1+xn)
+        pml.gx2[n] = pml.gx2[nx-1-n] = pml.gy2[n] = pml.gy2[ny-1-n] = 1/(1+xm)
+        pml.fx3[n] = pml.fx3[nx-2-n] = pml.fy3[n] = pml.fy3[ny-2-n] = (1-xn)/(1+xn)
+        pml.gx3[n] = pml.gx3[nx-1-n] = pml.gy3[n] = pml.gy3[ny-1-n] = (1-xm)/(1+xm)
+
+
+def dfield(t: int, nx: int, ny: int, pml: pmlayer, dz: np.ndarray, hx: np.ndarray, hy: np.ndarray) -> None:
+    """ calculate the electric flux density Dz """
+    dz[1:ny,1:nx] = pml.gy3[1:ny,None] * pml.gx3[1:nx] * dz[1:ny,1:nx] + pml.gy2[1:ny,None] * pml.gx2[1:nx] * 0.5 * (hy[1:ny,1:nx] - hy[0:ny-1,1:nx] - hx[1:ny,1:nx] + hx[1:ny,0:nx-1])
+    # put a sinusoidal source at a point that is offset five cells
+    # from the center of the problem space in each direction
+    dz[ny//2-5,nx//2-5] = sinusoidal(t, 0.01, 1500e6)
+
+
+def efield(nx: int, ny: int, naz: np.ndarray, dz: np.ndarray, ez: np.ndarray) -> None:
+    """ calculate the Ez field from Dz """
+    ez[0:ny,0:nx] = naz[0:ny,0:nx] * dz[0:ny,0:nx]
+
+
+def hfield(nx: int, ny: int, pml: pmlayer, ez: np.ndarray, ihx: np.ndarray, ihy: np.ndarray, hx: np.ndarray, hy: np.ndarray) -> None:
+    """ calculate the Hx and Hy field """
+    curl_em = ez[0:ny-1,0:nx-1] - ez[0:ny-1,1:nx]
+    curl_en = ez[0:ny-1,0:nx-1] - ez[1:ny,0:nx-1]
+    ihx[0:ny-1,0:nx-1] = ihx[0:ny-1,0:nx-1] + curl_em
+    ihy[0:ny-1,0:nx-1] = ihy[0:ny-1,0:nx-1] + curl_en
+    hx[0:ny-1,0:nx-1] = pml.fx3[0:nx-1] * hx[0:ny-1,0:nx-1] + pml.fx2[0:nx-1] * (0.5 * curl_em + pml.fy1[0:ny-1,None] * ihx[0:ny-1,0:nx-1])
+    hy[0:ny-1,0:nx-1] = pml.fy3[0:ny-1,None] * hy[0:ny-1,0:nx-1] - pml.fy2[0:ny-1,None] * (0.5 * curl_en + pml.fx1[0:nx-1] * ihy[0:ny-1,0:nx-1])
+
+
+def main():
+
+    nx: int = 60  # number of grid points
+    ny: int = 60  # number of grid points
+
+    ns: int = 180  # number of time steps
+
+    dz = np.zeros((ny, nx), dtype=np.float64)
+    ez = np.zeros((ny, nx), dtype=np.float64)
+    hx = np.zeros((ny, nx), dtype=np.float64)
+    hy = np.zeros((ny, nx), dtype=np.float64)
+
+    ihx = np.zeros((ny, nx), dtype=np.float64)
+    ihy = np.zeros((ny, nx), dtype=np.float64)
+
+    naz = np.ones((ny, nx), dtype=np.float64)
+
+    ds: float = 0.01  # spatial step (m)
+    dt: float = ds/6e8  # time step (s)
+
+    pml = pmlayer(
+        fx1 = np.zeros(nx, dtype=np.float64),
+        fx2 = np.ones(nx, dtype=np.float64),
+        fx3 = np.ones(nx, dtype=np.float64),
+        fy1 = np.zeros(ny, dtype=np.float64),
+        fy2 = np.ones(ny, dtype=np.float64),
+        fy3 = np.ones(ny, dtype=np.float64),
+        gx2 = np.ones(nx, dtype=np.float64),
+        gx3 = np.ones(nx, dtype=np.float64),
+        gy2 = np.ones(ny, dtype=np.float64),
+        gy3 = np.ones(ny, dtype=np.float64),
+    )
+
+    npml: int = 8  # pml thickness
+    pmlparam(npml, nx, ny, pml)
+
+    ezdata = [ez]
+    for t in np.arange(1, ns+1).astype(np.int32):
+        dfield(t, nx, ny, pml, dz, hx, hy)
+        efield(nx, ny, naz, dz, ez)
+        hfield(nx, ny, pml, ez, ihx, ihy, hx, hy)
+        ezdata.append(ez.copy())
+
+    ez = np.array(ezdata)
+
+    # define the meta data for the movie
+    fwriter = animation.writers["ffmpeg"]
+    data = {"title": "Simulation in the transverse magnetic (TM) mode with PML"}
+    writer = fwriter(fps=15, codec='h264', bitrate=2000, metadata=data)
+
+    # draw an empty plot, but preset the plot x-, y- and z- limits
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    fig.suptitle(r"FDTD simulation of a sinusoidal in free space with PML")
+    xv, yv = np.meshgrid(np.arange(nx), np.arange(ny))
+    ax.plot_surface(xv, yv, ez[0], rstride=1, cstride=1, cmap="gray", lw=0.25)
+    ax.text2D(0.8, 0.7, "", transform=ax.transAxes)
+    ax.set(xlim=(0, nx), ylim=(0, ny), zlim=(0, 1))
+    ax.set(xlabel=r"$x\;(cm)$", ylabel=r"$y\;(cm)$", zlabel=r"$E_z\;(V/m)$")
+    ax.zaxis.set_rotate_label(False)
+    ax.view_init(elev=20., azim=45)
+    plt.subplots_adjust(bottom=0.1, hspace=0.45)
+
+    with writer.saving(fig, "fd2d_surface_3_2.mp4", 300):
+        for t in np.arange(1, ns+1).astype(np.int32):
+            ax.clear()
+            ax.plot_surface(xv, yv, ez[t], rstride=1, cstride=1, cmap="gray", lw=0.25)
+            ax.text2D(0.8, 0.7, rf"$T$ = {t}", transform=ax.transAxes)
+            ax.set(xlim=(0, nx), ylim=(0, ny), zlim=(0, 1))
+            ax.set(xlabel=r"$x\;(cm)$", ylabel=r"$y\;(cm)$", zlabel=r"$E_z\;(V/m)$")
+            writer.grab_frame()
+
+    plt.close(fig)
+
+    # draw an empty plot, but preset the plot x-, y- and z- limits
+    fig, ax = plt.subplots(gridspec_kw={"hspace": 0.2})
+    fig.suptitle(r"FDTD simulation of a sinusoidal in free space with PML")
+    xv, yv = np.meshgrid(np.arange(nx), np.arange(ny))
+    ax.contourf(xv, yv, ez[0], cmap="gray", alpha=0.75)
+    ax.contour(xv, yv, ez[0], colors="k", linewidths=0.25)
+    ax.set(xlim=(0, nx-1), ylim=(0, ny-1), aspect="equal")
+    ax.set(xlabel=r"$x\;(cm)$", ylabel=r"$y\;(cm)$")
+    plt.subplots_adjust(bottom=0.1, hspace=0.45)
+
+    with writer.saving(fig, "fd2d_contour_3_2.mp4", 300):
+        for t in np.arange(1, ns+1).astype(np.int32):
+            ax.clear()
+            ax.contourf(xv, yv, ez[t], cmap="gray", alpha=0.75)
+            ax.contour(xv, yv, ez[t], colors="k", linewidths=0.25)
+            ax.set(xlim=(0, nx-1), ylim=(0, ny-1), aspect="equal")
+            ax.set(xlabel=r"$x\;(cm)$", ylabel=r"$y\;(cm)$")
+            writer.grab_frame()
+
+    plt.close(fig)
+
+
+if __name__ == "__main__":
+    main()
