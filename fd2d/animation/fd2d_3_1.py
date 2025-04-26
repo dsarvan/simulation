65e893f01b1e06222b186dadf9def2974176373b
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-04-26 19:32:10 +0530

    simulation of a pulse in free space in the transverse magnetic mode

--- fd2d/animation/fd2d_3_1.py
+++ fd2d/animation/fd2d_3_1.py
@@ -20,20 +20,20 @@ def gaussian(t: int, t0: int, sigma: float) -> float:
 
 def dfield(t: int, nx: int, ny: int, dz: np.ndarray, hx: np.ndarray, hy: np.ndarray) -> None:
     """ calculate the electric flux density Dz """
-    dz[1:ny,1:nx] += 0.5 * (hy[1:ny,1:nx] - hy[0:ny-1,1:nx] - hx[1:ny,1:nx] + hx[1:ny,0:nx-1])
+    dz[1:nx,1:ny] += 0.5 * (hy[1:nx,1:ny] - hy[0:nx-1,1:ny] - hx[1:nx,1:ny] + hx[1:nx,0:ny-1])
     # put a Gaussian pulse in the middle
-    dz[ny//2,nx//2] = gaussian(t, 20, 6)
+    dz[nx//2,ny//2] = gaussian(t, 20, 6)
 
 
 def efield(nx: int, ny: int, naz: np.ndarray, dz: np.ndarray, ez: np.ndarray) -> None:
     """ calculate the Ez field from Dz """
-    ez[1:ny,1:nx] = naz[1:ny,1:nx] * dz[1:ny,1:nx]
+    ez[0:nx,0:ny] = naz[0:nx,0:ny] * dz[0:nx,0:ny]
 
 
 def hfield(nx: int, ny: int, ez: np.ndarray, hx: np.ndarray, hy: np.ndarray) -> None:
     """ calculate the Hx and Hy field """
-    hx[0:ny-1,0:nx-1] += 0.5 * (ez[0:ny-1,0:nx-1] - ez[0:ny-1,1:nx])
-    hy[0:ny-1,0:nx-1] += 0.5 * (ez[1:ny,0:nx-1] - ez[0:ny-1,0:nx-1])
+    hx[0:nx-1,0:ny-1] += 0.5 * (ez[0:nx-1,0:ny-1] - ez[0:nx-1,1:ny])
+    hy[0:nx-1,0:ny-1] -= 0.5 * (ez[0:nx-1,0:ny-1] - ez[1:nx,0:ny-1])
 
 
 def main():
@@ -43,12 +43,12 @@ def main():
 
     ns: int = 180  # number of time steps
 
-    dz = np.zeros((ny, nx), dtype=np.float64)
-    ez = np.zeros((ny, nx), dtype=np.float64)
-    hx = np.zeros((ny, nx), dtype=np.float64)
-    hy = np.zeros((ny, nx), dtype=np.float64)
+    dz = np.zeros((nx, ny), dtype=np.float64)
+    ez = np.zeros((nx, ny), dtype=np.float64)
+    hx = np.zeros((nx, ny), dtype=np.float64)
+    hy = np.zeros((nx, ny), dtype=np.float64)
 
-    naz = np.ones((ny, nx), dtype=np.float64)
+    naz = np.ones((nx, ny), dtype=np.float64)
 
     ezdata = [ez]
     for t in np.arange(1, ns+1).astype(np.int32):
@@ -67,20 +67,19 @@ def main():
     # draw an empty plot, but preset the plot x-, y- and z- limits
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     fig.suptitle(r"FDTD simulation of a pulse in free space")
-    xv, yv = np.meshgrid(np.arange(nx), np.arange(ny))
-    ax.plot_surface(xv, yv, ez[0], rstride=1, cstride=1, cmap="gray", lw=0.25)
-    ax.text2D(0.8, 0.7, "", transform=ax.transAxes)
+    xv, yv = np.meshgrid(np.arange(ny), np.arange(nx))
+    ax.plot_surface(yv, xv, ez[0], rstride=1, cstride=1, cmap="gray", lw=0.25)
+    ax.text2D(0.1, 0.7, "", transform=ax.transAxes)
     ax.set(xlim=(0, nx), ylim=(0, ny), zlim=(0, 1))
     ax.set(xlabel=r"$x\;(cm)$", ylabel=r"$y\;(cm)$", zlabel=r"$E_z\;(V/m)$")
-    ax.zaxis.set_rotate_label(False)
-    ax.view_init(elev=20., azim=45)
+    ax.zaxis.set_rotate_label(False); ax.view_init(elev=20.0, azim=45)
     plt.subplots_adjust(bottom=0.1, hspace=0.45)
 
     with writer.saving(fig, "fd2d_surface_3_1.mp4", 300):
         for t in np.arange(1, ns+1).astype(np.int32):
             ax.clear()
-            ax.plot_surface(xv, yv, ez[t], rstride=1, cstride=1, cmap="gray", lw=0.25)
-            ax.text2D(0.8, 0.7, rf"$T$ = {t}", transform=ax.transAxes)
+            ax.plot_surface(yv, xv, ez[t], rstride=1, cstride=1, cmap="gray", lw=0.25)
+            ax.text2D(0.1, 0.7, rf"$T$ = {t}", transform=ax.transAxes)
             ax.set(xlim=(0, nx), ylim=(0, ny), zlim=(0, 1))
             ax.set(xlabel=r"$x\;(cm)$", ylabel=r"$y\;(cm)$", zlabel=r"$E_z\;(V/m)$")
             writer.grab_frame()
@@ -90,9 +89,9 @@ def main():
     # draw an empty plot, but preset the plot x-, y- and z- limits
     fig, ax = plt.subplots(gridspec_kw={"hspace": 0.2})
     fig.suptitle(r"FDTD simulation of a pulse in free space")
-    xv, yv = np.meshgrid(np.arange(nx), np.arange(ny))
-    ax.contourf(xv, yv, ez[0], cmap="gray", alpha=0.75)
-    ax.contour(xv, yv, ez[0], colors="k", linewidths=0.25)
+    xv, yv = np.meshgrid(np.arange(ny), np.arange(nx))
+    ax.contourf(yv, xv, ez[0], cmap="gray", alpha=0.75)
+    ax.contour(yv, xv, ez[0], colors="k", linewidths=0.25)
     ax.set(xlim=(0, nx-1), ylim=(0, ny-1), aspect="equal")
     ax.set(xlabel=r"$x\;(cm)$", ylabel=r"$y\;(cm)$")
     plt.subplots_adjust(bottom=0.1, hspace=0.45)
@@ -100,8 +99,8 @@ def main():
     with writer.saving(fig, "fd2d_contour_3_1.mp4", 300):
         for t in np.arange(1, ns+1).astype(np.int32):
             ax.clear()
-            ax.contourf(xv, yv, ez[t], cmap="gray", alpha=0.75)
-            ax.contour(xv, yv, ez[t], colors="k", linewidths=0.25)
+            ax.contourf(yv, xv, ez[t], cmap="gray", alpha=0.75)
+            ax.contour(yv, xv, ez[t], colors="k", linewidths=0.25)
             ax.set(xlim=(0, nx-1), ylim=(0, ny-1), aspect="equal")
             ax.set(xlabel=r"$x\;(cm)$", ylabel=r"$y\;(cm)$")
             writer.grab_frame()
