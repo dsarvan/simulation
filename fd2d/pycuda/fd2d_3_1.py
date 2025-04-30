f67958fc15563121a664df84feed2e4381bb5be1
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-04-30 19:32:10 +0530

    simulation of a pulse in free space in the transverse magnetic mode

--- fd2d/pycuda/fd2d_3_1.py
+++ fd2d/pycuda/fd2d_3_1.py
@@ -21,22 +21,21 @@ plt.style.use("../pyplot.mplstyle")
 def surfaceplot(ns: int, nx: int, ny: int, ez: np.ndarray) -> None:
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     fig.suptitle(r"FDTD simulation of a pulse in free space")
-    xv, yv = np.meshgrid(np.arange(nx), np.arange(ny))
-    ax.plot_surface(xv, yv, ez, rstride=1, cstride=1, cmap="gray", lw=0.25)
-    ax.text2D(0.8, 0.7, rf"$T$ = {ns}", transform=ax.transAxes)
+    xv, yv = np.meshgrid(np.arange(ny), np.arange(nx))
+    ax.plot_surface(yv, xv, ez, rstride=1, cstride=1, cmap="gray", lw=0.25)
+    ax.text2D(0.1, 0.7, rf"$T$ = {ns}", transform=ax.transAxes)
     ax.set(xlim=(0, nx), ylim=(0, ny), zlim=(0, 1))
     ax.set(xlabel=r"$x\;(cm)$", ylabel=r"$y\;(cm)$", zlabel=r"$E_z\;(V/m)$")
-    ax.zaxis.set_rotate_label(False)
-    ax.view_init(elev=20.0, azim=45)
+    ax.zaxis.set_rotate_label(False); ax.view_init(elev=20.0, azim=45)
     plt.show()
 
 
 def contourplot(ns: int, nx: int, ny: int, ez: np.ndarray) -> None:
     fig, ax = plt.subplots(figsize=(4,4), gridspec_kw={"hspace": 0.2})
     fig.suptitle(r"FDTD simulation of a pulse in free space")
-    xv, yv = np.meshgrid(np.arange(nx), np.arange(ny))
-    ax.contourf(xv, yv, ez, cmap="gray", alpha=0.75)
-    ax.contour(xv, yv, ez, colors="k", linewidths=0.25)
+    xv, yv = np.meshgrid(np.arange(ny), np.arange(nx))
+    ax.contourf(yv, xv, ez, cmap="gray", alpha=0.75)
+    ax.contour(yv, xv, ez, colors="k", linewidths=0.25)
     ax.set(xlim=(0, nx-1), ylim=(0, ny-1), aspect="equal")
     ax.set(xlabel=r"$x\;(cm)$", ylabel=r"$y\;(cm)$")
     plt.subplots_adjust(bottom=0.2, hspace=0.45)
@@ -59,23 +58,24 @@ float gaussian(int t, int t0, float sigma) {
 __global__
 void dfield(int t, int nx, int ny, float *dz, float *hx, float *hy) {
     /* calculate the electric flux density Dz */
-    for (int j = idy + 1; j < ny; j += sty) {
-        for (int i = idx + 1; i < nx; i += stx) {
-            int n = j*nx+i;
-            dz[n] += 0.5 * (hy[n] - hy[n-nx] - hx[n] + hx[n-1]);
+    for (int i = idy + 1; i < nx; i += sty) {
+        for (int j = idx + 1; j < ny; j += stx) {
+            int n = i*ny+j;
+            dz[n] += 0.5 * (hy[n] - hy[n-ny] - hx[n] + hx[n-1]);
         }
     }
+    __syncthreads();
     /* put a Gaussian pulse in the middle */
-    if ((idy == ny/2) && (idx == nx/2)) dz[ny/2*nx+nx/2] = gaussian(t, 20, 6);
+    if (idy == nx/2 && idx == ny/2) dz[nx/2*ny+ny/2] = gaussian(t, 20, 6);
 }
 
 
 __global__
 void efield(int nx, int ny, float *naz, float *dz, float *ez) {
     /* calculate the Ez field from Dz */
-    for (int j = idy + 1; j < ny; j += sty) {
-        for (int i = idx + 1; i < nx; i += stx) {
-            int n = j*nx+i;
+    for (int i = idy; i < nx; i += sty) {
+        for (int j = idx; j < ny; j += stx) {
+            int n = i*ny+j;
             ez[n] = naz[n] * dz[n];
         }
     }
@@ -85,11 +85,11 @@ void efield(int nx, int ny, float *naz, float *dz, float *ez) {
 __global__
 void hfield(int nx, int ny, float *ez, float *hx, float *hy) {
     /* calculate the Hx and Hy field */
-    for (int j = idy; j < ny - 1; j += sty) {
-        for (int i = idx; i < nx - 1; i += stx) {
-            int n = j*nx+i;
+    for (int i = idy; i < nx - 1; i += sty) {
+        for (int j = idx; j < ny - 1; j += stx) {
+            int n = i*ny+j;
             hx[n] += 0.5 * (ez[n] - ez[n+1]);
-            hy[n] += 0.5 * (ez[n+nx] - ez[n]);
+            hy[n] -= 0.5 * (ez[n] - ez[n+ny]);
         }
     }
 }
@@ -112,8 +112,8 @@ def main():
 
     blockDimx: int = 16
     blockDimy: int = 16
-    gridDimx: int = int((nx + blockDimx - 1)/blockDimx)
-    gridDimy: int = int((ny + blockDimy - 1)/blockDimy)
+    gridDimx: int = int((ny + blockDimx - 1)/blockDimx)
+    gridDimy: int = int((nx + blockDimy - 1)/blockDimy)
 
     gridDim = (gridDimx,gridDimy,1)
     blockDim = (blockDimx,blockDimy,1)
@@ -130,8 +130,8 @@ def main():
 
     drv.Context.synchronize()
 
-    surfaceplot(ns, nx, ny, ez.get().reshape(ny,nx))
-    contourplot(ns, nx, ny, ez.get().reshape(ny,nx))
+    surfaceplot(ns, nx, ny, ez.get().reshape(nx,ny))
+    contourplot(ns, nx, ny, ez.get().reshape(nx,ny))
 
 
 if __name__ == "__main__":
