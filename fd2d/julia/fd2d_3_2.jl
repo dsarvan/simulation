de20a72ec236656f8c0f821a633dccc5aa30504f
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-05-03 19:32:10 +0530

    simulation of a sinusoidal in free space in the transverse magnetic mode with pml

--- fd2d/julia/fd2d_3_2.jl
+++ fd2d/julia/fd2d_3_2.jl
@@ -10,35 +10,32 @@ import PyPlot as plt
 
 plt.matplotlib.style.use("classic")
 plt.matplotlib.style.use("../pyplot.mplstyle")
-
-
-meshgrid(xs, ys) = [xs[i] for i in 1:length(xs), j in 1:length(ys)], [ys[j] for i in 1:length(xs), j in 1:length(ys)]
+meshgrid(xs, ys) = xs .* ones(length(ys))', ones(length(xs)) .* ys'
 
 
 function surfaceplot(ns::Int, nx::Int, ny::Int, ez::Array{Float64})::Nothing
     fig, ax = plt.subplots(subplot_kw=Dict("projection" => "3d"))
     fig.suptitle(raw"FDTD simulation of a sinusoidal in free space with PML")
-    xv, yv = meshgrid(0:nx-1, 0:ny-1)
-    ax.plot_surface(xv, yv, ez', rstride=1, cstride=1, cmap="gray", lw=0.25)
-    ax.text2D(0.8, 0.7, raw"$T$ = "*"$ns", transform=ax.transAxes)
+    xv, yv = meshgrid(0:ny-1, 0:nx-1)
+    ax.plot_surface(yv, xv, ez', rstride=1, cstride=1, cmap="gray", lw=0.25)
+    ax.text2D(0.1, 0.7, raw"$T$ = "*"$ns", transform=ax.transAxes)
     ax.set(xlim=(0, nx), ylim=(0, ny), zlim=(0, 1))
     ax.set(xlabel=raw"$x\;(cm)$", ylabel=raw"$y\;(cm)$", zlabel=raw"$E_z\;(V/m)$")
-    ax.zaxis.set_rotate_label(false)
-    ax.view_init(elev=20.0, azim=45)
-    plt.savefig("fd2d_surface_3_2.png")
+    ax.zaxis.set_rotate_label(false); ax.view_init(elev=20.0, azim=45)
+    plt.savefig("fd2d_surface_3_2.png", dpi=100)
 end
 
 
 function contourplot(ns::Int, nx::Int, ny::Int, ez::Array{Float64})::Nothing
     fig, ax = plt.subplots(figsize=(4,4), gridspec_kw=Dict("hspace" => 0.2))
     fig.suptitle(raw"FDTD simulation of a sinusoidal in free space with PML")
-    xv, yv = meshgrid(0:nx-1, 0:ny-1)
-    ax.contourf(xv, yv, ez', cmap="gray", alpha=0.75)
-    ax.contour(xv, yv, ez', colors="k", linewidths=0.25)
+    xv, yv = meshgrid(0:ny-1, 0:nx-1)
+    ax.contourf(yv, xv, ez', cmap="gray", alpha=0.75)
+    ax.contour(yv, xv, ez', colors="k", linewidths=0.25)
     ax.set(xlim=(0, nx-1), ylim=(0, ny-1), aspect="equal")
     ax.set(xlabel=raw"$x\;(cm)$", ylabel=raw"$y\;(cm)$")
     plt.subplots_adjust(bottom=0.2, hspace=0.45)
-    plt.savefig("fd2d_contour_3_2.png")
+    plt.savefig("fd2d_contour_3_2.png", dpi=100)
 end
 
 
@@ -78,27 +75,27 @@ end
 
 function dfield(t::Int32, nx::Int, ny::Int, pml::pmlayer, dz::Array{Float64}, hx::Array{Float64}, hy::Array{Float64})
     """ calculate the electric flux density Dz """
-    @views dz[2:ny,2:nx] .= pml.gy3[2:ny] .* pml.gx3[2:nx]' .* dz[2:ny,2:nx] .+ pml.gy2[2:ny] .* pml.gx2[2:nx]' .* 0.5 .* (hy[2:ny,2:nx] .- hy[1:ny-1,2:nx] .- hx[2:ny,2:nx] .+ hx[2:ny,1:nx-1])
+    @views dz[2:nx,2:ny] .= pml.gx3[2:nx] .* pml.gy3[2:ny]' .* dz[2:nx,2:ny] .+ pml.gx2[2:nx] .* pml.gy2[2:ny]' .* 0.5 .* (hy[2:nx,2:ny] .- hy[1:nx-1,2:ny] .- hx[2:nx,2:ny] .+ hx[2:nx,1:ny-1])
     # put a sinusoidal source at a point that is offset four cells
     # from the center of the problem space in each direction
-    dz[div(ny,2)-4,div(nx,2)-4] = sinusoidal(t, 0.01, 1500e6)
+    dz[div(nx,2)-4,div(ny,2)-4] = sinusoidal(t, 0.01, 1500e6)
 end
 
 
 function efield(nx::Int, ny::Int, naz::Array{Float64}, dz::Array{Float64}, ez::Array{Float64})
     """ calculate the Ez field from Dz """
-    @views ez[1:ny,1:nx] .= naz[1:ny,1:nx] .* dz[1:ny,1:nx]
+    @views ez[1:nx,1:ny] .= naz[1:nx,1:ny] .* dz[1:nx,1:ny]
 end
 
 
 function hfield(nx::Int, ny::Int, pml::pmlayer, ez::Array{Float64}, ihx::Array{Float64}, ihy::Array{Float64}, hx::Array{Float64}, hy::Array{Float64})
     """ calculate the Hx and Hy field """
-    curl_em = ez[1:ny-1,1:nx-1] - ez[1:ny-1,2:nx]
-    curl_en = ez[1:ny-1,1:nx-1] - ez[2:ny,1:nx-1]
-    @views ihx[1:ny-1,1:nx-1] .+= curl_em
-    @views ihy[1:ny-1,1:nx-1] .+= curl_en
-    @views hx[1:ny-1,1:nx-1] .= pml.fx3[1:nx-1]' .* hx[1:ny-1,1:nx-1] .+ pml.fx2[1:nx-1]' .* (0.5 .* curl_em .+ pml.fy1[1:ny-1] .* ihx[1:ny-1,1:nx-1])
-    @views hy[1:ny-1,1:nx-1] .= pml.fy3[1:ny-1] .* hy[1:ny-1,1:nx-1] .- pml.fy2[1:ny-1] .* (0.5 .* curl_en .+ pml.fx1[1:nx-1]' .* ihy[1:ny-1,1:nx-1])
+    curl_em = ez[1:nx-1,1:ny-1] .- ez[1:nx-1,2:ny]
+    curl_en = ez[1:nx-1,1:ny-1] .- ez[2:nx,1:ny-1]
+    @views ihx[1:nx-1,1:ny-1] .+= curl_em
+    @views ihy[1:nx-1,1:ny-1] .+= curl_en
+    @views hx[1:nx-1,1:ny-1] .= pml.fy3[1:ny-1]' .* hx[1:nx-1,1:ny-1] .+ pml.fy2[1:ny-1]' .* (0.5 .* curl_em .+ pml.fx1[1:nx-1] .* ihx[1:nx-1,1:ny-1])
+    @views hy[1:nx-1,1:ny-1] .= pml.fx3[1:nx-1] .* hy[1:nx-1,1:ny-1] .- pml.fx2[1:nx-1] .* (0.5 .* curl_en .+ pml.fy1[1:ny-1]' .* ihy[1:nx-1,1:ny-1])
 end
 
 
@@ -109,15 +106,15 @@ function main()
 
     ns::Int = 100  # number of time steps
 
-    dz = zeros(Float64, (ny, nx))
-    ez = zeros(Float64, (ny, nx))
-    hx = zeros(Float64, (ny, nx))
-    hy = zeros(Float64, (ny, nx))
+    dz = zeros(Float64, (nx, ny))
+    ez = zeros(Float64, (nx, ny))
+    hx = zeros(Float64, (nx, ny))
+    hy = zeros(Float64, (nx, ny))
 
-    ihx = zeros(Float64, (ny, nx))
-    ihy = zeros(Float64, (ny, nx))
+    ihx = zeros(Float64, (nx, ny))
+    ihy = zeros(Float64, (nx, ny))
 
-    naz = ones(Float64, (ny, nx))
+    naz = ones(Float64, (nx, ny))
 
     ds::Float64 = 0.01  # spatial step (m)
     dt::Float64 = ds/6e8  # time step (s)
