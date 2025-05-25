2d56b69ec7bdfda171ab131cad558ebdb4d796ce
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-05-25 19:32:10 +0530

    simulation of a plane wave in free space in the transverse magnetic mode with pml

---
+++ fd2d/program/fd2d_3_3.jl
@@ -0,0 +1,195 @@
+#!/usr/bin/env julia
+# File: fd2d_3_3.jl
+# Name: D.Saravanan
+# Date: 05/03/2025
+
+""" Simulation of a plane wave pulse propagating in free space in the transverse
+magnetic (TM) mode with the two-dimensional perfectly matched layer (PML) """
+
+import PyPlot as plt
+
+plt.matplotlib.style.use("classic")
+plt.matplotlib.style.use("../pyplot.mplstyle")
+meshgrid(xs, ys) = xs .* ones(length(ys))', ones(length(xs)) .* ys'
+
+
+function surfaceplot(ns::Int, nx::Int, ny::Int, ez::Array{Float64})::Nothing
+    fig, ax = plt.subplots(subplot_kw=Dict("projection" => "3d"))
+    fig.suptitle(raw"FDTD simulation of a plane wave in free space with PML")
+    xv, yv = meshgrid(0:ny-1, 0:nx-1)
+    ax.plot_surface(yv, xv, ez', rstride=1, cstride=1, cmap="gray", lw=0.25)
+    ax.text2D(0.1, 0.7, raw"$T$ = "*"$ns", transform=ax.transAxes)
+    ax.set(xlim=(0, nx), ylim=(0, ny), zlim=(0, 1))
+    ax.set(xlabel=raw"$x\;(cm)$", ylabel=raw"$y\;(cm)$", zlabel=raw"$E_z\;(V/m)$")
+    ax.zaxis.set_rotate_label(false); ax.view_init(elev=20.0, azim=45)
+    plt.savefig("fd2d_surface_3_3.png", dpi=100)
+end
+
+
+function contourplot(ns::Int, nx::Int, ny::Int, ez::Array{Float64})::Nothing
+    fig, ax = plt.subplots(figsize=(4,4), gridspec_kw=Dict("hspace" => 0.2))
+    fig.suptitle(raw"FDTD simulation of a plane wave in free space with PML")
+    xv, yv = meshgrid(0:ny-1, 0:nx-1)
+    ax.contourf(yv, xv, ez', cmap="gray", alpha=0.75)
+    ax.contour(yv, xv, ez', colors="k", linewidths=0.25)
+    ax.set(xlim=(0, nx-1), ylim=(0, ny-1), aspect="equal")
+    ax.set(xlabel=raw"$x\;(cm)$", ylabel=raw"$y\;(cm)$")
+    plt.subplots_adjust(bottom=0.2, hspace=0.45)
+    plt.savefig("fd2d_contour_3_3.png", dpi=100)
+end
+
+
+struct pmlayer
+    fx1::Array{Float64}
+    fx2::Array{Float64}
+    fx3::Array{Float64}
+    fy1::Array{Float64}
+    fy2::Array{Float64}
+    fy3::Array{Float64}
+    gx2::Array{Float64}
+    gx3::Array{Float64}
+    gy2::Array{Float64}
+    gy3::Array{Float64}
+end
+
+
+function gaussian(t::Int32, t0::Int, sigma::Float64)::Float64
+    return exp(-0.5 * ((t - t0)/sigma)^2)
+end
+
+
+function pmlparam(npml::Int, nx::Int, ny::Int, pml::pmlayer)
+    """ calculate the two-dimensional perfectly matched layer (PML) parameters """
+    for n in (1:npml)
+        xm = 0.33 * ((npml-n+1)/npml)^3
+        xn = 0.33 * ((npml-n+1-0.5)/npml)^3
+        pml.fx1[n] = pml.fx1[nx-n] = pml.fy1[n] = pml.fy1[ny-n] = xn
+        pml.fx2[n] = pml.fx2[nx-n] = pml.fy2[n] = pml.fy2[ny-n] = 1/(1+xn)
+        pml.gx2[n] = pml.gx2[nx+1-n] = pml.gy2[n] = pml.gy2[ny+1-n] = 1/(1+xm)
+        pml.fx3[n] = pml.fx3[nx-n] = pml.fy3[n] = pml.fy3[ny-n] = (1-xn)/(1+xn)
+        pml.gx3[n] = pml.gx3[nx+1-n] = pml.gy3[n] = pml.gy3[ny+1-n] = (1-xm)/(1+xm)
+    end
+end
+
+
+function ezinct(ny::Int, ezi::Array{Float64}, hxi::Array{Float64}, bc::Array{Float64})
+    """ calculate the incident Ez """
+    @views ezi[2:ny] .+= 0.5 .* (hxi[1:ny-1] .- hxi[2:ny])
+    # absorbing boundary conditions
+    ezi[1], bc[1], bc[2] = bc[1], bc[2], ezi[2]
+    ezi[ny], bc[4], bc[3] = bc[4], bc[3], ezi[ny-1]
+end
+
+
+function dfield(t::Int32, nx::Int, ny::Int, pml::pmlayer, ezi::Array{Float64}, dz::Array{Float64}, hx::Array{Float64}, hy::Array{Float64})
+    """ calculate the electric flux density Dz """
+    @views dz[2:nx,2:ny] .= pml.gx3[2:nx] .* pml.gy3[2:ny]' .* dz[2:nx,2:ny] .+ pml.gx2[2:nx] .* pml.gy2[2:ny]' .* 0.5 .* (hy[2:nx,2:ny] .- hy[1:nx-1,2:ny] .- hx[2:nx,2:ny] .+ hx[2:nx,1:ny-1])
+    # put a Gaussian pulse at the low end
+    ezi[4] = gaussian(t, 20, 8.0)
+end
+
+
+function inctdz(nx::Int, ny::Int, npml::Int, hxi::Array{Float64}, dz::Array{Float64})
+    """ incident Dz values """
+    @views dz[npml:nx-npml+1,npml] .+= 0.5 .* hxi[npml-1]
+    @views dz[npml:nx-npml+1,ny-npml+1] .-= 0.5 .* hxi[ny-npml+1]
+end
+
+
+function efield(nx::Int, ny::Int, naz::Array{Float64}, dz::Array{Float64}, ez::Array{Float64})
+    """ calculate the Ez field from Dz """
+    @views ez[1:nx,1:ny] .= naz[1:nx,1:ny] .* dz[1:nx,1:ny]
+end
+
+
+function hxinct(ny::Int, ezi::Array{Float64}, hxi::Array{Float64})
+    """ calculate the incident Hx """
+    @views hxi[1:ny-1] .+= 0.5 .* (ezi[1:ny-1] .- ezi[2:ny])
+end
+
+
+function hfield(nx::Int, ny::Int, pml::pmlayer, ez::Array{Float64}, ihx::Array{Float64}, ihy::Array{Float64}, hx::Array{Float64}, hy::Array{Float64})
+    """ calculate the Hx and Hy field """
+    curl_em = ez[1:nx-1,1:ny-1] - ez[1:nx-1,2:ny]
+    curl_en = ez[1:nx-1,1:ny-1] - ez[2:nx,1:ny-1]
+    @views ihx[1:nx-1,1:ny-1] .+= curl_em
+    @views ihy[1:nx-1,1:ny-1] .+= curl_en
+    @views hx[1:nx-1,1:ny-1] .= pml.fy3[1:ny-1]' .* hx[1:nx-1,1:ny-1] .+ pml.fy2[1:ny-1]' .* (0.5 .* curl_em .+ pml.fx1[1:nx-1] .* ihx[1:nx-1,1:ny-1])
+    @views hy[1:nx-1,1:ny-1] .= pml.fx3[1:nx-1] .* hy[1:nx-1,1:ny-1] .- pml.fx2[1:nx-1] .* (0.5 .* curl_en .+ pml.fy1[1:ny-1]' .* ihy[1:nx-1,1:ny-1])
+end
+
+
+function incthx(nx::Int, ny::Int, npml::Int, ezi::Array{Float64}, hx::Array{Float64})
+    """ incident Hx values """
+    hx[npml:nx-npml+1,npml-1] .+= 0.5 .* ezi[npml]
+    hx[npml:nx-npml+1,ny-npml+1] .-= 0.5 .* ezi[ny-npml+1]
+end
+
+
+function incthy(nx::Int, ny::Int, npml::Int, ezi::Array{Float64}, hy::Array{Float64})
+    """ incident Hy values """
+    hy[npml-1,npml:ny-npml+1] .-= 0.5 .* ezi[npml:ny-npml+1]
+    hy[nx-npml+1,npml:ny-npml+1] .+= 0.5 .* ezi[npml:ny-npml+1]
+end
+
+
+function main()
+
+    nx::Int = 60  # number of grid points
+    ny::Int = 60  # number of grid points
+
+    ns::Int = 115  # number of time steps
+
+    ezi = zeros(Float64, ny)
+    hxi = zeros(Float64, ny)
+
+    dz = zeros(Float64, (nx, ny))
+    ez = zeros(Float64, (nx, ny))
+    hx = zeros(Float64, (nx, ny))
+    hy = zeros(Float64, (nx, ny))
+
+    ihx = zeros(Float64, (nx, ny))
+    ihy = zeros(Float64, (nx, ny))
+
+    bc = zeros(Float64, 4)
+
+    naz = ones(Float64, (nx, ny))
+
+    ds::Float64 = 0.01  # spatial step (m)
+    dt::Float64 = ds/6e8  # time step (s)
+
+    pml = pmlayer(
+        zeros(Float64, nx),
+        ones(Float64, nx),
+        ones(Float64, nx),
+        zeros(Float64, ny),
+        ones(Float64, ny),
+        ones(Float64, ny),
+        ones(Float64, nx),
+        ones(Float64, nx),
+        ones(Float64, ny),
+        ones(Float64, ny),
+    )
+
+    npml::Int = 8  # pml thickness
+    pmlparam(npml, nx, ny, pml)
+
+    for t in Int32.(1:ns)
+        ezinct(ny, ezi, hxi, bc)
+        dfield(t, nx, ny, pml, ezi, dz, hx, hy)
+        inctdz(nx, ny, npml, hxi, dz)
+        efield(nx, ny, naz, dz, ez)
+        hxinct(ny, ezi, hxi)
+        hfield(nx, ny, pml, ez, ihx, ihy, hx, hy)
+        incthx(nx, ny, npml, ezi, hx)
+        incthy(nx, ny, npml, ezi, hy)
+    end
+
+    surfaceplot(ns, nx, ny, ez)
+    contourplot(ns, nx, ny, ez)
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
