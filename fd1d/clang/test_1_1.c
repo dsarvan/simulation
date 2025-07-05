fbd85c6d0215194235318ff69023c616807af6c7
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-07-05 19:32:10 +0530

    simulation of a pulse in free space

--- fd1d/clang/test_1_1.c
+++ fd1d/clang/test_1_1.c
@@ -11,7 +11,7 @@
 
 
 float gaussian(int t, int t0, float sigma) {
-    return exp(-0.5*((t-t0)/sigma)*((t-t0)/sigma));
+    return exp(-0.5*(t - t0)/sigma*(t - t0)/sigma);
 }
 
 
@@ -20,7 +20,7 @@ void exfield(int t, int nx, float *ex, float *hy) {
     for (int i = 1; i < nx; i++)
         ex[i] += 0.5 * (hy[i-1] - hy[i]);
     /* put a Gaussian pulse in the middle */
-    ex[nx/2] = gaussian(t, 40, 12);
+    ex[nx/2] = gaussian(t, 40, 12.0f);
 }
 
 
