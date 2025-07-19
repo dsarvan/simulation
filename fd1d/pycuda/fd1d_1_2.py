f87eee1361facc093d55460be622eecc18313d7b
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-07-19 19:32:10 +0530

    simulation of a pulse with absorbing boundary conditions

--- fd1d/pycuda/fd1d_1_2.py
+++ fd1d/pycuda/fd1d_1_2.py
@@ -18,7 +18,7 @@ plt.style.use("../pyplot.mplstyle")
 
 
 def visualize(ns: int, nx: int, ex: np.ndarray) -> None:
-    fig, ax = plt.subplots(figsize=(8,3), gridspec_kw={"hspace": 0.2})
+    fig, ax = plt.subplots(figsize=(8,3), gridspec_kw={"hspace":0.2})
     fig.suptitle(r"FDTD simulation of a pulse with absorbing boundary conditions")
     ax.plot(ex, color="black", linewidth=1)
     ax.set(xlim=(0, nx-1), ylim=(-1.2, 1.2))
@@ -26,27 +26,27 @@ def visualize(ns: int, nx: int, ex: np.ndarray) -> None:
     ax.set(xlabel=r"$z\;(cm)$", ylabel=r"$E_x\;(V/m)$")
     ax.text(0.02, 0.90, rf"$T$ = {ns}", transform=ax.transAxes)
     plt.subplots_adjust(bottom=0.2, hspace=0.45)
-    plt.show()
+    plt.savefig("fd1d_1_2.png", dpi=100)
 
 
 kernel = """
-#define idx (blockIdx.x * blockDim.x + threadIdx.x)
-#define stx (blockDim.x * gridDim.x)
+#define idx blockIdx.x*blockDim.x+threadIdx.x
+#define stx blockDim.x*gridDim.x
 
 
 __device__
 float gaussian(int t, int t0, float sigma) {
-    return exp(-0.5 * ((t - t0)/sigma) * ((t - t0)/sigma));
+    return exp(-0.5*(t - t0)/sigma*(t - t0)/sigma);
 }
 
 
 __global__
 void exfield(int t, int nx, float *ex, float *hy) {
     /* calculate the Ex field */
-    for (int i = idx + 1; i < nx; i += stx)
-        ex[i] = ex[i] + 0.5 * (hy[i-1] - hy[i]);
+    for (int i = idx+1; i < nx; i += stx)
+        ex[i] += 0.5 * (hy[i-1] - hy[i]);
     /* put a Gaussian pulse in the middle */
-    if (idx == nx/2) ex[nx/2] = gaussian(t, 40, 12);
+    if (idx == nx/2) ex[nx/2] = gaussian(t, 40, 12.0f);
 }
 
 
@@ -56,8 +56,8 @@ void hyfield(int nx, float *ex, float *hy, float *bc) {
     if (idx == 0) ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
     if (idx == nx-1) ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
     /* calculate the Hy field */
-    for (int i = idx; i < nx - 1; i += stx)
-        hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
+    for (int i = idx; i < nx-1; i += stx)
+        hy[i] += 0.5 * (ex[i] - ex[i+1]);
 }
 """
 
