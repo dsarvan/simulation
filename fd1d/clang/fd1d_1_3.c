5fedc728c6f9206f802774cbb578ce38e33b195e
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-07-26 19:32:10 +0530

    simulation of a pulse hitting a dielectric medium

--- fd1d/clang/fd1d_1_3.c
+++ fd1d/clang/fd1d_1_3.c
@@ -10,16 +10,16 @@
 
 
 double gaussian(int t, int t0, double sigma) {
-    return exp(-0.5 * ((t - t0)/sigma) * ((t - t0)/sigma));
+    return exp(-0.5*(t - t0)/sigma*(t - t0)/sigma);
 }
 
 
 void exfield(int t, int nx, double *cb, double *ex, double *hy) {
     /* calculate the Ex field */
     for (int i = 1; i < nx; i++)
-        ex[i] = ex[i] + cb[i] * (hy[i-1] - hy[i]);
+        ex[i] += cb[i] * (hy[i-1] - hy[i]);
     /* put a Gaussian pulse at the low end */
-    ex[1] = ex[1] + gaussian(t, 40, 12);
+    ex[1] += gaussian(t, 40, 12.0);
 }
 
 
@@ -28,14 +28,14 @@ void hyfield(int nx, double *ex, double *hy, double *bc) {
     ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
     ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
     /* calculate the Hy field */
-    for (int i = 0; i < nx - 1; i++)
-        hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
+    for (int i = 0; i < nx-1; i++)
+        hy[i] += 0.5 * (ex[i] - ex[i+1]);
 }
 
 
 double *dielectric(int nx, double epsr) {
-    double *cb = (double *) calloc(nx, sizeof(*cb));
-    for (int i = 0; i < nx; cb[i] = 0.5f, i++);
+    double *cb = (double*) calloc(nx, sizeof(*cb));
+    for (int i = 0; i < nx; cb[i] = 0.5, i++);
     for (int i = nx/2; i < nx; cb[i] = 0.5/epsr, i++);
     return cb;
 }
@@ -46,12 +46,12 @@ int main() {
     int nx = 512;  /* number of grid points */
     int ns = 740;  /* number of time steps */
 
-    double *ex = (double *) calloc(nx, sizeof(*ex));
-    double *hy = (double *) calloc(nx, sizeof(*hy));
+    double *ex = (double*) calloc(nx, sizeof(*ex));
+    double *hy = (double*) calloc(nx, sizeof(*hy));
 
-    double bc[4] = {0.0f};
+    double bc[4] = {0.0};
 
-    double epsr = 4;  /* relative permittivity */
+    double epsr = 4.0;  /* relative permittivity */
     double *cb = dielectric(nx, epsr);
 
     for (int t = 1; t <= ns; t++) {
