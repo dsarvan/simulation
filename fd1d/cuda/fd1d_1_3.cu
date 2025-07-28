6d40bfe00abb02abd1f72e4c9884a138a96cfa55
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-07-28 19:32:10 +0530

    simulation of a pulse hitting a dielectric medium

--- fd1d/cuda/fd1d_1_3.cu
+++ fd1d/cuda/fd1d_1_3.cu
@@ -9,23 +9,23 @@
 #include <math.h>
 #include <cuda_runtime.h>
 
-#define idx (blockIdx.x * blockDim.x + threadIdx.x)
-#define stx (blockDim.x * gridDim.x)
+#define idx blockIdx.x*blockDim.x+threadIdx.x
+#define stx blockDim.x*gridDim.x
 
 
 __device__
 float gaussian(int t, int t0, float sigma) {
-    return exp(-0.5 * ((t - t0)/sigma) * ((t - t0)/sigma));
+    return exp(-0.5*(t - t0)/sigma*(t - t0)/sigma);
 }
 
 
 __global__
 void exfield(int t, int nx, float *cb, float *ex, float *hy) {
     /* calculate the Ex field */
-    for (int i = idx + 1; i < nx; i += stx)
-        ex[i] = ex[i] + cb[i] * (hy[i-1] - hy[i]);
+    for (int i = idx+1; i < nx; i += stx)
+        ex[i] += cb[i] * (hy[i-1] - hy[i]);
     /* put a Gaussian pulse at the low end */
-    if (idx == 1) ex[1] = ex[1] + gaussian(t, 40, 12);
+    if (idx == 1) ex[1] += gaussian(t, 40, 12.0f);
 }
 
 
@@ -35,8 +35,8 @@ void hyfield(int nx, float *ex, float *hy, float *bc) {
     if (idx == 0) ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
     if (idx == nx-1) ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
     /* calculate the Hy field */
-    for (int i = idx; i < nx - 1; i += stx)
-        hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
+    for (int i = idx; i < nx-1; i += stx)
+        hy[i] += 0.5 * (ex[i] - ex[i+1]);
 }
 
 
@@ -69,7 +69,7 @@ int main() {
     cudaMallocManaged(&bc, 4*sizeof(*bc));
     for (int i = 0; i < 4; bc[i] = 0.0f, i++);
 
-    float epsr = 4;  /* relative permittivity */
+    float epsr = 4.0;  /* relative permittivity */
     float *cb = dielectric(nx, epsr);
 
     int numSM;
@@ -86,8 +86,8 @@ int main() {
 
     cudaDeviceSynchronize();
 
-    cudaFree(bc);
     cudaFree(cb);
+    cudaFree(bc);
     cudaFree(ex);
     cudaFree(hy);
 
