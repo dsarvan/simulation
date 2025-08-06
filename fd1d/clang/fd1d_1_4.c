8eee6d6af6f5d0b21d8d83f5c4ec485b4d5bb8b0
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-08-06 19:32:10 +0530

    simulation of a propagating sinusoidal striking a dielectric medium

--- fd1d/clang/fd1d_1_4.c
+++ fd1d/clang/fd1d_1_4.c
@@ -11,16 +11,16 @@
 
 double sinusoidal(int t, double ds, double freq) {
     double dt = ds/6e8;  /* time step (s) */
-    return sin(2 * M_PI * freq * dt * t);
+    return sin(2*M_PI*freq*dt*t);
 }
 
 
 void exfield(int t, int nx, double *cb, double *ex, double *hy) {
     /* calculate the Ex field */
     for (int i = 1; i < nx; i++)
-        ex[i] = ex[i] + cb[i] * (hy[i-1] - hy[i]);
+        ex[i] += cb[i] * (hy[i-1] - hy[i]);
     /* put a sinusoidal wave at the low end */
-    ex[1] = ex[1] + sinusoidal(t, 0.01, 700e6);
+    ex[1] += sinusoidal(t, 0.01, 700e6);
 }
 
 
@@ -29,14 +29,14 @@ void hyfield(int nx, double *ex, double *hy, double *bc) {
     ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
     ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
     /* calculate the Hy field */
-    for (int i = 0; i < nx - 1; i++)
-        hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
+    for (int i = 0; i < nx-1; i++)
+        hy[i] += 0.5 * (ex[i] - ex[i+1]);
 }
 
 
 double *dielectric(int nx, double epsr) {
-    double *cb = (double *) calloc(nx, sizeof(*cb));
-    for (int i = 0; i < nx; cb[i] = 0.5f, i++);
+    double *cb = (double*) calloc(nx, sizeof(*cb));
+    for (int i = 0; i < nx; cb[i] = 0.5, i++);
     for (int i = nx/2; i < nx; cb[i] = 0.5/epsr, i++);
     return cb;
 }
@@ -47,14 +47,14 @@ int main() {
     int nx = 512;  /* number of grid points */
     int ns = 740;  /* number of time steps */
 
-    double *ex = (double *) calloc(nx, sizeof(*ex));
-    double *hy = (double *) calloc(nx, sizeof(*hy));
+    double *ex = (double*) calloc(nx, sizeof(*ex));
+    double *hy = (double*) calloc(nx, sizeof(*hy));
 
-    double bc[4] = {0.0f};
+    double bc[4] = {0.0};
 
     double ds = 0.01;  /* spatial step (m) */
     double dt = ds/6e8;  /* time step (s) */
-    double epsr = 4;  /* relative permittivity */
+    double epsr = 4.0;  /* relative permittivity */
     double *cb = dielectric(nx, epsr);
 
     for (int t = 1; t <= ns; t++) {
