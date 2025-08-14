99936537ea9c10997e480fc07ced62358de96f79
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-08-14 19:32:10 +0530

    simulation of a propagating sinusoidal striking a lossy dielectric medium

--- fd1d/animation/fd1d_1_5.py
+++ fd1d/animation/fd1d_1_5.py
@@ -16,16 +16,16 @@ plt.style.use("../pyplot.mplstyle")
 
 def sinusoidal(t: int, ds: float, freq: float) -> float:
     dt: float = ds/6e8  # time step (s)
-    return np.sin(2 * np.pi * freq * dt * t)
+    return np.sin(2*np.pi*freq*dt*t)
 
 
 def dielectric(nx: int, dt: float, epsr: float, sigma: float) -> tuple:
-    ca = 1.0 * np.ones(nx, dtype=np.float64)
-    cb = 0.5 * np.ones(nx, dtype=np.float64)
+    ca = 1.0 + np.zeros(nx, dtype=np.float64)
+    cb = 0.5 + np.zeros(nx, dtype=np.float64)
     eps0: float = 8.854e-12  # vacuum permittivity (F/m)
-    epsf: float = dt * sigma/(2 * eps0 * epsr)
+    epsf: float = dt*sigma/(2*eps0*epsr)
     ca[nx//2:] = (1 - epsf)/(1 + epsf)
-    cb[nx//2:] = 0.5/(epsr * (1 + epsf))
+    cb[nx//2:] = 0.5/(epsr*(1 + epsf))
     return ca, cb
 
 
@@ -41,7 +41,7 @@ def main():
 
     ds: float = 0.01  # spatial step (m)
     dt: float = ds/6e8  # time step (s)
-    epsr: float = 4  # relative permittivity
+    epsr: float = 4.0  # relative permittivity
     sigma: float = 0.04  # conductivity (S/m)
     ca, cb = dielectric(nx, dt, epsr, sigma)
 
@@ -51,9 +51,9 @@ def main():
     writer = fwriter(fps=15, codec='h264', bitrate=2000, metadata=data)
 
     # draw an empty plot, but preset the plot x- and y- limits
-    fig, ax = plt.subplots(figsize=(8,3), gridspec_kw={"hspace": 0.2})
+    fig, ax = plt.subplots(figsize=(8,3), gridspec_kw={"hspace":0.2})
     fig.suptitle(r"FDTD simulation of a sinusoidal striking lossy dielectric material")
-    medium = (0.5/cb - 1)/(epsr - 1)*1e3 if epsr > 1 else (0.5/cb - 1)
+    medium = (0.5/cb-1)/(epsr-1)*1e3 if epsr > 1 else (0.5/cb-1)
     medium[medium==0] = -1e3
     axline, = ax.plot(ex, color="black", linewidth=1)
     ax.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
@@ -70,12 +70,12 @@ def main():
             # calculate the Ex field
             ex[1:nx] = ca[1:nx] * ex[1:nx] + cb[1:nx] * (hy[0:nx-1] - hy[1:nx])
             # put a sinusoidal wave at the low end
-            ex[1] = ex[1] + sinusoidal(t, 0.01, 700e6)
+            ex[1] += sinusoidal(t, 0.01, 700e6)
             # absorbing boundary conditions
             ex[0], bc[0], bc[1] = bc[0], bc[1], ex[1]
             ex[nx-1], bc[3], bc[2] = bc[3], bc[2], ex[nx-2]
             # calculate the Hy field
-            hy[0:nx-1] = hy[0:nx-1] + 0.5 * (ex[0:nx-1] - ex[1:nx])
+            hy[0:nx-1] += 0.5 * (ex[0:nx-1] - ex[1:nx])
 
             axline.set_ydata(ex)
             axtime.set_text(rf"$T$ = {t}")
