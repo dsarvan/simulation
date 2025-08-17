fcc9d37dfb249449acee9ad8580cbdf90299bce2
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-08-17 19:32:10 +0530

    simulation of a propagating sinusoidal striking a lossy dielectric medium

--- fd1d/clang/fd1d_1_5.c
+++ fd1d/clang/fd1d_1_5.c
@@ -18,7 +18,7 @@ typedef struct {
 
 double sinusoidal(int t, double ds, double freq) {
     double dt = ds/6e8;  /* time step (s) */
-    return sin(2 * M_PI * freq * dt * t);
+    return sin(2*M_PI*freq*dt*t);
 }
 
 
@@ -27,7 +27,7 @@ void exfield(int t, int nx, double *ca, double *cb, double *ex, double *hy) {
     for (int i = 1; i < nx; i++)
         ex[i] = ca[i] * ex[i] + cb[i] * (hy[i-1] - hy[i]);
     /* put a sinusoidal wave at the low end */
-    ex[1] = ex[1] + sinusoidal(t, 0.01, 700e6);
+    ex[1] += sinusoidal(t, 0.01, 700e6);
 }
 
 
@@ -36,21 +36,21 @@ void hyfield(int nx, double *ex, double *hy, double *bc) {
     ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
     ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
     /* calculate the Hy field */
-    for (int i = 0; i < nx - 1; i++)
-        hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
+    for (int i = 0; i < nx-1; i++)
+        hy[i] += 0.5 * (ex[i] - ex[i+1]);
 }
 
 
 tuple dielectric(int nx, double dt, double epsr, double sigma) {
     tuple n;
-    n.ca = (double *) calloc(nx, sizeof(*n.ca));
-    n.cb = (double *) calloc(nx, sizeof(*n.cb));
-    for (int i = 0; i < nx; n.ca[i] = 1.0f, i++);
-    for (int i = 0; i < nx; n.cb[i] = 0.5f, i++);
+    n.ca = (double*) calloc(nx, sizeof(*n.ca));
+    n.cb = (double*) calloc(nx, sizeof(*n.cb));
+    for (int i = 0; i < nx; n.ca[i] = 1.0, i++);
+    for (int i = 0; i < nx; n.cb[i] = 0.5, i++);
     double eps0 = 8.854e-12;  /* vacuum permittivity (F/m) */
-    double epsf = dt * sigma/(2 * eps0 * epsr);
+    double epsf = dt*sigma/(2*eps0*epsr);
     for (int i = nx/2; i < nx; n.ca[i] = (1 - epsf)/(1 + epsf), i++);
-    for (int i = nx/2; i < nx; n.cb[i] = 0.5/(epsr * (1 + epsf)), i++);
+    for (int i = nx/2; i < nx; n.cb[i] = 0.5/(epsr*(1 + epsf)), i++);
     return n;
 }
 
@@ -60,14 +60,14 @@ int main() {
     int nx = 512;  /* number of grid points */
     int ns = 740;  /* number of time steps */
 
-    double *ex = (double *) calloc(nx, sizeof(*ex));
-    double *hy = (double *) calloc(nx, sizeof(*hy));
+    double *ex = (double*) calloc(nx, sizeof(*ex));
+    double *hy = (double*) calloc(nx, sizeof(*hy));
 
-    double bc[4] = {0.0f};
+    double bc[4] = {0.0};
 
     double ds = 0.01;  /* spatial step (m) */
     double dt = ds/6e8;  /* time step (s) */
-    double epsr = 4;  /* relative permittivity */
+    double epsr = 4.0;  /* relative permittivity */
     double sigma = 0.04;  /* conductivity (S/m) */
     tuple n = dielectric(nx, dt, epsr, sigma);
 
