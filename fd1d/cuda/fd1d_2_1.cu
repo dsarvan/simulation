1ca06a58fd1ee735804cce0e72620dc1a6f2ea28
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-08-30 19:32:10 +0530

    simulation of a propagating sinusoidal striking a lossy dielectric medium

--- fd1d/cuda/fd1d_2_1.cu
+++ fd1d/cuda/fd1d_2_1.cu
@@ -10,8 +10,8 @@
 #include <math.h>
 #include <cuda_runtime.h>
 
-#define idx (blockIdx.x * blockDim.x + threadIdx.x)
-#define stx (blockDim.x * gridDim.x)
+#define idx blockIdx.x*blockDim.x+threadIdx.x
+#define stx blockDim.x*gridDim.x
 
 
 typedef struct {
@@ -22,26 +22,26 @@ typedef struct {
 __device__
 float sinusoidal(int t, float ds, float freq) {
     float dt = ds/6e8;  /* time step (s) */
-    return sin(2 * M_PI * freq * dt * t);
+    return sin(2*M_PI*freq*dt*t);
 }
 
 
 __global__
 void dxfield(int t, int nx, float *dx, float *hy) {
     /* calculate the electric flux density Dx */
-    for (int i = idx + 1; i < nx; i += stx)
-        dx[i] = dx[i] + 0.5 * (hy[i-1] - hy[i]);
+    for (int i = idx+1; i < nx; i += stx)
+        dx[i] += 0.5 * (hy[i-1] - hy[i]);
     /* put a sinusoidal wave at the low end */
-    if (idx == 1) dx[1] = dx[1] + sinusoidal(t, 0.01, 700e6);
+    if (idx == 1) dx[1] += sinusoidal(t, 0.01f, 700e6);
 }
 
 
 __global__
 void exfield(int nx, medium *md, float *dx, float *ix, float *ex) {
     /* calculate the Ex field from Dx */
-    for (int i = idx + 1; i < nx; i += stx) {
+    for (int i = idx+1; i < nx; i += stx) {
         ex[i] = md->nax[i] * (dx[i] - ix[i]);
-        ix[i] = ix[i] + md->nbx[i] * ex[i];
+        ix[i] += md->nbx[i] * ex[i];
     }
 }
 
@@ -52,8 +52,8 @@ void hyfield(int nx, float *ex, float *hy, float *bc) {
     if (idx == 0) ex[0] = bc[0], bc[0] = bc[1], bc[1] = ex[1];
     if (idx == nx-1) ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
     /* calculate the Hy field */
-    for (int i = idx; i < nx - 1; i += stx)
-        hy[i] = hy[i] + 0.5 * (ex[i] - ex[i+1]);
+    for (int i = idx; i < nx-1; i += stx)
+        hy[i] += 0.5 * (ex[i] - ex[i+1]);
 }
 
 
@@ -62,10 +62,11 @@ medium dielectric(int nx, float dt, float epsr, float sigma) {
     cudaMallocManaged(&md.nax, nx*sizeof(*md.nax));
     cudaMallocManaged(&md.nbx, nx*sizeof(*md.nbx));
     for (int i = 0; i < nx; md.nax[i] = 1.0f, i++);
+    for (int i = 0; i < nx; md.nbx[i] = 0.0f, i++);
     float eps0 = 8.854e-12;  /* vacuum permittivity (F/m) */
     for (int i = nx/2; i < nx; i++) {
-        md.nax[i] = 1/(epsr + (sigma * dt/eps0));
-        md.nbx[i] = sigma * dt/eps0;
+        md.nax[i] = 1/(epsr + sigma*dt/eps0);
+        md.nbx[i] = sigma*dt/eps0;
     }
     return md;
 }
@@ -97,7 +98,7 @@ int main() {
 
     float ds = 0.01;  /* spatial step (m) */
     float dt = ds/6e8;  /* time step (s) */
-    float epsr = 4;  /* relative permittivity */
+    float epsr = 4.0;  /* relative permittivity */
     float sigma = 0.04;  /* conductivity (S/m) */
     medium md = dielectric(nx, dt, epsr, sigma);
 
@@ -116,9 +117,9 @@ int main() {
 
     cudaDeviceSynchronize();
 
-    cudaFree(bc);
     cudaFree(md.nax);
     cudaFree(md.nbx);
+    cudaFree(bc);
     cudaFree(dx);
     cudaFree(ex);
     cudaFree(ix);
