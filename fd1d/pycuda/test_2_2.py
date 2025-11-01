d4f0e6798b46a741c1a00202ec4bc288b7d42953
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-11-01 19:57:29 +0530

    implements the discrete fourier transform with a gaussian pulse as its source

--- fd1d/pycuda/test_2_2.py
+++ fd1d/pycuda/test_2_2.py
@@ -23,35 +23,33 @@ plt.style.use("../pyplot.mplstyle")
 def visualize(ns: int, nx: int, epsr: float, sigma: float, nax: np.ndarray, ex: np.ndarray) -> None:
     fig, ax = plt.subplots(figsize=(8,3), gridspec_kw={"hspace":0.2})
     fig.suptitle(r"FDTD simulation of a pulse striking dielectric material")
-    medium = (1-nax)/(1-nax[-1])*1e3 if epsr > 1 else (1-nax)
-    medium[medium==0] = -1e3
-    ax.plot(ex, color="black", linewidth=1)
-    ax.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
+    medium = np.where(1.0/nax-1)[0] if epsr > 1 else (1.0/nax-1)
+    ax.plot(range(nx), ex, color="k", linewidth=1.0)
+    ax.axvspan(medium[0], medium[-1], color="y", alpha=0.3)
     ax.set(xlim=(0, nx-1), ylim=(-1.2, 1.2))
-    ax.set(xticks=range(0, nx+1, round(nx//10,-1)))
+    ax.set(xticks=range(0, nx+1, int(np.ceil(nx/500)*50)))
     ax.set(xlabel=r"$z\;(cm)$", ylabel=r"$E_x\;(V/m)$")
     ax.text(0.02, 0.90, rf"$T$ = {ns}", transform=ax.transAxes)
     ax.text(0.90, 0.90, rf"$\epsilon_r$ = {epsr}", transform=ax.transAxes)
     ax.text(0.85, 0.80, rf"$\sigma$ = {sigma} $S/m$", transform=ax.transAxes)
     plt.subplots_adjust(bottom=0.2, hspace=0.45)
-    plt.show()
+    plt.savefig("test_2_2.png", dpi=100)
 
 
 def amplitude(ns: int, nx: int, epsr: float, sigma: float, nax: np.ndarray, amp: np.ndarray) -> None:
     fig, ax = plt.subplots(figsize=(8,3), gridspec_kw={"hspace":0.2})
     fig.suptitle(r"The discrete Fourier transform with pulse as its source")
-    medium = (1-nax)/(1-nax[-1])*1e3 if epsr > 1 else (1-nax)
-    medium[medium==0] = -1e3
-    ax.plot(amp, color="black", linewidth=1)
-    ax.fill_between(range(nx), medium, medium[0], color='y', alpha=0.3)
+    medium = np.where(1.0/nax-1)[0] if epsr > 1 else (1.0/nax-1)
+    ax.plot(range(nx), amp, color="k", linewidth=1.0)
+    ax.axvspan(medium[0], medium[-1], color="y", alpha=0.3)
     ax.set(xlim=(0, nx-1), ylim=(-0.2, 2.2))
-    ax.set(xticks=range(0, nx+1, round(nx//10,-1)))
-    ax.set(xlabel=r"$z\;(cm)$", ylabel=r"$Amp\;(V)$")
+    ax.set(xticks=range(0, nx+1, int(np.ceil(nx/500)*50)))
+    ax.set(xlabel=r"$z\;(cm)$", ylabel=r"$Amplitude$")
     ax.text(0.02, 0.90, rf"$T$ = {ns}", transform=ax.transAxes)
     ax.text(0.90, 0.90, rf"$\epsilon_r$ = {epsr}", transform=ax.transAxes)
     ax.text(0.85, 0.80, rf"$\sigma$ = {sigma} $S/m$", transform=ax.transAxes)
     plt.subplots_adjust(bottom=0.2, hspace=0.45)
-    plt.show()
+    plt.savefig("test_amp_2_2.png", dpi=100)
 
 
 medium = namedtuple('medium', (
@@ -83,23 +81,21 @@ typedef struct {
 
 __device__
 float gaussian(int t, int t0, float sigma) {
-    return exp(-0.5*(t - t0)/sigma*(t - t0)/sigma);
+    return expf(-0.5f*(t - t0)/sigma*(t - t0)/sigma);
 }
 
 
 __global__
 void fourier(int t, int nf, int nx, float dt, float *freq, float *ex, ftrans *ft) {
     for (int n = threadIdx.y; n < nf; n += blockDim.y) {
+        /* calculate the Fourier transform of input source */
+        if (idx == 0) ft->r_in[n] += cosf(2*M_PI*freq[n]*dt*t) * ex[10];
+        if (idx == 0) ft->i_in[n] -= sinf(2*M_PI*freq[n]*dt*t) * ex[10];
         for (int i = idx; i < nx; i += stx) {
             /* calculate the Fourier transform of Ex field */
             int m = n*nx+i;
-            ft->r_pt[m] += cos(2*M_PI*freq[n]*dt*t) * ex[i];
-            ft->i_pt[m] -= sin(2*M_PI*freq[n]*dt*t) * ex[i];
-        }
-        if (idx == 0 && t < nx/2) {
-            /* calculate the Fourier transform of input source */
-            ft->r_in[n] += cos(2*M_PI*freq[n]*dt*t) * ex[10];
-            ft->i_in[n] -= sin(2*M_PI*freq[n]*dt*t) * ex[10];
+            ft->r_pt[m] += cosf(2*M_PI*freq[n]*dt*t) * ex[i];
+            ft->i_pt[m] -= sinf(2*M_PI*freq[n]*dt*t) * ex[i];
         }
     }
 }
@@ -109,7 +105,7 @@ __global__
 void dxfield(int t, int nx, float *dx, float *hy) {
     /* calculate the electric flux density Dx */
     for (int i = idx+1; i < nx; i += stx)
-        dx[i] += 0.5 * (hy[i-1] - hy[i]);
+        dx[i] += 0.5f * (hy[i-1] - hy[i]);
     /* put a Gaussian pulse at the low end */
     if (idx == 1) dx[1] += gaussian(t, 50, 10.0f);
 }
@@ -132,7 +128,7 @@ void hyfield(int nx, float *ex, float *hy, float *bc) {
     if (idx == nx-1) ex[nx-1] = bc[3], bc[3] = bc[2], bc[2] = ex[nx-2];
     /* calculate the Hy field */
     for (int i = idx; i < nx-1; i += stx)
-        hy[i] += 0.5 * (ex[i] - ex[i+1]);
+        hy[i] += 0.5f * (ex[i] - ex[i+1]);
 }
 """
 
