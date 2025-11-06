5f4907ef351da29d3c8c0a3c827fe6f082201931
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-11-06 13:08:14 +0530

    simulation of a sinusoidal in free space in the transverse magnetic mode with pml

--- fd2d/julia/fd2d_3_2.jl
+++ fd2d/julia/fd2d_3_2.jl
@@ -15,43 +15,41 @@ plt.matplotlib.style.use("../pyplot.mplstyle")
 meshgrid(xs, ys) = xs'.+ 0 .*ys, ys.+ 0 .*xs'
 
 
-function surfaceplot(ns::Int, nx::Int, ny::Int, ez::Array{Float64})::Nothing
+function surfaceplot(ns::Int, nx::Int, ny::Int, npml::Int, ez::Array{Float64})::Nothing
     fig, ax = plt.subplots(subplot_kw=Dict("projection"=>"3d"))
     fig.suptitle(raw"FDTD simulation of a sinusoidal in free space with PML")
-    yv, xv = meshgrid(0:ny-1, 0:nx-1)
-    ax.plot_surface(xv, yv, ez, rstride=1, cstride=1, cmap="gray", lw=0.25)
-    ax.text2D(0.1, 0.7, raw"$T$ = "*"$ns", transform=ax.transAxes)
+    yv, xv = meshgrid(0:ny-1, 0:nx-1); levels = [0.50,1.50]
+    pmlmsk = ((xv .< npml).|(xv .>= nx-npml).|(yv .< npml).|(yv .>= ny-npml))
+    ax.plot_surface(xv, yv, ez, rstride=1, cstride=1, cmap="gray", lw=10/nx)
+    ax.contourf(xv, yv, pmlmsk, levels, offset=0, colors="k", alpha=0.40)
+    ax.text2D(0.1, 0.7, "\$T\$ = $ns", transform=ax.transAxes)
     ax.set(xlim=(0, nx), ylim=(0, ny), zlim=(0, 1))
-    ax.set(xlabel=raw"$x\;(cm)$", ylabel=raw"$y\;(cm)$", zlabel=raw"$E_z\;(V/m)$")
+    ax.set(xlabel="\$x\\;(cm)\$", ylabel="\$y\\;(cm)\$", zlabel="\$E_z\\;(V/m)\$")
     ax.zaxis.set_rotate_label(false); ax.view_init(elev=20.0, azim=45)
     plt.savefig("fd2d_surface_3_2.png", dpi=100)
 end
 
 
-function contourplot(ns::Int, nx::Int, ny::Int, ez::Array{Float64})::Nothing
+function contourplot(ns::Int, nx::Int, ny::Int, npml::Int, ez::Array{Float64})::Nothing
     fig, ax = plt.subplots(figsize=(4,4), gridspec_kw=Dict("hspace"=>0.2))
     fig.suptitle(raw"FDTD simulation of a sinusoidal in free space with PML")
-    yv, xv = meshgrid(0:ny-1, 0:nx-1)
-    ax.contourf(xv, yv, ez, cmap="gray", alpha=0.75)
-    ax.contour(xv, yv, ez, colors="k", linewidths=0.25)
+    yv, xv = meshgrid(0:ny-1, 0:nx-1); ezmax = maximum(abs.(ez))
+    levels = range(-ezmax, ezmax, Int(2/0.04))
+    pmlmsk = ((xv .< npml).|(xv .>= nx-npml).|(yv .< npml).|(yv .>= ny-npml))
+    ax.contour(xv, yv, ez, levels, cmap="gray", alpha=1.0, linewidths=1.5)
+    ax.contourf(xv, yv, pmlmsk, levels=[0.50,1.50], colors="k", alpha=0.40)
     ax.set(xlim=(0, nx-1), ylim=(0, ny-1), aspect="equal")
-    ax.set(xlabel=raw"$x\;(cm)$", ylabel=raw"$y\;(cm)$")
+    ax.set(xlabel="\$x\\;(cm)\$", ylabel="\$y\\;(cm)\$")
     plt.subplots_adjust(bottom=0.2, hspace=0.45)
     plt.savefig("fd2d_contour_3_2.png", dpi=100)
 end
 
 
 struct pmlayer
-    fx1::Array{Float64}
-    fx2::Array{Float64}
-    fx3::Array{Float64}
-    fy1::Array{Float64}
-    fy2::Array{Float64}
-    fy3::Array{Float64}
-    gx2::Array{Float64}
-    gx3::Array{Float64}
-    gy2::Array{Float64}
-    gy3::Array{Float64}
+    fx1::Array{Float64}; fx2::Array{Float64}; fx3::Array{Float64}
+    fy1::Array{Float64}; fy2::Array{Float64}; fy3::Array{Float64}
+    gx2::Array{Float64}; gx3::Array{Float64}
+    gy2::Array{Float64}; gy3::Array{Float64}
 end
 
 
@@ -102,10 +100,10 @@ function pmlparam(nx::Int, ny::Int, npml::Int, pml::pmlayer)::Nothing
     for n in 1:npml
         xm = 0.33*((npml-n+1)/npml)^3
         xn = 0.33*((npml-n+0.5)/npml)^3
-        pml.fx1[n] = pml.fx1[nx-n] = pml.fy1[n] = pml.fy1[ny-n] = xn
-        pml.fx2[n] = pml.fx2[nx-n] = pml.fy2[n] = pml.fy2[ny-n] = 1/(1+xn)
+        pml.fx1[n] = pml.fx1[nx+0-n] = pml.fy1[n] = pml.fy1[ny+0-n] = xn
+        pml.fx2[n] = pml.fx2[nx+0-n] = pml.fy2[n] = pml.fy2[ny+0-n] = 1/(1+xn)
         pml.gx2[n] = pml.gx2[nx+1-n] = pml.gy2[n] = pml.gy2[ny+1-n] = 1/(1+xm)
-        pml.fx3[n] = pml.fx3[nx-n] = pml.fy3[n] = pml.fy3[ny-n] = (1-xn)/(1+xn)
+        pml.fx3[n] = pml.fx3[nx+0-n] = pml.fy3[n] = pml.fy3[ny+0-n] = (1-xn)/(1+xn)
         pml.gx3[n] = pml.gx3[nx+1-n] = pml.gy3[n] = pml.gy3[ny+1-n] = (1-xm)/(1+xm)
     end
 end
@@ -113,10 +111,10 @@ end
 
 function main()
 
-    nx::Int = 60  # number of grid points
-    ny::Int = 60  # number of grid points
+    nx::Int = 100  # number of grid points
+    ny::Int = 100  # number of grid points
 
-    ns::Int = 100  # number of time steps
+    ns::Int = 120  # number of time steps
 
     dz = zeros(Float64, (nx, ny))
     ez = zeros(Float64, (nx, ny))
@@ -153,8 +151,8 @@ function main()
         hfield(nx, ny, pml, ez, ihx, ihy, hx, hy)
     end
 
-    surfaceplot(ns, nx, ny, ez)
-    contourplot(ns, nx, ny, ez)
+    surfaceplot(ns, nx, ny, npml, ez)
+    contourplot(ns, nx, ny, npml, ez)
 end
 
 
