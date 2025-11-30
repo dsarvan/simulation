68cce01d5ceabbe31d3a682c14cdb1f822a65a59
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-11-30 19:32:10 +0530

    simulation of a propagating sinusoidal striking a lossy dielectric medium

--- fd1d/program/fd1d_1_5.py
+++ fd1d/program/fd1d_1_5.py
@@ -6,37 +6,41 @@
 """ Simulation of a propagating sinusoidal wave of 700 MHz striking a lossy
 dielectric with a dielectric constant of 4 and conductivity of 0.04 (S/m) """
 
-import matplotlib.pyplot as plt
-import numpy as np
+import numpy
+from numpy import pi, sin
+from matplotlib import pyplot
+from numpy.typing import NDArray
 
-plt.style.use("classic")
-plt.style.use("../pyplot.mplstyle")
+pyplot.style.use("classic")
+pyplot.style.use("pyplot.mplstyle")
+
+type F64Array = NDArray[numpy.float64]
 
 
-def visualize(ns: int, nx: int, epsr: float, sigma: float, cb: np.ndarray, ex: np.ndarray) -> None:
-    fig, ax = plt.subplots(figsize=(8,3), gridspec_kw={"hspace":0.2})
+def visualize(ns: int, nx: int, epsr: float, sigma: float, cb: F64Array, ex: F64Array):
+    fig, axs = pyplot.subplots(figsize=(8,3), gridspec_kw={"hspace":0.2})
     fig.suptitle(r"FDTD simulation of a sinusoidal striking lossy dielectric material")
-    medium = np.where(0.5/cb-1)[0] if epsr > 1 else (0.5/cb-1)
-    ax.plot(range(nx), ex, color="k", linewidth=1.0)
-    ax.axvspan(medium[0], medium[-1], color="y", alpha=0.3)
-    ax.set(xlim=(0, nx-1), ylim=(-1.2, 1.2))
-    ax.set(xticks=range(0, nx+1, int(np.ceil(nx/500)*25)))
-    ax.set(xlabel=r"$z\;(cm)$", ylabel=r"$E_x\;(V/m)$")
-    ax.text(0.02, 0.90, rf"$T$ = {ns}", transform=ax.transAxes)
-    ax.text(0.90, 0.90, rf"$\epsilon_r$ = {epsr}", transform=ax.transAxes)
-    ax.text(0.85, 0.80, rf"$\sigma$ = {sigma} $S/m$", transform=ax.transAxes)
-    plt.subplots_adjust(bottom=0.2, hspace=0.45)
-    plt.savefig("fd1d_1_5.png", dpi=100)
+    medium = numpy.where(0.5/cb-1)[0] if epsr > 1 else (0.5/cb-1)
+    axs.plot(range(nx), ex, color="k", linewidth=1.0)
+    axs.axvspan(medium[0], medium[-1], color="y", alpha=0.3)
+    axs.set(xlim=(0, nx-1), ylim=(-1.2, 1.2))
+    axs.set(xticks=range(0, nx+1, int(numpy.ceil(nx/500)*25)))
+    axs.set(xlabel=r"$z\;(cm)$", ylabel=r"$E_x\;(V/m)$")
+    axs.text(0.02, 0.90, rf"$T$ = {ns}", transform=axs.transAxes)
+    axs.text(0.90, 0.90, rf"$\epsilon_r$ = {epsr}", transform=axs.transAxes)
+    axs.text(0.85, 0.80, rf"$\sigma$ = {sigma} $S/m$", transform=axs.transAxes)
+    fig.subplots_adjust(bottom=0.2, hspace=0.45)
+    fig.savefig("fd1d_1_5.png", dpi=100)
 
 
-def sinusoidal(t: int, ds: float, freq: float) -> float:
+def sinusoidal(ts: int, ds: float, freq: float):
     dt: float = ds/6e8  # time step (s)
-    return np.sin(2*np.pi*freq*dt*t)
+    return sin(2*pi*freq*dt*ts)
 
 
-def dielectric(nx: int, dt: float, epsr: float, sigma: float) -> tuple:
-    ca = 1.0 + np.zeros(nx, dtype=np.float64)
-    cb = 0.5 + np.zeros(nx, dtype=np.float64)
+def dielectric(nx: int, dt: float, epsr: float, sigma: float):
+    ca = 1.0 + numpy.zeros(nx, numpy.float64)
+    cb = 0.5 + numpy.zeros(nx, numpy.float64)
     eps0: float = 8.854e-12  # vacuum permittivity (F/m)
     epsf: float = dt*sigma/(2*eps0*epsr)
     ca[nx//2:] = (1 - epsf)/(1 + epsf)
@@ -49,10 +53,10 @@ def main():
     nx: int = 512  # number of grid points
     ns: int = 740  # number of time steps
 
-    ex = np.zeros(nx, dtype=np.float64)
-    hy = np.zeros(nx, dtype=np.float64)
+    ex = numpy.zeros(nx, numpy.float64)
+    hy = numpy.zeros(nx, numpy.float64)
 
-    bc = np.zeros(4, dtype=np.float64)
+    bc = numpy.zeros(4, numpy.float64)
 
     ds: float = 0.01  # spatial step (m)
     dt: float = ds/6e8  # time step (s)
@@ -60,11 +64,11 @@ def main():
     sigma: float = 0.04  # conductivity (S/m)
     ca, cb = dielectric(nx, dt, epsr, sigma)
 
-    for t in np.arange(1, ns+1).astype(np.int32):
+    for ts in map(int,range(1,ns+1)):
         # calculate the Ex field
         ex[1:nx] = ca[1:nx] * ex[1:nx] + cb[1:nx] * (hy[0:nx-1] - hy[1:nx])
         # put a sinusoidal wave at the low end
-        ex[1] += sinusoidal(t, 0.01, 700e6)
+        ex[1] += sinusoidal(ts, 0.01, 700e6)
         # absorbing boundary conditions
         ex[0], bc[0], bc[1] = bc[0], bc[1], ex[1]
         ex[nx-1], bc[3], bc[2] = bc[3], bc[2], ex[nx-2]
