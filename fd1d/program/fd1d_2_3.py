d9dacfaf43947ee0ab9f31ce19ce2cf4e60d37b7
Saravanan Dayalan <dayalsaravanan@gmail.com>
2025-12-14 19:32:10 +0530

    simulation of a pulse striking a frequency-dependent dielectric medium

--- fd1d/program/fd1d_2_3.py
+++ fd1d/program/fd1d_2_3.py
@@ -6,87 +6,90 @@
 """ Simulation of a pulse striking a frequency-dependent dielectric material and
 implements the discrete Fourier transform with a Gaussian pulse as its source """
 
-import matplotlib.pyplot as plt
-import numpy as np
-from numpy import pi, exp, hypot, sin, cos, arctan2
-from collections import namedtuple
+import numpy
+from matplotlib import pyplot
+from typing import NamedTuple
+from numpy.typing import NDArray
+from numpy import pi, exp, sin, cos, hypot, arctan2
 
-plt.style.use("classic")
-plt.style.use("../pyplot.mplstyle")
+pyplot.style.use("classic")
+pyplot.style.use("pyplot.mplstyle")
+
+type F64Array = NDArray[numpy.float64]
 
 
-def visualize(ns: int, nx: int, epsr: float, sigma: float, nax: np.ndarray, ex: np.ndarray) -> None:
-    fig, ax = plt.subplots(figsize=(8,3), gridspec_kw={"hspace":0.2})
+def visualize(ns: int, nx: int, epsr: float, sigma: float, nax: F64Array, ex: F64Array):
+    fig, axs = pyplot.subplots(figsize=(8,3), gridspec_kw={"hspace":0.2})
     fig.suptitle(r"FDTD simulation of a pulse striking Debye dielectric material")
-    medium = np.where(1.0/nax-1)[0] if epsr > 1 else (1.0/nax-1)
-    ax.plot(range(nx), ex, color="k", linewidth=1.0)
-    ax.axvspan(medium[0], medium[-1], color="y", alpha=0.3)
-    ax.set(xlim=(0, nx-1), ylim=(-1.2, 1.2))
-    ax.set(xticks=range(0, nx+1, int(np.ceil(nx/500)*25)))
-    ax.set(xlabel=r"$z\;(cm)$", ylabel=r"$E_x\;(V/m)$")
-    ax.text(0.02, 0.90, rf"$T$ = {ns}", transform=ax.transAxes)
-    ax.text(0.90, 0.90, rf"$\epsilon_r$ = {epsr}", transform=ax.transAxes)
-    ax.text(0.85, 0.80, rf"$\sigma$ = {sigma} $S/m$", transform=ax.transAxes)
-    plt.subplots_adjust(bottom=0.2, hspace=0.45)
-    plt.savefig("fd1d_2_3.png", dpi=100)
+    medium = numpy.where(1.0/nax-1)[0] if epsr > 1 else (1.0/nax-1)
+    axs.plot(range(nx), ex, color="k", linewidth=1.0)
+    axs.axvspan(medium[0], medium[-1], color="y", alpha=0.3)
+    axs.set(xlim=(0, nx-1), ylim=(-1.2, 1.2))
+    axs.set(xticks=range(0, nx+1, int(numpy.ceil(nx/500)*25)))
+    axs.set(xlabel=r"$z\;(cm)$", ylabel=r"$E_x\;(V/m)$")
+    axs.text(0.02, 0.90, rf"$T$ = {ns}", transform=axs.transAxes)
+    axs.text(0.90, 0.90, rf"$\epsilon_r$ = {epsr}", transform=axs.transAxes)
+    axs.text(0.85, 0.80, rf"$\sigma$ = {sigma} $S/m$", transform=axs.transAxes)
+    fig.subplots_adjust(bottom=0.2, hspace=0.45)
+    fig.savefig("fd1d_2_3.png", dpi=100)
 
 
-def amplitude(ns: int, nx: int, epsr: float, sigma: float, nax: np.ndarray, amp: np.ndarray) -> None:
-    fig, ax = plt.subplots(figsize=(8,3), gridspec_kw={"hspace":0.2})
+def amplitude(ns: int, nx: int, epsr: float, sigma: float, nax: F64Array, amp: F64Array):
+    fig, axs = pyplot.subplots(figsize=(8,3), gridspec_kw={"hspace":0.2})
     fig.suptitle(r"The discrete Fourier transform with pulse as its source")
-    medium = np.where(1.0/nax-1)[0] if epsr > 1 else (1.0/nax-1)
-    ax.plot(range(nx), amp, color="k", linewidth=1.0)
-    ax.axvspan(medium[0], medium[-1], color="y", alpha=0.3)
-    ax.set(xlim=(0, nx-1), ylim=(-0.2, 2.2))
-    ax.set(xticks=range(0, nx+1, int(np.ceil(nx/500)*25)))
-    ax.set(xlabel=r"$z\;(cm)$", ylabel=r"$Amplitude$")
-    ax.text(0.02, 0.90, rf"$T$ = {ns}", transform=ax.transAxes)
-    ax.text(0.90, 0.90, rf"$\epsilon_r$ = {epsr}", transform=ax.transAxes)
-    ax.text(0.85, 0.80, rf"$\sigma$ = {sigma} $S/m$", transform=ax.transAxes)
-    plt.subplots_adjust(bottom=0.2, hspace=0.45)
-    plt.savefig("fd1d_amp_2_3.png", dpi=100)
+    medium = numpy.where(1.0/nax-1)[0] if epsr > 1 else (1.0/nax-1)
+    axs.plot(range(nx), amp, color="k", linewidth=1.0)
+    axs.axvspan(medium[0], medium[-1], color="y", alpha=0.3)
+    axs.set(xlim=(0, nx-1), ylim=(-0.2, 2.2))
+    axs.set(xticks=range(0, nx+1, int(numpy.ceil(nx/500)*25)))
+    axs.set(xlabel=r"$z\;(cm)$", ylabel=r"$Amplitude$")
+    axs.text(0.02, 0.90, rf"$T$ = {ns}", transform=axs.transAxes)
+    axs.text(0.90, 0.90, rf"$\epsilon_r$ = {epsr}", transform=axs.transAxes)
+    axs.text(0.85, 0.80, rf"$\sigma$ = {sigma} $S/m$", transform=axs.transAxes)
+    fig.subplots_adjust(bottom=0.2, hspace=0.45)
+    fig.savefig("fd1d_amp_2_3.png", dpi=100)
 
 
-medium = namedtuple('medium', (
-    'nax', 'nbx',
-    'ncx', 'ndx',
-))
+FTrans = NamedTuple('FTrans',[
+    ('rpt',F64Array), ('ipt',F64Array),
+    ('rin',F64Array), ('iin',F64Array),
+])
 
 
-ftrans = namedtuple('ftrans', (
-    'r_pt', 'i_pt',
-    'r_in', 'i_in',
-))
+Medium = NamedTuple('Medium',[
+    ('nax',F64Array), ('nbx',F64Array),
+    ('ncx',F64Array), ('ndx',F64Array),
+])
 
 
-def gaussian(t: int, t0: int, sigma: float) -> float:
-    return np.exp(-0.5*((t - t0)/sigma)**2)
+def gaussian(ts: int, t0: int, sigma: float):
+    return exp(-0.5*((ts - t0)/sigma)**2)
 
 
-def fourier(t: int, nf: int, nx: int, dt: float, freq: np.ndarray, ex: np.ndarray, ft: ftrans) -> None:
+def fourier(ts: int, nf: int, nx: int, dt: float, freq: F64Array, ex: F64Array, ft: FTrans):
     # calculate the Fourier transform of input source
-    ft.r_in[0:nf] += cos(2*pi*freq[0:nf]*dt*t) * ex[10]
-    ft.i_in[0:nf] -= sin(2*pi*freq[0:nf]*dt*t) * ex[10]
+    ft.rin[0:nf] += cos(2*pi*freq[0:nf]*dt*ts) * ex[10]
+    ft.iin[0:nf] -= sin(2*pi*freq[0:nf]*dt*ts) * ex[10]
     # calculate the Fourier transform of Ex field
-    ft.r_pt[0:nf,0:nx] += cos(2*pi*freq[0:nf]*dt*t) * ex[0:nx]
-    ft.i_pt[0:nf,0:nx] -= sin(2*pi*freq[0:nf]*dt*t) * ex[0:nx]
+    ft.rpt[0:nf,0:nx] += cos(2*pi*freq[0:nf]*dt*ts) * ex[0:nx]
+    ft.ipt[0:nf,0:nx] -= sin(2*pi*freq[0:nf]*dt*ts) * ex[0:nx]
 
 
-def dxfield(t: int, nx: int, dx: np.ndarray, hy: np.ndarray) -> None:
+def dxfield(ts: int, nx: int, hy: F64Array, dx: F64Array):
     # calculate the electric flux density Dx
     dx[1:nx] += 0.5 * (hy[0:nx-1] - hy[1:nx])
     # put a Gaussian pulse at the low end
-    dx[1] += gaussian(t, 50, 10.0)
+    dx[1] += gaussian(ts, 50, 10.0)
 
 
-def exfield(nx: int, md: medium, dx: np.ndarray, ix: np.ndarray, sx: np.ndarray, ex: np.ndarray) -> None:
+def exfield(nx: int, md: Medium, ix: F64Array, sx: F64Array, dx: F64Array, ex: F64Array):
     # calculate the Ex field from Dx
     ex[1:nx] = md.nax[1:nx] * (dx[1:nx] - ix[1:nx] - md.ncx[1:nx] * sx[1:nx])
     ix[1:nx] += md.nbx[1:nx] * ex[1:nx]
     sx[1:nx] = md.ncx[1:nx] * sx[1:nx] + md.ndx[1:nx] * ex[1:nx]
 
 
-def hyfield(nx: int, ex: np.ndarray, hy: np.ndarray, bc: np.ndarray) -> None:
+def hyfield(nx: int, bc: F64Array, ex: F64Array, hy: F64Array):
     # absorbing boundary conditions
     ex[0], bc[0], bc[1] = bc[0], bc[1], ex[1]
     ex[nx-1], bc[3], bc[2] = bc[3], bc[2], ex[nx-2]
@@ -94,12 +97,12 @@ def hyfield(nx: int, ex: np.ndarray, hy: np.ndarray, bc: np.ndarray) -> None:
     hy[0:nx-1] += 0.5 * (ex[0:nx-1] - ex[1:nx])
 
 
-def dielectric(nx: int, dt: float, chi: float, tau: float, epsr: float, sigma: float) -> medium:
-    md = medium(
-        nax = np.full(nx, 1.0, dtype=np.float64),
-        nbx = np.full(nx, 0.0, dtype=np.float64),
-        ncx = np.full(nx, 0.0, dtype=np.float64),
-        ndx = np.full(nx, 0.0, dtype=np.float64),
+def dielectric(nx: int, dt: float, chi: float, tau: float, epsr: float, sigma: float):
+    md = Medium(
+        nax = numpy.full(nx, 1.0, numpy.float64),
+        nbx = numpy.full(nx, 0.0, numpy.float64),
+        ncx = numpy.full(nx, 0.0, numpy.float64),
+        ndx = numpy.full(nx, 0.0, numpy.float64),
     )
     eps0: float = 8.854e-12  # vacuum permittivity (F/m)
     md.nax[nx//2:] = 1/(epsr + sigma*dt/eps0 + chi*dt/tau)
@@ -114,13 +117,13 @@ def main():
     nx: int = 512  # number of grid points
     ns: int = 740  # number of time steps
 
-    dx = np.zeros(nx, dtype=np.float64)
-    ex = np.zeros(nx, dtype=np.float64)
-    ix = np.zeros(nx, dtype=np.float64)
-    sx = np.zeros(nx, dtype=np.float64)
-    hy = np.zeros(nx, dtype=np.float64)
+    dx = numpy.zeros(nx, numpy.float64)
+    ex = numpy.zeros(nx, numpy.float64)
+    ix = numpy.zeros(nx, numpy.float64)
+    sx = numpy.zeros(nx, numpy.float64)
+    hy = numpy.zeros(nx, numpy.float64)
 
-    bc = np.zeros(4, dtype=np.float64)
+    bc = numpy.zeros(4, numpy.float64)
 
     ds: float = 0.01  # spatial step (m)
     dt: float = ds/6e8  # time step (s)
@@ -128,31 +131,31 @@ def main():
     tau: float = 0.001e-6  # relaxation time (s)
     epsr: float = 2.0  # relative permittivity
     sigma: float = 0.01  # conductivity (S/m)
-    md: medium = dielectric(nx, dt, chi, tau, epsr, sigma)
+    md: Medium = dielectric(nx, dt, chi, tau, epsr, sigma)
 
     # frequency 50 MHz, 200 MHz, 500 MHz
-    freq = np.array(([50e6], [200e6], [500e6]), dtype=np.float64)
+    freq = numpy.array(([50e6], [200e6], [500e6]), numpy.float64)
     nf: int = len(freq)  # number of frequencies
 
-    ft = ftrans(
-        r_pt = np.zeros((nf, nx), dtype=np.float64),
-        i_pt = np.zeros((nf, nx), dtype=np.float64),
-        r_in = np.zeros((nf, 1), dtype=np.float64),
-        i_in = np.zeros((nf, 1), dtype=np.float64),
+    ft = FTrans(
+        rpt = numpy.zeros((nf,nx), numpy.float64),
+        ipt = numpy.zeros((nf,nx), numpy.float64),
+        rin = numpy.zeros((nf, 1), numpy.float64),
+        iin = numpy.zeros((nf, 1), numpy.float64),
     )
 
-    amplt = np.zeros((nf, nx), dtype=np.float64)
-    phase = np.zeros((nf, nx), dtype=np.float64)
+    amplt = numpy.zeros((nf,nx), numpy.float64)
+    phase = numpy.zeros((nf,nx), numpy.float64)
 
-    for t in np.arange(1, ns+1).astype(np.int32):
-        dxfield(t, nx, dx, hy)
-        exfield(nx, md, dx, ix, sx, ex)
-        fourier(t, nf, nx, dt, freq, ex, ft)
-        hyfield(nx, ex, hy, bc)
+    for ts in map(int,range(1,ns+1)):
+        dxfield(ts, nx, hy, dx)
+        exfield(nx, md, ix, sx, dx, ex)
+        fourier(ts, nf, nx, dt, freq, ex, ft)
+        hyfield(nx, bc, ex, hy)
 
     # calculate the amplitude and phase at each frequency
-    amplt = 1/hypot(ft.r_in,ft.i_in) * hypot(ft.r_pt,ft.i_pt)
-    phase = arctan2(ft.i_pt,ft.r_pt) - arctan2(ft.i_in,ft.r_in)
+    amplt = 1/hypot(ft.rin,ft.iin) * hypot(ft.rpt,ft.ipt)
+    phase = arctan2(ft.ipt,ft.rpt) - arctan2(ft.iin,ft.rin)
 
     visualize(ns, nx, epsr, sigma, md.nax, ex)
     amplitude(ns, nx, epsr, sigma, md.nax, amplt[2])
